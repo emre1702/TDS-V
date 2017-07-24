cb435d73cd6bb4b6ab2f0321c67283679a1d956d
emre1702 <emre1702@live.de>
2017-07-24 02:17:38 +0200

    easy way to trigger event to a lobby

---
+++ TDS/server/class/lobby/APIExtend.cs
@@ -0,0 +1,12 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+
+static partial class APIExtend {
+
+	#pragma warning disable IDE1006 // Benennungsstile
+	public static void triggerClientEventForLobby ( this API api, Class.Lobby lobby, string eventName, int teamID = -1, params object[] args ) {
+		lobby.SendAllPlayerEvent ( eventName, teamID, args );
+	}
+
+	#pragma warning restore IDE1006 // Benennungsstile
+}
