c0c68c706f634675dc88981632c5c304d7c3f63f
emre1702 <emre1702@live.de>
2017-08-02 17:05:48 +0200

    more weapons in arena

--- TDS/server/manager/lobby/ArenaManager.cs
+++ TDS/server/manager/lobby/ArenaManager.cs
@@ -13,10 +13,37 @@ namespace Manager {
 			lobby.AddTeam ( "Gut", (PedHash) ( 2047212121 ) );
 			lobby.AddTeam ( "Böse", (PedHash) ( 275618457 ) );
 			lobby.deleteWhenEmpty = false;
-			lobby.AddWeapon ( (WeaponHash) ( 453432689 ), 1000 );
-			lobby.AddWeapon ( (WeaponHash) ( 736523883 ), 1000 );
-			lobby.AddWeapon ( (WeaponHash) ( -2084633992 ), 1000 );
-			lobby.AddWeapon ( (WeaponHash) ( -1466123874 ), 1000 );
+
+			// Handguns //
+			lobby.AddWeapon ( (WeaponHash) ( 453432689 ), 1000 ); // Pistol
+			lobby.AddWeapon ( (WeaponHash) ( 1593441988 ), 1000 ); // CombatPistol
+			lobby.AddWeapon ( (WeaponHash) ( -1716589765 ), 1000 ); // Pistol50
+			lobby.AddWeapon ( (WeaponHash) ( -1076751822 ), 1000 ); // SNSPistol
+			lobby.AddWeapon ( (WeaponHash) ( -771403250 ), 1000 ); // HeavyPistol
+			lobby.AddWeapon ( (WeaponHash) ( 137902532 ), 1000 ); // VintagePistol
+			lobby.AddWeapon ( (WeaponHash) ( -598887786 ), 1000 ); // MarksmanPistol
+			lobby.AddWeapon ( (WeaponHash) ( -1045183535 ), 1000 ); // Revolver
+			lobby.AddWeapon ( (WeaponHash) ( -584646201 ), 1000 ); // APPistol
+
+			// Machine Guns //
+			lobby.AddWeapon ( (WeaponHash) ( 324215364 ), 1000 ); // MicroSMG
+			lobby.AddWeapon ( (WeaponHash) ( -619010992 ), 1000 ); // MachinePistol
+			lobby.AddWeapon ( (WeaponHash) ( 736523883 ), 1000 ); // SMG
+			lobby.AddWeapon ( (WeaponHash) ( -270015777 ), 1000 ); // AssaultSMG
+			lobby.AddWeapon ( (WeaponHash) ( 171789620 ), 1000 ); // CombatPDW
+			lobby.AddWeapon ( (WeaponHash) ( -1660422300 ), 1000 ); // MG
+			lobby.AddWeapon ( (WeaponHash) ( 2144741730 ), 1000 ); // CombatMG
+			lobby.AddWeapon ( (WeaponHash) ( 1627465347 ), 1000 ); // Gusenberg
+			lobby.AddWeapon ( (WeaponHash) ( -1121678507 ), 1000 ); // MiniSMG
+
+			// Assault Rifles //
+			lobby.AddWeapon ( (WeaponHash) ( -1074790547 ), 1000 ); // AssaultRifle
+			lobby.AddWeapon ( (WeaponHash) ( -2084633992 ), 1000 ); // CarbineRifle
+			lobby.AddWeapon ( (WeaponHash) ( -1357824103 ), 1000 ); // AdvancedRifle
+			lobby.AddWeapon ( (WeaponHash) ( -1063057011 ), 1000 ); // SpecialCarbine
+			lobby.AddWeapon ( (WeaponHash) ( 2132975508 ), 1000 ); // BullpupRifle
+			lobby.AddWeapon ( (WeaponHash) ( 1649403952 ), 1000 ); // CompactRifle
+
 			lobby.Start ();
 		}
 
