25579cd53296c2e950eeb31ac5a25626a0ca2fc4
emre1702 <emre1702@live.de>
2017-08-04 14:53:33 +0200

    chat-logs added

---
+++ TDS/server/manager/logs/Chat.cs
@@ -0,0 +1,9 @@
+﻿using GrandTheftMultiplayer.Server.Elements;
+
+namespace Manager {
+	partial class Log {
+		public static void Chat ( string chatstr, Client player, string lobbyname ) {
+			AddLogEntry ( "chat", chatstr, lobbyname, Account.playerUIDs[player.socialClubName].ToString() );
+		}
+	}
+}
