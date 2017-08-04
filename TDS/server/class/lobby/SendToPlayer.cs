5c2dba57d74c84d101f5d00259a23be76fa9ebe8
emre1702 <emre1702@live.de>
2017-08-04 14:52:49 +0200

    file for SendToPlayer methods

---
+++ TDS/server/class/lobby/SendToPlayer.cs
@@ -0,0 +1,37 @@
+﻿using GrandTheftMultiplayer.Server.API;
+
+namespace Class {
+
+	partial class Lobby : Script {
+
+		public void SendAllPlayerEvent ( string eventName, int teamindex = -1, params object[] args ) {
+			if ( teamindex == -1 )
+				for ( int i = 0; i < this.players.Count; i++ )
+					for ( int j = 0; j < this.players[i].Count; j++ )
+						this.players[i][j].triggerEvent ( eventName, args );
+			else
+				for ( int j = 0; j < this.players[teamindex].Count; j++ )
+					this.players[teamindex][j].triggerEvent ( eventName, args );
+		}
+
+		public void SendAllPlayerLangNotification ( string langstr, int teamindex = -1, params string[] args ) {
+			if ( teamindex == -1 )
+				for ( int i = 0; i < this.players.Count; i++ )
+					for ( int j = 0; j < this.players[i].Count; j++ )
+						this.players[i][j].SendLangNotification ( langstr, args );
+			else
+				for ( int j = 0; j < this.players[teamindex].Count; j++ )
+					this.players[teamindex][j].SendLangNotification ( langstr, args );
+		}
+
+		public void SendAllPlayerChatMessage ( string message, int teamindex = -1 ) {
+			if ( teamindex == -1 )
+				for ( int i = 0; i < this.players.Count; i++ )
+					for ( int j = 0; j < this.players[i].Count; j++ )
+						this.players[i][j].sendChatMessage ( message );
+			else
+				for ( int j = 0; j < this.players[teamindex].Count; j++ )
+					this.players[teamindex][j].sendChatMessage ( message );
+		}
+	}
+}
