5027b6a20f898d64c0c8c5c4c6936b49cf8f0342
emre1702 <emre1702@live.de>
2017-08-04 23:01:28 +0200

    "player_doesnt_exist" added

--- TDS/server/manager/utility/LanguageManager.cs
+++ TDS/server/manager/utility/LanguageManager.cs
@@ -25,6 +25,7 @@ static class Language {
 			{ "got_last_hitted_kill", "Du hast {1} zuletzt getroffen und den Kill bekommen." },
 			{ "got_assist", "Du hast den Assist von {1} bekommen." },
 			{ "map_won_voting", "Die Map {1} hat das Voting gewonnen!" },
+			{ "player_doesnt_exist", "Der Spieler existiert nicht!" },
 
 			{ "welcome_1", "Willkommen auf dem ~b~Team Deathmatch Server~w~." },
 			{ "welcome_2", "Für Ankündigungen, Support, Bug-Meldung usw." },
@@ -50,6 +51,7 @@ static class Language {
 			{ "got_last_hitted_kill", "You hitted {1} last and got the kill." },
 			{ "got_assist", "You got the assist of {1}." },
 			{ "map_won_voting", "The map {1} won the voting!" },
+			{ "player_doesnt_exist", "The player doesn't exist!" },
 
 			{ "welcome_1", "Welcome to ~b~Team Deathmatch Server~w~." },
 			{ "welcome_2", "For announcements, support, bug-reports etc." },
