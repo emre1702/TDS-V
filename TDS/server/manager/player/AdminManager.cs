2773100bc2e1bb7ad00529be1b3c9d9b026f600b
emre1702 <emre1702@live.de>
2017-08-04 11:09:19 +0200

    Bugfix: Admin-commands didn't work anymore

--- TDS/server/manager/player/AdminManager.cs
+++ TDS/server/manager/player/AdminManager.cs
@@ -4,7 +4,7 @@ using GrandTheftMultiplayer.Server.Elements;
 using System.Collections.Generic;
 
 namespace Manager {
-	class Admin {
+	class Admin : Script {
 		private static Dictionary<string, int> neededLevels = new Dictionary<string, int> {
 			{ "next", 1 },
 			{ "lobbykick", 1 },
