0f32cb64df923e486775090d3175097a7bf03b4a
emre1702 <emre1702@live.de>
2017-08-04 23:00:56 +0200

    Adminlog code improvement

--- TDS/server/manager/logs/Admin.cs
+++ TDS/server/manager/logs/Admin.cs
@@ -2,8 +2,12 @@
 
 namespace Manager {
 	partial class Log {
+		public static void Admin ( string command, Client player, string targetuid, string lobbyname ) {
+			AddLogEntry ( "admin", command, lobbyname, Account.playerUIDs[player.socialClubName].ToString (), targetuid );
+		}
+
 		public static void Admin ( string command, Client player, Client target, string lobbyname ) {
-			AddLogEntry ( "admin", command, lobbyname, Account.playerUIDs[player.socialClubName].ToString (), target != null ? Account.playerUIDs[target.socialClubName].ToString () : "0" );
+			Admin ( command, player, target != null ? Account.playerUIDs[target.socialClubName].ToString () : "0", lobbyname );
 		}
 	}
 }
