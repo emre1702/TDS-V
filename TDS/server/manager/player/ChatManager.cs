eb67e60110aea82581d082e936e712d51a3f1bdb
emre1702 <emre1702@live.de>
2017-08-04 14:54:09 +0200

    chatmanager added (for chat-colors and logging)

---
+++ TDS/server/manager/player/ChatManager.cs
@@ -0,0 +1,19 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+
+namespace Manager {
+	class Chat {
+		public static void ChatOnStart ( API api ) {
+			api.onChatMessage += OnChatMessage;
+		}
+
+		private static void OnChatMessage ( Client player, string message, CancelEventArgs e ) {
+			e.Cancel = true;
+			Class.Character character = player.GetChar ();
+			Log.Chat ( message, player, character.lobby.name );
+			string teamfontcolor = character.lobby.teamColorStrings[character.team] ?? "w";
+			string changedmessage = "~" + teamfontcolor + "~" + player.socialClubName + "~s~: " + message;
+			character.lobby.SendAllPlayerChatMessage ( changedmessage );
+		}
+	}
+}
