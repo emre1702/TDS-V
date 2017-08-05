3376f4681013519fe5f0d963fc9ae834a3c74b45
emre1702 <emre1702@live.de>
2017-08-05 12:37:22 +0200

    maps with 0 votes get removed + votes reset after round-end

--- TDS/server/class/lobby/MapVoting.cs
+++ TDS/server/class/lobby/MapVoting.cs
@@ -15,42 +15,54 @@ namespace Class {
 		}
 
 		private void AddVoteToMap ( Client player, string mapname ) {
-			if ( playerVotes.ContainsKey ( player ) ) {
-				if ( playerVotes[player] == mapname )
+			if ( this.playerVotes.ContainsKey ( player ) ) {
+				string oldvote = this.playerVotes[player];
+				if ( oldvote == mapname )
 					return;
-				mapVotes[playerVotes[player]]--;
-				this.SendAllPlayerEvent ( "onAddVoteToMap", -1, mapname, playerVotes[player] );
+				this.mapVotes[oldvote]--;
+				if ( this.mapVotes[oldvote] <= 0 ) {
+					this.SendAllPlayerEvent ( "onMapRemoveFromVoting", -1, oldvote );
+					this.SendAllPlayerEvent ( "onAddVoteToMap", -1, mapname );
+					this.mapVotes.Remove ( oldvote );
+				} else {
+					this.SendAllPlayerEvent ( "onAddVoteToMap", -1, mapname, oldvote );
+				}
 			} else
 				this.SendAllPlayerEvent ( "onAddVoteToMap", -1, mapname );
-			playerVotes[player] = mapname;
-			mapVotes[mapname]++;
+			this.playerVotes[player] = mapname;
+			this.mapVotes[mapname]++;
 		}
 
 		private void AddMapToVoting ( Client player, string mapname ) {
-			if ( !mapVotes.ContainsKey ( mapname ) ) {
-				// Anti-Cheat //
-				if ( !this.mapNames.Contains ( mapname ) ) {
-					Manager.Log.Error ( player.socialClubName + " voted for " + mapname + ", but it doesn't exist!", this.name );
-					return;
-				}
-				///////////////
-				mapVotes[mapname] = 0;
-				this.SendAllPlayerEvent ( "onNewMapForVoting", -1, mapname );
+			if ( !this.mapVotes.ContainsKey ( mapname ) ) {
+				if ( this.mapVotes.Count < 6 ) {
+					// Anti-Cheat //
+					if ( !this.mapNames.Contains ( mapname ) ) {
+						Manager.Log.Error ( player.socialClubName + " voted for " + mapname + ", but it doesn't exist!", this.name );
+						return;
+					}
+					///////////////
+					this.mapVotes[mapname] = 0;
+					this.SendAllPlayerEvent ( "onNewMapForVoting", -1, mapname );
+				} else
+					player.SendLangNotification ( "not_more_maps_for_voting_allowed" );
 			}
 			this.AddVoteToMap ( player, mapname );
 		}
 
 		private Map GetNextMap ( ) {
-			if ( mapVotes.Count > 0 ) {
-				string wonmap = mapVotes.Aggregate ( ( l, r ) => l.Value > r.Value ? l : r ).Key;
+			if ( this.mapVotes.Count > 0 ) {
+				string wonmap = this.mapVotes.Aggregate ( ( l, r ) => l.Value > r.Value ? l : r ).Key;
 				this.SendAllPlayerLangNotification ( "map_won_voting", -1, wonmap );
+				this.mapVotes = new Dictionary<string, int> ();
+				this.playerVotes = new Dictionary<Client, string> ();
 				return Manager.Map.GetMapClass ( wonmap, this );
 			} else
 				return this.GetRandomMap ();
 		}
 
 		private void SyncMapVotingOnJoin ( Client player ) {
-			if ( mapVotes.Count > 0 ) {
+			if ( this.mapVotes.Count > 0 ) {
 				List<string> mapnames = new List<string> ();
 				List<int> mapvotes = new List<int> ();
 
