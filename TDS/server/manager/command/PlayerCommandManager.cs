e401509c59f9b1285520b020ebbe1fb75b81d73e
emre1702 <emre1702@live.de>
2017-08-07 21:10:30 +0200

    new file, added /leave and /kick

---
+++ TDS/server/manager/command/PlayerCommandManager.cs
@@ -0,0 +1,27 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+
+namespace Manager {
+	class PlayerCommand : Script {
+
+		[Command ( "leave", Alias = "leavelobby,lobbyleave", Description = "Leaves the lobby", AddToHelpmanager = true )]
+		public void Leave ( Client player ) {
+			Class.Lobby lobby = player.GetChar ().lobby;
+			if ( lobby != MainMenu.lobby ) {
+				lobby.RemovePlayer ( player );
+			}
+		}
+
+		[Command ( "kill", Alias = "suicide", Description = "Commits suicide", AddToHelpmanager = true )]
+		public void Kill ( Client player ) {
+			Class.Character character = player.GetChar ();
+			Class.Lobby lobby = character.lobby;
+			if ( lobby.isPlayable ) {
+				if ( character.lifes > 0 ) {
+					lobby.KillPlayer ( player, "commited_suicide" );
+				}
+			}
+		}
+	}
+}
