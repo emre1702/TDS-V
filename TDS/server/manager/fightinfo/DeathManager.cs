63a1924b59aef3db6861d9340ac4c93b2fb7e5e8
emre1702 <emre1702@live.de>
2017-08-07 20:50:33 +0200

    create file for onClientPlayerDeath

---
+++ TDS/server/manager/fightinfo/DeathManager.cs
@@ -0,0 +1,12 @@
+﻿using GrandTheftMultiplayer.Server.Elements;
+
+namespace Manager {
+
+	static partial class FightInfo {
+
+		public static void DeathInfoSync ( this Class.Lobby lobby, Client player, int team ) {
+			lobby.SendAllPlayerEvent ( "onClientPlayerDeath", -1, player, team );
+		}
+
+	}
+}
