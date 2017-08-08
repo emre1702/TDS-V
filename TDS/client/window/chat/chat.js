0d268d8d3cb5bfbef5612a3bf00e519462d8baa1
emre1702 <emre1702@live.de>
2017-08-08 13:17:48 +0200

    use new element for text, scroll-fix, replace fontcolors with htmlcolors, line-break after each text, focus after 100ms

--- TDS/client/window/chat/chat.js
+++ TDS/client/window/chat/chat.js
@@ -2,34 +2,32 @@ var False = false;
 var True = true;
 
 function updateScroll() {
-	var body = $("#chat-body");
-	if (body.scrollTop() >= body[0].scrollHeight - 400) {
-		body.scrollTop(body[0].scrollHeight);
-	}		
+	var body = $( "#chat-body" );
+	body.animate( { scrollTop: body.height() }, "slow" );	
 }
 
 function formatMsg(input) {
 	var start = '<span style="color: white;">';
 	var output = start;
-	
-	var pass = input.replace( "~r~", '</span><span style="color: rgb(222, 50, 50);">');
-	pass = pass.replace( "~b~", '</span><span style="color: rgb(92, 180, 227);">');
-	pass = pass.replace( "~g~", '</span><span style="color: rgb(113, 202, 113);">');
-	//pass = pass.replace( "~y~", '</span><span style="color: rgb(238, 198, 80);">' );
-	//pass = pass.replace( "~p~", '</span><span style="color: rgb(131, 101, 224);">' );
-	//pass = pass.replace( "~q~", '</span><span style="color: rgb(226, 79, 128);">' );
-	pass = pass.replace( "~o~", '</span><span style="color: rgb(253, 132, 85);">' );
-	//pass = pass.replace( "~c~", '</span><span style="color: rgb(139, 139, 139);">' );
-	//pass = pass.replace( "~m~", '</span><span style="color: rgb(99, 99, 99);">' );
-	//pass = pass.replace( "~u~", '</span><span style="color: rgb(0, 0, 0);">' );
-	pass = pass.replace( "~s~", '</span><span style="color: rgb(220, 220, 220);">' );
-	pass = pass.replace( "~w~", '</span><span style="color: white;">' );
+
+	var pass = input.replace( /~r~/g, '</span><span style="color: rgb(222, 50, 50);">' );
+	pass = pass.replace( /~b~/g, '</span><span style="color: rgb(92, 180, 227);">');
+	pass = pass.replace( /~g~/g, '</span><span style="color: rgb(113, 202, 113);">');
+	//pass = pass.replace( /~y~/g, '</span><span style="color: rgb(238, 198, 80);">' );
+	//pass = pass.replace( /~p~/g, '</span><span style="color: rgb(131, 101, 224);">' );
+	//pass = pass.replace( /~q~/g, '</span><span style="color: rgb(226, 79, 128);">' );
+	pass = pass.replace( /~o~/g, '</span><span style="color: rgb(253, 132, 85);">' );
+	//pass = pass.replace( /~c~/g, '</span><span style="color: rgb(139, 139, 139);">' );
+	//pass = pass.replace( /~m~/g, '</span><span style="color: rgb(99, 99, 99);">' );
+	//pass = pass.replace( /~u~/g, '</span><span style="color: rgb(0, 0, 0);">' );
+	pass = pass.replace( /~s~/g, '</span><span style="color: rgb(220, 220, 220);">' );
+	pass = pass.replace( /~w~/g, '</span><span style="color: white;">' );
 
 	return output + pass + "</span>";
 }
 
 function addMessage(msg) {
-	var child = $("<p>" + formatMsg(msg) + "</p>");
+	var child = $( "<text>" + formatMsg( msg ) + "</text><br>");
 	child.hide();
 	$("#chat-body").append(child);
 	child.fadeIn();
@@ -38,7 +36,7 @@ function addMessage(msg) {
 }
 
 function addColoredMessage(msg, r,g,b) {
-	var child = $('<p style="color: rgb(' + r + ', ' + g + ', ' + b + ');">' + formatMsg(msg) + '</p>');
+	var child = $('<text style="color: rgb(' + r + ', ' + g + ', ' + b + ');">' + formatMsg(msg) + '</text><br>');
 	child.hide();
 	$("#chat-body").append(child);
 	child.fadeIn();
@@ -46,15 +44,20 @@ function addColoredMessage(msg, r,g,b) {
 	updateScroll();
 }
 
-function setFocus(focus) {
+function setFocus( focus, fromscript = false ) {
 	var mainInput = $("#main-input");
-	if (focus) {		
-		mainInput.show();
-		mainInput.val("");
-		mainInput.focus();
+	if ( focus ) {
+		mainInput.fadeIn();
+		mainInput.val( "" );
+		setTimeout( function () {
+			mainInput.focus();
+		}, 100 );
+		
 	} else {
 		mainInput.hide();
-		mainInput.val("");
+		mainInput.val( "" );
+		if ( !fromscript )
+			resourceCall( "onFocusChange", false, true );
 	}
 }
 
@@ -70,7 +73,8 @@ function onKeyUp(event) {
 			catch(err) {
 				$("body").text(err);
 			}
-		}
+		} 
+			
 		setFocus(false);	
 	}
 }
