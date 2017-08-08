940c7c1fada3d1dd5301cfa09a6c39a837030f63
emre1702 <emre1702@live.de>
2017-08-08 18:28:44 +0200

    put commands from utility-manager to this file

--- TDS/server/manager/command/PlayerCommandManager.cs
+++ TDS/server/manager/command/PlayerCommandManager.cs
@@ -1,6 +1,8 @@
 ﻿using GrandTheftMultiplayer.Server.API;
 using GrandTheftMultiplayer.Server.Elements;
 using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Math;
 
 namespace Manager {
 	class PlayerCommand : Script {
@@ -27,6 +29,31 @@ namespace Manager {
 		[Command ( "globalchat", Alias = "globalsay, global", Description = "Writes in global-chat", AddToHelpmanager = true )]
 		public void GlobalChat ( Client player, string text ) {
 			Chat.SendGlobalMessage ( player, text );
+		}
+
+		[Command ( "pos", Alias = "getpos,rot,getrot", Description = "Gets your position and rotation", AddToHelpmanager = true )]
+		public void SendPlayerPosition ( Client sender ) {
+			if ( API.shared.isPlayerInAnyVehicle ( sender ) ) {
+				NetHandle veh = API.shared.getPlayerVehicle ( sender );
+				Vector3 pos = API.shared.getEntityPosition ( veh );
+				API.shared.sendChatMessageToPlayer ( sender, "Vehicle X: " + pos.X + " Y: " + pos.Y + " Z: " + pos.Z );
+				Vector3 rot = API.shared.getEntityRotation ( veh );
+				API.shared.sendChatMessageToPlayer ( sender, "Vehicle ROT RX: " + rot.X + " RY: " + rot.Y + " RZ: " + rot.Z );
+			} else {
+				Vector3 pos = API.shared.getEntityPosition ( sender );
+				API.shared.sendChatMessageToPlayer ( sender, "Player X: " + pos.X + " Y: " + pos.Y + " Z: " + pos.Z );
+				Vector3 rot = API.shared.getEntityRotation ( sender );
+				API.shared.sendChatMessageToPlayer ( sender, "Player ROT RX: " + rot.X + " RY: " + rot.Y + " RZ: " + rot.Z );
+			}
+		}
+
+		[Command ( "checkmapname", Description = "Checks if a map-name is already taken (needed to now for new maps)", AddToHelpmanager = true )]
+		public void CheckMapName ( Client player, string mapname ) {
+			if ( Map.mapByName.ContainsKey ( mapname ) ) {
+				API.sendNotificationToPlayer ( player, "map-name already taken" );
+			} else {
+				API.sendNotificationToPlayer ( player, "map-name is available" );
+			}
 		}
 	}
 }
