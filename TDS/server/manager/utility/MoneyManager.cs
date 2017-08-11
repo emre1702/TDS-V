22aede3d4e9172035500d6d9758ee0ab7eedd4c4
emre1702 <emre1702@live.de>
2017-08-11 14:11:18 +0200

    money manager

---
+++ TDS/server/manager/utility/MoneyManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using GrandTheftMultiplayer.Server.Elements;
+
+namespace Manager {
+	static class Money {
+		public static Dictionary<string, double> moneyForDict = new Dictionary<string, double> {
+			{ "kill", 20 },
+			{ "assist", 10 },
+			{ "damage", 1/10 },
+		};
+
+		public static void GivePoints ( this Client player, double points, Class.Character character = null ) {
+			character = character ?? player.GetChar ();
+			character.money += (int) points;
+		}
+
+	}
+}
