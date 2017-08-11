d2d62fd78d0a90208c2eef881100f3abf1ada624
emre1702 <emre1702@live.de>
2017-08-12 01:17:00 +0200

    hitsound loading the audio and loop = false

--- TDS/client/damagesys/hitsound.js
+++ TDS/client/damagesys/hitsound.js
@@ -1,7 +1,10 @@
 "use strict";
 API.onServerEventTrigger.connect(function (eventName, args) {
     if (eventName == "onClientPlayerHittedOpponent") {
-        API.startAudio(soundspath + "hit.wav");
+        API.startAudio(soundspath + "hit.wav", false);
         API.setAudioVolume(0.5);
     }
+});
+API.onResourceStart.connect(function () {
+    API.preloadAudio(soundspath + "hit.wav");
 });
--- TDS/client/damagesys/hitsound.ts
+++ TDS/client/damagesys/hitsound.ts
@@ -2,7 +2,11 @@
 
 API.onServerEventTrigger.connect( function ( eventName, args ) {
 	if ( eventName == "onClientPlayerHittedOpponent" ) {
-		API.startAudio( soundspath+"hit.wav" );
+		API.startAudio( soundspath+"hit.wav", false );
 		API.setAudioVolume( 0.5 );
 	}
+} );
+
+API.onResourceStart.connect( function () {
+	API.preloadAudio( soundspath + "hit.wav" );
 } );
