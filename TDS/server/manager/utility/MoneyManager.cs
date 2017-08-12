b9e77c772b0011ef41278b32c6f50bd7d8a8c5be
emre1702 <emre1702@live.de>
2017-08-12 14:22:23 +0200

    nothing

--- TDS/server/manager/utility/MoneyManager.cs
+++ TDS/server/manager/utility/MoneyManager.cs
@@ -7,7 +7,7 @@ namespace Manager {
 		public static Dictionary<string, double> moneyForDict = new Dictionary<string, double> {
 			{ "kill", 20 },
 			{ "assist", 10 },
-			{ "damage", 1/10 },
+			{ "damage", 0.1 },
 		};
 
 		public static void GivePoints ( this Client player, double points, Class.Character character = null ) {
