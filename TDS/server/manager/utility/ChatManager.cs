5182bc5fcc735a78d25c5ad11de1dae77770fedd
emre1702 <emre1702@live.de>
2017-08-18 00:00:22 +0200

    added chat for adminsay and adminchat

--- TDS/server/manager/utility/ChatManager.cs
+++ TDS/server/manager/utility/ChatManager.cs
@@ -10,7 +10,7 @@ namespace Manager {
 
 		private static void OnChatMessageFunc ( Client player, string message ) {
 			Class.Character character = player.GetChar ();
-			Log.Chat ( message, player, character.lobby.name );
+			Log.Chat ( message, player, "chat "+character.lobby.name );
 			string teamfontcolor = character.lobby.teamColorStrings[character.team] ?? "w";
 			string changedmessage = "~" + teamfontcolor + "~" + player.socialClubName + "~s~: " + message;
 			character.lobby.SendAllPlayerChatMessage ( changedmessage );
@@ -27,6 +27,20 @@ namespace Manager {
 			string teamfontcolor = character.lobby.teamColorStrings[character.team];
 			string changedmessage = "[GLOBAL] ~" + teamfontcolor + "~" + player.socialClubName + "~s~: " + message;
 			API.shared.sendChatMessageToAll ( changedmessage );
+		}
+
+		public static void SendAdminMessage ( Client player, string message ) {
+			Class.Character character = player.GetChar ();
+			Log.Chat ( message, player, "osay" );
+			string changedmessage = Admin.levelFontColor[character.adminLvl]+"["+Admin.nameByLevel[character.adminLvl]+"] ~w~"+player.socialClubName+": ~s~"+message;
+			API.shared.sendChatMessageToAll ( changedmessage );
+		}
+
+		public static void SendAdminChat ( Client player, string message ) {
+			Class.Character character = player.GetChar ();
+			Log.Chat ( message, player, "achat" );
+			string changedmessage = "~w~[ADMINCHAT] "+ Admin.levelFontColor[character.adminLvl] + player.socialClubName + ": ~s~" + message;
+			Admin.SendChatMessageToAdmins ( changedmessage );
 		}
 
 		public static void SendTeamChat ( Client player, string message ) {
