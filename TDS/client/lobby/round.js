1a8f2188c33fb6b27a4ff7171d7ed819b8cb900f
emre1702 <emre1702@live.de>
2017-08-22 01:42:30 +0200

    deactivate weapon-usage when not in round

--- TDS/client/lobby/round.js
+++ TDS/client/lobby/round.js
@@ -2,10 +2,17 @@
 let rounddata = {
     mapinfo: null,
     isspectator: true,
+    inround: false
 };
 function setMapInfo(mapname) {
     rounddata.mapinfo = new cText(mapname, res.Width * 0.5, res.Height * 0.95, 0.5, 255, 255, 255, 255, 0, 2, true);
 }
+API.onUpdate.connect(function () {
+    if (!rounddata.inround) {
+        API.disableControlThisFrame(24);
+        API.disableControlThisFrame(257);
+    }
+});
 function removeMapInfo() {
     if (rounddata.mapinfo != null) {
         rounddata.mapinfo.remove();
--- TDS/client/lobby/round.ts
+++ TDS/client/lobby/round.ts
@@ -1,14 +1,24 @@
 ﻿/// <reference path="../types-gt-mp/index.d.ts" />
 
+
 let rounddata = {
 	mapinfo: null,
 	isspectator: true,
+	inround: false
 }
 
 
 function setMapInfo ( mapname ) {
 	rounddata.mapinfo = new cText( mapname, res.Width * 0.5, res.Height * 0.95, 0.5, 255, 255, 255, 255, 0, 2, true );
 }
+
+
+API.onUpdate.connect( function () {
+	if ( !rounddata.inround ) {
+		API.disableControlThisFrame( 24 );
+		API.disableControlThisFrame( 257 );
+	}
+} );
 
 
 function removeMapInfo() {
