90f96d7360c723f7f5bebd96ce0b742c6d1167a2
emre1702 <emre1702@live.de>
2017-08-22 00:54:00 +0200

    started killingspree

---
+++ TDS/server/class/damagesys/KillingSpree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Shared;
+
+namespace Class {
+	partial class Damagesys {
+
+		private static Dictionary<int, int> spreeReward = new Dictionary<int, int> ();
+
+		private void CheckKillingSpree ( Client player ) {
+			if ( spreeReward.ContainsKey ( this.playerKills[player] ) ) {
+
+			}
+		}
+
+	}
+}
