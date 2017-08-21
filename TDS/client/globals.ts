ddd9e82d42cfb5702e735fb061e487df2256f1dd
emre1702 <emre1702@live.de>
2017-08-22 00:51:00 +0200

    nothing

--- TDS/client/globals.ts
+++ TDS/client/globals.ts
@@ -4,3 +4,11 @@ var res = API.getScreenResolutionMaintainRatio();
 var nothidecursor = 0;
 var soundspath = "client/sounds/";
 var currentmoney = null;
+
+/* API.onUpdate.connect( function () {
+	// GAME CONTROLS DEBUG
+	for ( var control = 0; control < 338; control++ ) {
+		if ( API.isControlJustPressed( control ) )
+			API.sendChatMessage( "CONTROL PRESSED: " + control );
+	}
+} );*/
