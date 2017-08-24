5401382812c58cda7ca0d7651924fe10459f55e4
emre1702 <emre1702@live.de>
2017-08-24 14:02:00 +0200

    - added directories in map folder to show the creator - enumerate through all directories in map folder - save the creator of the map - AddInfosToMapClass to map.AddInfos - save mapmid (used later) - better debugging for map-manager

--- TDS/server/manager/map/MapManager.cs
+++ TDS/server/manager/map/MapManager.cs
@@ -7,7 +7,7 @@ using System.IO;
 using Manager;
 
 namespace Manager {
-	class Map {
+	static class Map {
 		private static string mapsPath = "resources/TDS/server/maps/";
 		public static List<string> normalMapNames = new List<string> ();
 		public static List<string> hostageMapNames = new List<string> ();
@@ -16,41 +16,36 @@ namespace Manager {
 			{ "german", new List<string>() },
 		};
 		public static Dictionary<string, string> mapByName = new Dictionary<string, string> ();
+		public static Dictionary<string, string> mapCreator = new Dictionary<string, string> ();
 
 		public static void MapOnStart () {
-
-			IEnumerable<string> files = Directory.EnumerateFiles ( mapsPath, "*.xml" );
+			IEnumerable<string> directories = Directory.EnumerateDirectories ( mapsPath );
 			Class.Map map = new Class.Map ();
-			foreach ( string filepath in files ) {
-				string filename = Path.GetFileNameWithoutExtension ( filepath );
-				if ( AddInfosToMapClass ( map, filename, out map ) ) {
-					if ( map.name != null ) {
-						if ( map.type == "normal" )
-							normalMapNames.Add ( map.name );
-						else if ( map.type == "hostage" )
-							hostageMapNames.Add ( map.name );
-						mapDescriptions["english"].Add ( map.description["english"] );
-						mapDescriptions["german"].Add ( map.description["german"] );
-						mapByName[map.name] = filename;
-					} else
-						Log.Error ( "Map " + filename + " got no name!" );
+			foreach ( string dir in directories ) {
+				string creator = Path.GetFileName ( dir );
+				IEnumerable<string> files = Directory.EnumerateFiles ( dir, "*.xml" );
+				foreach ( string filepath in files ) {
+					string filename = Path.GetFileNameWithoutExtension ( filepath );
+					mapCreator[filename] = creator;
+					if ( map.AddInfos ( filename ) ) {
+						if ( map.name != null ) {
+							if ( map.type == "normal" )
+								normalMapNames.Add ( map.name );
+							else if ( map.type == "hostage" )
+								hostageMapNames.Add ( map.name );
+							mapDescriptions["english"].Add ( map.description["english"] );
+							mapDescriptions["german"].Add ( map.description["german"] );
+							mapByName[map.name] = filename;
+						} else
+							Log.Error ( "Map " + filename + " got no name!" );
+					}
 				}
 			}
 		}
 
-		private static bool AddInfosToMapClass ( Class.Map map, string mapname, out Class.Map newmap ) {
+		private static bool AddInfos ( this Class.Map map, string mapfilename ) {
+			string path = mapsPath + mapCreator[mapfilename] + "/" + mapfilename + ".xml";
 			try {
-				string path = mapsPath + mapname + ".xml";
-				map.type = "normal";
-				map.description = new Dictionary<string, string> {
-					{ "english", "No info available!" },
-					{ "german", "Keine Info verfügbar!" }
-				};
-				map.teamSpawns = new Dictionary<int, List<Vector3>> ();
-				map.teamRots = new Dictionary<int, List<Vector3>> ();
-				map.mapLimits = new List<Vector3> ();
-				map.created = true;
-
 				using ( XmlReader reader = XmlReader.Create ( path ) ) {
 					while ( reader.Read () ) {
 						if ( reader.NodeType == XmlNodeType.Element ) {
@@ -61,8 +56,10 @@ namespace Manager {
 							} else if ( reader.Name == "english" || reader.Name == "german" ) {
 								map.description[reader.Name] = reader.ReadString ();
 							} else if ( reader.Name == "limit" ) {
-								Vector3 pos = new Vector3 ( reader["x"].ToFloat(), reader["y"].ToFloat (), 0 );
+								Vector3 pos = new Vector3 ( reader["x"].ToFloat (), reader["y"].ToFloat (), 0 );
 								map.mapLimits.Add ( pos );
+							} else if ( reader.Name == "middle" ) {
+								map.mapmid = new Vector3 ( reader["x"].ToFloat (), reader["y"].ToFloat (), reader["z"].ToFloat () );
 							} else if ( reader.Name.StartsWith ( "team" ) ) {
 								int teamnumber = Convert.ToInt16 ( reader.Name.Substring ( 4 ) );
 								if ( !map.teamSpawns.ContainsKey ( teamnumber ) ) {
@@ -77,18 +74,16 @@ namespace Manager {
 						}
 					}
 				}
-				newmap = map;
 				return true;
 			} catch ( Exception e ) {
-				Log.Error ( "Error in Manager.Map.GetMapClass: " + e.ToString () );
-				newmap = map;
+				Log.Error ( "Error in Manager.Map.GetMapClass: " + e.ToString ()+" ("+path+")" );
 				return false;
 			}
 		}
 
 		public static Class.Map GetMapClass ( string mapname, Class.Lobby lobby ) {
 			Class.Map map = new Class.Map ();
-			if ( AddInfosToMapClass ( map, mapByName[mapname], out map ) ) {
+			if ( map.AddInfos ( mapByName[mapname] ) ) {
 				return map;
 			} else
 				return lobby.GetRandomMap ();
