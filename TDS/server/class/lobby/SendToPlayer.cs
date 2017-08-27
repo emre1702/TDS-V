5b50fbb3154d62cec9feaa10c069f393311e8273
emre1702 <emre1702@live.de>
2017-08-27 02:57:51 +0200

    check if player exists

--- TDS/server/class/lobby/SendToPlayer.cs
+++ TDS/server/class/lobby/SendToPlayer.cs
@@ -5,33 +5,40 @@ namespace Class {
 	partial class Lobby {
 
 		public void SendAllPlayerEvent ( string eventName, int teamindex = -1, params object[] args ) {
-			if ( teamindex == -1 )
+			if ( teamindex == -1 ) {
 				for ( int i = 0; i < this.players.Count; i++ )
-					for ( int j = 0; j < this.players[i].Count; j++ )
-						this.players[i][j].triggerEvent ( eventName, args );
-			else
+					for ( int j = this.players[i].Count - 1; j >= 0; j-- )
+						if ( this.players[i][j].exists )
+							this.players[i][j].triggerEvent ( eventName, args );
+
+			} else
 				for ( int j = 0; j < this.players[teamindex].Count; j++ )
-					this.players[teamindex][j].triggerEvent ( eventName, args );
+					if ( this.players[teamindex][j].exists )
+						this.players[teamindex][j].triggerEvent ( eventName, args );
 		}
 
 		public void SendAllPlayerLangNotification ( string langstr, int teamindex = -1, params string[] args ) {
-			if ( teamindex == -1 )
+			if ( teamindex == -1 ) { 
 				for ( int i = 0; i < this.players.Count; i++ )
 					for ( int j = 0; j < this.players[i].Count; j++ )
-						this.players[i][j].SendLangNotification ( langstr, args );
-			else
+						if ( this.players[i][j].exists )
+							this.players[i][j].SendLangNotification ( langstr, args );
+			} else
 				for ( int j = 0; j < this.players[teamindex].Count; j++ )
-					this.players[teamindex][j].SendLangNotification ( langstr, args );
+					if ( this.players[teamindex][j].exists )
+						this.players[teamindex][j].SendLangNotification ( langstr, args );
 		}
 
 		public void SendAllPlayerChatMessage ( string message, int teamindex = -1 ) {
-			if ( teamindex == -1 )
+			if ( teamindex == -1 ) { 
 				for ( int i = 0; i < this.players.Count; i++ )
 					for ( int j = 0; j < this.players[i].Count; j++ )
-						this.players[i][j].sendChatMessage ( message );
-			else
+						if ( this.players[i][j].exists )
+							this.players[i][j].sendChatMessage ( message );
+			} else
 				for ( int j = 0; j < this.players[teamindex].Count; j++ )
-					this.players[teamindex][j].sendChatMessage ( message );
+					if ( this.players[teamindex][j].exists )
+						this.players[teamindex][j].sendChatMessage ( message );
 		}
 	}
 }
