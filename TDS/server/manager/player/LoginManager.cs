cca40e6a15267238724424c943807042998242c4
emre1702 <emre1702@live.de>
2017-08-29 21:48:48 +0200

    run in other task and THEN join mainmenu

--- TDS/server/manager/player/LoginManager.cs
+++ TDS/server/manager/player/LoginManager.cs
@@ -6,70 +6,74 @@ using System.Security.Cryptography;
 using System;
 using GrandTheftMultiplayer.Shared.Math;
 using GrandTheftMultiplayer.Server.Constant;
+using System.Threading.Tasks;
 
 namespace Manager {
 	static class Login {
 
-		public static void LoginPlayer ( Client player, int uid, string password = "" ) {
-			int adminlvl = 0;
-			int donatorLvl = 0;
-			int playtime = 0;
-			int money = 0;
-			int kills = 0;
-			int assists = 0;
-			int deaths = 0;
-			int damage = 0;
-			bool isvip = false;
-			bool hitsoundon = true;
-			
-			if ( password != "" ) {
-				DataTable result = Database.ExecPreparedResult ( "SELECT * FROM player, playersetting WHERE player.UID = @UID AND player.UID = playersetting.UID", new Dictionary<string, string> { { "@UID", uid.ToString () } } );
-				if ( result.Rows.Count > 0 ) {
-					DataRow row = result.Rows[0];
-					if ( Utility.ConvertToSHA512 ( password ) == row["password"].ToString () ) {
-						player.name = row["name"].ToString();
-						adminlvl = Convert.ToInt32 ( row["adminlvl"] );
-						donatorLvl = Convert.ToInt32 ( row["donatorlvl"] );
-						playtime = Convert.ToInt32 ( row["playtime"] );
-						money = Convert.ToInt32 ( row["money"] );
-						kills = Convert.ToInt32 ( row["kills"] );
-						assists = Convert.ToInt32 ( row["assists"] );
-						deaths = Convert.ToInt32 ( row["deaths"] );
-						damage = Convert.ToInt32 ( row["damage"] );
-						isvip = row["isvip"].ToString () == "1";
-						hitsoundon = row["hitsound"].ToString () == "1";
+		public static async void LoginPlayer ( Client player, int uid, string password = "" ) {
+			await Task.Run ( ( ) => {
+				int adminlvl = 0;
+				int donatorLvl = 0;
+				int playtime = 0;
+				int money = 0;
+				int kills = 0;
+				int assists = 0;
+				int deaths = 0;
+				int damage = 0;
+				bool isvip = false;
+				bool hitsoundon = true;
+
+				if ( password != "" ) {
+					DataTable result = Database.ExecPreparedResult ( "SELECT * FROM player, playersetting WHERE player.UID = @UID AND player.UID = playersetting.UID", new Dictionary<string, string> { { "@UID", uid.ToString () } } );
+					if ( result.Rows.Count > 0 ) {
+						DataRow row = result.Rows[0];
+						if ( Utility.ConvertToSHA512 ( password ) == row["password"].ToString () ) {
+							player.name = row["name"].ToString ();
+							adminlvl = Convert.ToInt32 ( row["adminlvl"] );
+							donatorLvl = Convert.ToInt32 ( row["donatorlvl"] );
+							playtime = Convert.ToInt32 ( row["playtime"] );
+							money = Convert.ToInt32 ( row["money"] );
+							kills = Convert.ToInt32 ( row["kills"] );
+							assists = Convert.ToInt32 ( row["assists"] );
+							deaths = Convert.ToInt32 ( row["deaths"] );
+							damage = Convert.ToInt32 ( row["damage"] );
+							isvip = row["isvip"].ToString () == "1";
+							hitsoundon = row["hitsound"].ToString () == "1";
+						} else {
+							player.SendLangMessage ( "wrong_password" );
+							return;
+						}
 					} else {
-						player.SendLangMessage ( "wrong_password" );
+						player.SendLangMessage ( "account_doesnt_exist" );
 						return;
 					}
-				} else {
-					player.SendLangMessage ( "account_doesnt_exist" );
-					return;
+
 				}
+				player.team = 1;  // Damage canceln lassen;
+				Class.Character character = player.GetChar ();
 
-			}
-			player.team = 1;  // Damage canceln lassen;
-			Class.Character character = player.GetChar ();
+				character.uID = uid;
+				character.adminLvl = adminlvl;
+				character.donatorLvl = donatorLvl;
+				character.playtime = playtime;
+				character.kills = kills;
+				character.assists = assists;
+				character.deaths = deaths;
+				character.damage = damage;
+				character.isVIP = isvip;
+				character.hitsoundOn = hitsoundon;
 
-			character.uID = uid;
-			character.adminLvl = adminlvl;
-			character.donatorLvl = donatorLvl;
-			character.playtime = playtime;
-			character.kills = kills;
-			character.assists = assists;
-			character.deaths = deaths;
-			character.damage = damage;
-			character.isVIP = isvip;
-			character.hitsoundOn = hitsoundon;
+				character.loggedIn = true;
 
-			character.loggedIn = true;
+				player.GiveMoney ( money, character );
 
-			player.GiveMoney ( money, character );
+				if ( adminlvl > 0 )
+					Admin.SetOnline ( player, adminlvl );
 
-			if ( adminlvl > 0 )
-				Admin.SetOnline ( player, adminlvl );
+				API.shared.triggerClientEvent ( player, "registerLoginSuccessful" );
 
-			API.shared.triggerClientEvent ( player, "registerLoginSuccessful" );
+			} );
 
 			MainMenu.Join ( player );
 		}
