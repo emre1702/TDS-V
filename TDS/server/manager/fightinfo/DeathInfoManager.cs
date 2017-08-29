d50d97fc59d390d88e2ba55afa696eacb9e47f0e
emre1702 <emre1702@live.de>
2017-08-29 16:36:16 +0200

    file-name changed, also added killer and weapon

---
+++ TDS/server/manager/fightinfo/DeathInfoManager.cs
@@ -0,0 +1,12 @@
+﻿using GrandTheftMultiplayer.Server.Elements;
+
+namespace Manager {
+
+	static partial class FightInfo {
+
+		public static void DeathInfoSync ( this Class.Lobby lobby, Client player, int team, Client killer, int weapon ) {
+			lobby.SendAllPlayerEvent ( "onClientPlayerDeath", -1, player, team, killer, weapon );
+		}
+
+	}
+}
