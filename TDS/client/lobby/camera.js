9d049e404d61e5061613993c23d9f116748524df
emre1702 <emre1702@live.de>
2017-09-09 04:04:15 +0200

    added camera-fly for cooler round-starts

---
+++ TDS/client/lobby/camera.js
@@ -0,0 +1,22 @@
+"use strict";
+let cameradata = {
+    camera: null,
+    tocamera: null,
+    moving: false,
+    timer: null,
+};
+function loadMapMiddleForCamera(mapmiddle) {
+    let camerapos = new Vector3(mapmiddle.X, mapmiddle.Y, mapmiddle.Z + 70);
+    cameradata.camera = API.createCamera(camerapos, new Vector3(270, 0, 0));
+    API.pointCameraAtPosition(cameradata.camera, camerapos);
+    API.setActiveCamera(cameradata.camera);
+}
+function setCameraGoTowardsPlayer() {
+    cameradata.tocamera = API.createCamera(API.getGameplayCamPos(), API.getGameplayCamRot());
+    API.interpolateCameras(cameradata.camera, cameradata.tocamera, lobbysettings.countdowntime * 1000 * 0.9, true, true);
+}
+function stopCountdownCamera() {
+    cameradata.tocamera = null;
+    cameradata.camera = null;
+    API.setActiveCamera(null);
+}
