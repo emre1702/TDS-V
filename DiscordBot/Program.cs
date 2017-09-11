fbaef1399cd71ae584c18bb8e50350c02844df66
emre1702 <emre1702@live.de>
2017-09-11 03:18:18 +0200

    added DiscordBot (a great bot is incoming)

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace DiscordBot {
+	class Program {
+		static DiscordClient client;
+
+		static void Main ( string[] args ) {
+			MainAsync ( args ).ConfigureAwait ( false ).GetAwaiter ().GetResult ();
+		}
+
+		static async Task MainAsync ( string[] args ) {
+
+			client = new DiscordClient ( new DiscordConfiguration {
+				Token = "MzU2NTc4NTE1NDcyMDg5MDg5.DJdZLg.eGrl3o54bdpyWIexxfnvIw3z_VI",
+				TokenType = TokenType.Bot,
+				AutoReconnect = true
+			} );
+
+			client.MessageCreated += async ( e ) => {
+				if ( e.Message.Content.ToLower ().StartsWith ( "hi" ) )
+					await e.Message.RespondAsync ( "Hello!" );
+			};
+
+			await client.ConnectAsync ();
+			await Task.Delay ( -1 );
+		}
+	}
+}
