62150020cc0b9beac83e6b3aa9c03fc20d031c07
emre1702 <emre1702@live.de>
2017-09-12 02:50:37 +0200

    New Bot with Youtube support

--- DiscordBot/Commands.cs
+++ DiscordBot/Commands.cs
@@ -1,14 +1,109 @@
-﻿using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
 
 namespace DiscordBot {
-	public class Commands {
+	public class UserCommands : ModuleBase {
 
-		[Command("hi")]
-		public async Task GreetPlayer ( CommandContext ctx ) {
-			await ctx.RespondAsync ( $"👋 Hello {ctx.User.Mention}!" );
+		[Command("hi"), Summary("Greets the user"), Alias("hello", "hallo", "greet")]
+		public async Task GreetUser () {
+			await ReplyAsync ( $":wave: Hello {this.Context.User.Mention} :wave:" );
 		}
+	}
+
+	public class MusicCommands : ModuleBase {
+
+		[Command ( "volume" ), Summary ( "Sets the volume of the music." ), Alias ( "setvolume", "vol" )]
+		public async Task SetSoundVolume ( string volume ) {
+			Program.clientsVolume[this.Context.Client] = float.Parse ( volume, CultureInfo.InvariantCulture );
+		}
+
+		[Command("join"), Summary("Joins your channel"), Alias ("come")]
+		public async Task ComeToChannel ( ) {
+			IVoiceChannel channel = ( this.Context.User as IGuildUser )?.VoiceChannel;
+			if ( channel == null ) {
+				await this.Context.Channel.SendMessageAsync ( "You are not in a voice channel!" );
+				return;
+			}
+			IAudioClient audioclient = await channel.ConnectAsync ();
+			Program.clientsAudioClient[this.Context.Client] = audioclient;
+		} 
+
+		private Process CreateStream ( string url, float volume ) {
+			Process currentsong = new Process ();
+
+			string volstr = volume.ToString ().Replace ( ',', '.' );
+			this.Context.Channel.SendMessageAsync ( volstr );
+
+			currentsong.StartInfo = new ProcessStartInfo {
+				FileName = "bash",
+				Arguments = "-c \"/usr/local/bin/youtube-dl -o - " + url+ " | ffmpeg -i pipe:0 -ac 2 -f s16le -ar 48000 -af volume="+ volstr + " pipe:1\"",
+				UseShellExecute = false,
+				RedirectStandardOutput = true,
+				CreateNoWindow = true
+			};
+
+			currentsong.Start ();
+			return currentsong;
+		}
+
+
+		[Command ("play"), Summary("Plays a youtube-link"), Alias ( "yt", "youtube")]
+		public async Task PlayYoutube ( string url ) {
+			IVoiceChannel channel = ( this.Context.User as IVoiceState ).VoiceChannel;
+
+			IAudioClient client;
+
+			if ( Program.clientsAudioClient.ContainsKey ( this.Context.Client ) )
+				client = Program.clientsAudioClient[this.Context.Client];
+			else {
+				client = await channel.ConnectAsync ();
+				Program.clientsAudioClient[this.Context.Client] = client;
+			}
+
+			if ( !Program.clientsVolume.ContainsKey ( this.Context.Client ) )
+				Program.clientsVolume[this.Context.Client] = 0.1f;
+
+			Process ffmpeg = CreateStream ( url, Program.clientsVolume[this.Context.Client] );
+			Stream output = ffmpeg.StandardOutput.BaseStream;
+			AudioOutStream discord = client.CreatePCMStream ( AudioApplication.Music );
+			await output.CopyToAsync ( discord );
+			await Task.Delay ( 10000 );
+			await discord.FlushAsync ();
+		}
+
+		/* [Command ( "stream" ), Summary ( "Plays a stream" ), Alias ( "radio" )]
+		public async Task PlayStream ( string url ) {
+			IVoiceChannel channel = ( this.Context.User as IVoiceState ).VoiceChannel;
+
+			IAudioClient client;
+
+			if ( Program.clientsAudioClient.ContainsKey ( this.Context.Client ) )
+				client = Program.clientsAudioClient[this.Context.Client];
+			else {
+				client = await channel.ConnectAsync ();
+				Program.clientsAudioClient[this.Context.Client] = client;
+			}
+			Process ffmpeg = CreateStream ( url, this.Context.Channel );
+			Stream output = ffmpeg.StandardOutput.BaseStream;
+			AudioOutStream discord = client.CreatePCMStream ( AudioApplication.Music );
+
+			byte[] soundbyte = await GetByteByStream ( output );
+
+			await output.CopyToAsync ( discord );
+			await discord.FlushAsync ();
+		} */
+
+		
+	}
+
+	public class AdminCommands : ModuleBase {
+
 	}
 }
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,37 +1,78 @@
-﻿using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot {
-	class Program {
-		static DiscordClient discord;
-		static CommandsNextModule commands;
+	public partial class Program {
+		public static Dictionary<IDiscordClient, IAudioClient> clientsAudioClient = new Dictionary<IDiscordClient, IAudioClient> ();
+		public static Dictionary<IDiscordClient, float> clientsVolume = new Dictionary<IDiscordClient, float> ();
 
-		static void Main ( string[] args ) {
-			MainAsync ( args ).ConfigureAwait ( false ).GetAwaiter ().GetResult ();
+		private DiscordSocketClient client;
+		private CommandService commands;
+		private IServiceProvider services;
+
+		public static void Main ( string[] args ) {
+			new Program ().MainAsync ().GetAwaiter ().GetResult ();
 		}
 
-		static async Task MainAsync ( string[] args ) {
+		public async Task MainAsync () {
+			this.client = new DiscordSocketClient ();
 
-			discord = new DiscordClient ( new DiscordConfiguration {
-				Token = "MzU2NTc4NTE1NDcyMDg5MDg5.DJdZLg.eGrl3o54bdpyWIexxfnvIw3z_VI",
-				TokenType = TokenType.Bot,
-				AutoReconnect = true,
-				UseInternalLogHandler = true,
-				LogLevel = LogLevel.Debug
+			this.client.Log += this.Log;
+
+			this.commands = new CommandService ( new CommandServiceConfig {
+				DefaultRunMode = RunMode.Async,
+				LogLevel = LogSeverity.Debug
 			} );
 
-			commands = discord.UseCommandsNext ( new CommandsNextConfiguration {
-				StringPrefix = "!",
-				CaseSensitive = false
-			} );
+			this.services = new ServiceCollection ().BuildServiceProvider ();
 
-			commands.RegisterCommands<Commands> ();
+			await InstallCommands ();
 
-			await discord.ConnectAsync ();
+			await this.client.LoginAsync ( TokenType.Bot, "MzU2NTc4NTE1NDcyMDg5MDg5.DJdZLg.eGrl3o54bdpyWIexxfnvIw3z_VI" );
+			await this.client.StartAsync ();
+
 			await Task.Delay ( -1 );
 		}
 
-		
+		private Task Log ( LogMessage msg ) {
+			Console.WriteLine ( msg.ToString () );
+			return Task.CompletedTask;
+		}
+
+		public async Task InstallCommands ( ) {
+			// Hook the MessageReceived Event into our Command Handler
+			client.MessageReceived += this.HandleCommand;
+			// Discover all of the commands in this assembly and load them.
+			await commands.AddModulesAsync ( Assembly.GetEntryAssembly () );
+		}
+
+		public async Task HandleCommand ( SocketMessage messageParam ) {
+			// Don't process the command if it was a System Message
+			SocketUserMessage message = messageParam as SocketUserMessage;
+			if ( message == null )
+				return;
+			// Ignore self when checking commands
+			if ( messageParam.Author.IsBot )
+				return;     
+			// Create a number to track where the prefix ends and the command begins
+			int argPos = 0;
+			// Determine if the message is a command, based on if it starts with '!' or a mention prefix
+			if ( !( message.HasCharPrefix ( '!', ref argPos ) || message.HasMentionPrefix ( client.CurrentUser, ref argPos ) ) )
+				return;
+			// Create a Command Context
+			CommandContext context = new CommandContext ( client, message );
+			// Execute the command. (result does not indicate a return value, 
+			// rather an object stating if the command executed successfully)
+			IResult result = await commands.ExecuteAsync ( context, argPos, this.services );
+			if ( !result.IsSuccess )
+				await context.Channel.SendMessageAsync ( result.ErrorReason );
+		}
 	}
 }
