dbf258d9e11fa0beffe78e92e8b8c6ceb3d470d4
emre1702 <emre1702@live.de>
2017-09-16 00:49:23 +0200

    added killing-spree (right now it will only give hp/armor)

--- TDS/server/class/damagesys/KillingSpree.cs
+++ TDS/server/class/damagesys/KillingSpree.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GrandTheftMultiplayer.Server.Elements;
 using GrandTheftMultiplayer.Shared;
 
 namespace Class {
 	partial class Damagesys {
 
-		private static Dictionary<int, int> spreeReward = new Dictionary<int, int> ();
+		private static Dictionary<int, Tuple<string, int, int>> spreeReward = new Dictionary<int, Tuple<string, int, int>> {
+			{ 3, new Tuple<string, int, int> ( "healtharmor", 30, 0 ) },
+			{ 5, new Tuple<string, int, int> ( "healtharmor", 50, 0 ) },
+			{ 10, new Tuple<string, int, int> ( "healtharmor", 100, 0 ) },
+			{ 15, new Tuple<string, int, int> ( "healtharmor", 100, 0 ) }
+		};
+		public Dictionary<Client, int> playerSpree = new Dictionary<Client, int> ();
 
 		private void CheckKillingSpree ( Client player ) {
-			if ( spreeReward.ContainsKey ( this.playerKills[player] ) ) {
-
+			if ( spreeReward.ContainsKey ( this.playerSpree[player] ) ) {
+				Tuple<string, int, int> reward = spreeReward[this.playerSpree[player]];
+				string rewardtyp = reward.Item1;
+				if ( rewardtyp == "healtharmor" ) {
+					int bonus = reward.Item2;
+					player.GetChar ().lobby.SendAllPlayerLangNotification ( "killing_spree_healtharmor", -1, player.name, this.playerSpree[player].ToString (), bonus.ToString() );
+					if ( player.health + bonus <= 100 ) {
+						player.health += bonus;
+					} else {
+						bonus -= 100 - player.health;
+						player.health = 100;
+						if ( player.armor + bonus <= 100 ) {
+							player.armor += bonus;
+						} else {
+							player.armor = 100;
+						}
+					}
+				}
+				
 			}
+		}
+
+		public void AddToKillingSpree ( Client player ) {
+			if ( !this.playerSpree.ContainsKey ( player ) ) {
+				this.playerSpree[player] = 0;
+			}
+			this.playerSpree[player]++;
 		}
 
 	}
