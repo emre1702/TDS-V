734cb5a5e60d0534d2bb05512c8486b47ec6bf66
emre1702 <emre1702@live.de>
2017-10-03 03:21:36 +0200

    better structure

---
+++ TDS/server/class/lobby/ClientEvent.cs
@@ -0,0 +1,69 @@
+﻿using GrandTheftMultiplayer.Server.Elements;
+
+namespace Class {
+
+	partial class Lobby {
+
+		private static void OnClientEventTrigger ( Client player, string eventName, params dynamic[] args ) {
+			switch ( eventName ) {
+
+				case "joinLobby":
+					if ( lobbysbyindex.ContainsKey ( args[0] ) ) {
+						Lobby lobby = lobbysbyindex[args[0]];
+						lobby.AddPlayer ( player, args[1] );
+					} else {
+						/* player.sendNotification (  lobby doesn't exist ); */
+						player.triggerEvent ( "onClientJoinMainMenu" );
+					}
+					break;
+
+				case "spectateNext":
+					Class.Character character = player.GetChar ();
+					if ( character.lifes == 0 && ( character.lobby.status == "round" || character.team == 0 && character.lobby.status == "countdown" ) ) {
+						if ( character.team == 0 )
+							character.lobby.SpectateAllTeams ( player, args[0] );
+						else
+							character.lobby.SpectateTeammate ( player, args[0] );
+					}
+					break;
+
+				case "onPlayerWasTooLongOutsideMap":
+					Class.Character character2 = player.GetChar ();
+					if ( character2.lobby.isPlayable ) {
+						character2.lobby.KillPlayer ( player, "too_long_outside_map" );
+					}
+					break;
+
+				case "onMapMenuOpen":
+					player.GetChar ().lobby.SendMapsForVoting ( player );
+					break;
+
+				case "onMapVotingRequest":
+					player.GetChar ().lobby.AddMapToVoting ( player, args[0] );
+					break;
+
+				case "onVoteForMap":
+					player.GetChar ().lobby.AddVoteToMap ( player, args[0] );
+					break;
+
+				// BOMB //
+				case "onPlayerStartPlanting":
+					player.GetChar ().lobby.StartBombPlanting ( player );
+					break;
+
+				case "onPlayerStopPlanting":
+					player.GetChar ().lobby.StopBombPlanting ( player );
+					break;
+
+				case "onPlayerStartDefusing":
+					player.GetChar ().lobby.StartBombDefusing ( player );
+					break;
+
+				case "onPlayerStopDefusing":
+					player.GetChar ().lobby.StopBombDefusing ( player );
+					break;
+			}
+		}
+	}
+
+}
