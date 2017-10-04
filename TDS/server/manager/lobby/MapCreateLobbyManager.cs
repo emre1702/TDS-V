2efd06b12b5ea7776238ce4f462efa0913c580d3
emre1702 <emre1702@live.de>
2017-10-04 20:17:47 +0200

    add map-create-lobby

--- TDS/server/manager/lobby/MapCreateLobbyManager.cs
+++ TDS/server/manager/lobby/MapCreateLobbyManager.cs
@@ -8,8 +8,11 @@ namespace Manager {
 		public static Class.Lobby lobby;
 
 		public static void Create ( ) {
-			lobby = new Class.Lobby ( "mapcreate", 3, false, false );
+			lobby = new Class.Lobby ( "mapcreate", 3, false, false, true );
 			lobby.deleteWhenEmpty = false;
+			lobby.isOfficial = true;
+			lobby.AddSpawnPoint ( new Vector3 ( 8.699318, -2.050943, 70.29607 ), new Vector3 ( 0, 0, 65.79314 ) );
+			lobby.Start ();
 		}
 
 		public static void Join ( Client player ) {
