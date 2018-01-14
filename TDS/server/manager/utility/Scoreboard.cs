39483b10fb4898ce2094ca6424559633e36ace5b
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-01-14 04:42:19 +0100

    Improve Scoreboard serversided

--- TDS/server/manager/utility/Scoreboard.cs
+++ TDS/server/manager/utility/Scoreboard.cs
@@ -1,9 +1,20 @@
 namespace TDS.server.manager.utility {
-
-	using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 	using extend;
 	using GTANetworkAPI;
 	using instance.player;
+    using Newtonsoft.Json;
+
+    [Serializable]
+    class ScoreboardPlayerData {
+        public string Name;
+        public string PlayTime;
+        public int Kills;
+        public int Assists;
+        public int Deaths;
+        public string TeamOrLobby;   
+    }
 
 	class Scoreboard : Script {
 
@@ -21,12 +32,8 @@ namespace TDS.server.manager.utility {
 			if ( eventName == "onClientRequestPlayerListDatas" ) {
                 List<Client> players = NAPI.Pools.GetAllPlayers ();
 
-				List<string> nameList = new List<string> ();
-				List<string> playtimeList = new List<string> ();
-				List<string> killsList = new List<string> ();
-				List<string> assistsList = new List<string> ();
-				List<string> deathsList = new List<string> ();
-				List<string> teamorlobbyList = new List<string> (); // lobby in mainmenu, team rest
+                List<ScoreboardPlayerData> playersdata = new List<ScoreboardPlayerData> ();
+
 				List<string> otherLobbyNames = new List<string> ();
 				List<int> otherLobbyAmounts = new List<int> ();
 
@@ -39,22 +46,23 @@ namespace TDS.server.manager.utility {
 						Character character = target.GetChar ();
 						int lobbyID = character.Lobby.ID;
 						if ( lobbyID == ownLobbyID /* he is same lobby */ || ownLobbyID == 0 /* you are in mainmenu */ ) {
-							nameList.Add ( target.Name );
+                            ScoreboardPlayerData data = new ScoreboardPlayerData ();
+                            data.Name = target.Name;
 
 							if ( character.LoggedIn ) {
 								// character stats //
-								playtimeList.Add ( GetHoursOpticByMinutes ( character.Playtime ) );
-								killsList.Add ( character.Kills.ToString () );
-								assistsList.Add ( character.Assists.ToString () );
-								deathsList.Add ( character.Deaths.ToString () );
-								teamorlobbyList.Add ( ownLobbyID == 0 ? character.Lobby.Name : character.Lobby.GetTeamName ( character.Team ) );
+								data.PlayTime = GetHoursOpticByMinutes ( character.Playtime );
+								data.Kills = (int) character.Kills;
+								data.Assists = (int) character.Assists;
+								data.Deaths = (int) character.Deaths;
+								data.TeamOrLobby = ownLobbyID == 0 ? character.Lobby.Name : character.Lobby.GetTeamName ( character.Team );
 							} else {
-								// default status //
-								playtimeList.Add ( "-" );
-								killsList.Add ( "-" );
-								assistsList.Add ( "-" );
-								deathsList.Add ( "-" );
-								teamorlobbyList.Add ( player.GetLang ( "connecting" ) );
+                                // default status //
+                                data.PlayTime = "-";
+                                data.Kills = -1;
+                                data.Assists = -1;
+                                data.Deaths = -1;
+                                data.TeamOrLobby = player.GetLang ( "connecting" );
 							}
 						} else {
 							if ( doneLobbyIDs.ContainsKey ( lobbyID ) ) {
@@ -69,9 +77,9 @@ namespace TDS.server.manager.utility {
 				}
 
 				if ( ownLobbyID != 0 )
-                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatas", nameList, playtimeList, killsList, assistsList, deathsList, teamorlobbyList, otherLobbyNames, otherLobbyAmounts );
+                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatas", JsonConvert.SerializeObject ( playersdata ), otherLobbyNames, otherLobbyAmounts );
 				else
-                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatasMainmenu", nameList, playtimeList, killsList, assistsList, deathsList, teamorlobbyList );
+                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatasMainmenu", JsonConvert.SerializeObject ( playersdata ) );
 			}
 		}
 	}
