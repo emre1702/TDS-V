95aaed89b582fdeef314d6910232e8d330202e6c
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-01-15 19:56:20 +0100

    Fix & improve Scoreboard

--- TDS/server/manager/utility/Scoreboard.cs
+++ TDS/server/manager/utility/Scoreboard.cs
@@ -9,11 +9,17 @@ namespace TDS.server.manager.utility {
     [Serializable]
     class ScoreboardPlayerData {
         public string Name;
-        public string PlayTime;
-        public int Kills;
-        public int Assists;
-        public int Deaths;
+        public string PlayTime = "-";
+        public int Kills = -1;
+        public int Assists = -1;
+        public int Deaths = -1;
         public string TeamOrLobby;   
+    }
+
+    [Serializable]
+    class ScoreboardOtherLobbyData {
+        public string Name;
+        public int Amount = 1;
     }
 
 	class Scoreboard : Script {
@@ -33,53 +39,47 @@ namespace TDS.server.manager.utility {
                 List<Client> players = NAPI.Pools.GetAllPlayers ();
 
                 List<ScoreboardPlayerData> playersdata = new List<ScoreboardPlayerData> ();
-
-				List<string> otherLobbyNames = new List<string> ();
-				List<int> otherLobbyAmounts = new List<int> ();
-
+                List<ScoreboardOtherLobbyData> otherlobbydata = new List<ScoreboardOtherLobbyData> ();
 				Dictionary<int, int> doneLobbyIDs = new Dictionary<int, int> ();
+
 				Character playerCharacter = player.GetChar ();
 				int ownLobbyID = playerCharacter.Lobby.ID;
 
 				foreach ( Client target in players ) {
-					if ( target.Exists ) {
-						Character character = target.GetChar ();
-						int lobbyID = character.Lobby.ID;
-						if ( lobbyID == ownLobbyID /* he is same lobby */ || ownLobbyID == 0 /* you are in mainmenu */ ) {
-                            ScoreboardPlayerData data = new ScoreboardPlayerData ();
-                            data.Name = target.Name;
+					Character character = target.GetChar ();
+					int lobbyID = character.Lobby.ID;
+					if ( lobbyID == ownLobbyID /* he is same lobby */ || ownLobbyID == 0 /* you are in mainmenu */ ) {
+                        ScoreboardPlayerData data = new ScoreboardPlayerData ();
+                        playersdata.Add ( data );
+                        data.Name = target.Name;
 
-							if ( character.LoggedIn ) {
-								// character stats //
-								data.PlayTime = GetHoursOpticByMinutes ( character.Playtime );
-								data.Kills = (int) character.Kills;
-								data.Assists = (int) character.Assists;
-								data.Deaths = (int) character.Deaths;
-								data.TeamOrLobby = ownLobbyID == 0 ? character.Lobby.Name : character.Lobby.GetTeamName ( character.Team );
-							} else {
-                                // default status //
-                                data.PlayTime = "-";
-                                data.Kills = -1;
-                                data.Assists = -1;
-                                data.Deaths = -1;
-                                data.TeamOrLobby = player.GetLang ( "connecting" );
-							}
+						if ( character.LoggedIn ) {
+							// character stats //
+							data.PlayTime = GetHoursOpticByMinutes ( character.Playtime );
+							data.Kills = (int) character.Kills;
+							data.Assists = (int) character.Assists;
+							data.Deaths = (int) character.Deaths;
+							data.TeamOrLobby = ownLobbyID == 0 ? character.Lobby.Name : character.Lobby.GetTeamName ( character.Team );
 						} else {
-							if ( doneLobbyIDs.ContainsKey ( lobbyID ) ) {
-								otherLobbyAmounts[doneLobbyIDs[lobbyID]]++;
-							} else {
-								otherLobbyNames.Add ( character.Lobby.Name );
-								otherLobbyAmounts.Add ( 1 );
-								doneLobbyIDs[lobbyID] = otherLobbyNames.Count - 1;
-							}
+                            // default status //
+                            data.TeamOrLobby = player.GetLang ( "connecting" );
+						}
+					} else {
+						if ( doneLobbyIDs.ContainsKey ( lobbyID ) ) {
+                            ++otherlobbydata[doneLobbyIDs[lobbyID]].Amount;
+						} else {
+                            ScoreboardOtherLobbyData lobbydata = new ScoreboardOtherLobbyData {
+                                Name = character.Lobby.Name
+                            };
+                            otherlobbydata.Add ( lobbydata );
+                            doneLobbyIDs[lobbyID] = otherlobbydata.Count - 1;
 						}
 					}
 				}
-
 				if ( ownLobbyID != 0 )
-                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatas", JsonConvert.SerializeObject ( playersdata ), otherLobbyNames, otherLobbyAmounts );
+                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatas", JsonConvert.SerializeObject ( playersdata ), JsonConvert.SerializeObject ( otherlobbydata ) );
 				else
-                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatasMainmenu", JsonConvert.SerializeObject ( playersdata ) );
+                    NAPI.ClientEvent.TriggerClientEvent ( player, "giveRequestedPlayerListDatas", JsonConvert.SerializeObject ( playersdata ) );
 			}
 		}
 	}
