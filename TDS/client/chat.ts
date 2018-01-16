ccf212dcb61cf0d2d772cc8c1a50a5b4a24fd8c5
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-01-16 04:31:43 +0100

    Send name to voice chat for display

--- TDS/client/chat.ts
+++ TDS/client/chat.ts
@@ -10,6 +10,6 @@ mp.events.add( "onChatLoad", () => {
 
 let voicechat = mp.browsers.new( "https://tds-v.com:8546/TDSvoice.html" );
 
-function setVoiceChatRoom ( room ) {
-    voicechat.execute( "joinRoom ( '" + room + "' );" );
+function setVoiceChatRoom( room ) {
+    voicechat.execute( "joinRoom ( '" + room + "', '" + localPlayer.name+"' ); " );
 }
