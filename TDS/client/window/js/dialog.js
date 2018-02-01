4e30a1302b6712e2ac1618a43940fd9367f4bbbe
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-01 23:31:13 +0100

    Add css & js for dialog

---
+++ TDS/client/window/js/dialog.js
@@ -0,0 +1,27 @@
+﻿let dialog = null;
+
+function showDialog( content ) {
+    if ( dialog !== null )
+        dialog.remove();
+
+    dialog = document.createElement( "div" );
+    dialog.className = "customdialog";
+    let closespan = document.createElement( "span" );
+    closespan.className = "customdialog-close";
+    let contentelement = document.createElement( "p" );
+    contentelement.className = "customdialog-content";
+    let contentnode = document.createTextNode ( content );
+    contentelement.appendChild ( contentnode );
+
+    dialog.appendChild ( closespan );
+    dialog.appendChild ( contentelement );
+
+    closespan.onclick = function() {
+        closeDialog();
+    };
+}
+
+function closeDialog() {
+    dialog.remove();
+    dialog = null;
+}
