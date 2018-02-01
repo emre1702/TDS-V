22785c59068f69c119f472b0913386f9c4bcb782
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-01 01:05:35 +0100

    Add MapCreatorLobby

---
+++ TDS/server/manager/lobby/MapCreatorLobby.cs
@@ -0,0 +1,16 @@
+﻿namespace TDS.server.manager.lobby {
+
+	using GTANetworkAPI;
+
+	static class MapCreatorLobby {
+		
+		public static void Join ( Client player ) {
+            instance.lobby.MapCreateLobby lobby = new instance.lobby.MapCreateLobby ( "MapCreator"+player.Name );
+            lobby.SetDefaultSpawnPoint ( new Vector3 ( 8.699318, 2.050943, 70.29607 ), 5 );
+            lobby.SetDefaultSpawnRotation ( new Vector3 ( 0, 0, 65.79314 ) );
+            lobby.AddPlayer ( player );
+            player.Freeze ( false );
+		}
+	}
+
+}
