d42d92a514a4b15d7e601a3cc4cab4c08db80532
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-05 12:13:30 +0100

    Add map-creator

--- TDS/client/window/mapcreator/mapcreator.js
+++ TDS/client/window/mapcreator/mapcreator.js
@@ -1,9 +1,12 @@
 let mapcreatormenu = $( "#mapcreatormenu" );
-let mapcreatortabs = [$( "#general_tabcontent" ), $( "#description_tabcontent" ), $( "#team_spawns_tabcontent" ), $( "#map_limit_tabcontent" ), $( "#map_points_tabcontent" )];
-let mapcreatortables = [$( "#team_spawn_table" ), $( "#map_limit_table" )];
+let mapcreatortabs = [$( "#general_tabcontent" ), $( "#description_tabcontent" ), $( "#team_spawns_tabcontent" ), $( "#map_limit_tabcontent" ), $( "#map_points_tabcontent" ), $( "#last_tabcontent" )];
+let mapcreatortables = [$( "#team_spawns_table" ), $( "#map_limit_table" )];
 let teamnumberteamspawns = $( "#team_number_teamspawns" );
 let selectedrow = null;
 let addpositionfor = 0;
+let mapNames = null;
+let langdata;
+let sendbuttondisabled = true;
 
 setElementDraggable( mapcreatormenu[0] );
 
@@ -13,40 +16,48 @@ function openMapCreatorMenu( index ) {
 
     mapcreatortabs[index].addClass( "active" ).show();
 
-    if ( selectedrow !== null )
+    if ( selectedrow !== null ) {
         selectedrow.removeAttr( "bgcolor" );
-    selectedrow = null;
+        selectedrow = null;
+    }
 }
 openMapCreatorMenu( 0 );
 
-$( "tr:not(:first-child)" ).click( function () {
+function clickOnRow () {
     if ( selectedrow !== null )
         selectedrow.removeAttr( "bgcolor" );
 
     if ( selectedrow === null || selectedrow[0] !== $( this )[0] )
         selectedrow = $( this ).attr( "bgcolor", "#04074eC7" );
-} );
+    else
+        selectedrow = null;
+}
 
-function AddCurrentPosition( number ) {
+function addCurrentPosition( number ) {
     addpositionfor = number;
     mp.trigger( "requestCurrentPositionForMapCreator" );
 }
 
-function GotoPosition( number ) {
-    if ( number != 2 && selectedrow === null )
+function gotoPosition( number ) {
+    if ( number !== 2 && selectedrow === null )
         return;
 
     let x, y, z;
+    let rot = "";
+    let row;
     switch ( number ) {
         case 0:
-            x = selectedrow.find( "td:eq(1)" ).text();
-            y = selectedrow.find( "td:eq(2)" ).text();
-            z = selectedrow.find( "td:eq(3)" ).text();
+            row = selectedrow.find( "td" );
+            x = row.eq( 1 ).text();
+            y = row.eq( 2 ).text();
+            z = row.eq( 3 ).text();
+            rot = row.eq( 4 ).text();
             break;
         case 1:
-            x = selectedrow.find( "td:eq(0)" ).text();
-            y = selectedrow.find( "td:eq(1)" ).text();
-            z = selectedrow.find( "td:eq(2)" ).text();
+            row = selectedrow.find( "td" );
+            x = row.eq( 0 ).text();
+            y = row.eq( 1 ).text();
+            z = row.eq( 2 ).text();
             break;
         case 2:
             x = $( "#map_center_x" ).text().substr( 3 );
@@ -54,23 +65,27 @@ function GotoPosition( number ) {
             z = $( "#map_center_z" ).text().substr( 3 );
             break;
     }
-    mp.trigger( "gotoPositionByMapCreator", Number.parseFloat( x ), Number.parseFloat( y ), Number.parseFloat( z ) );
+    mp.trigger( "gotoPositionByMapCreator", parseFloat( x ), parseFloat( y ), parseFloat( z ), parseFloat ( rot ) );
 }
 
-function RemovePosition() {
-    if ( selectedrow !== null )
+function removePosition() {
+    if ( selectedrow !== null ) {
         selectedrow.remove();
+        disableSendButton();
+    }
 }
 
-function LoadPositionFromClient( x, y, z ) {
+function loadPositionFromClient( x, y, z, rot ) {
     let row;
     switch ( addpositionfor ) {
         case 0:
-            row = $( "<tr><td>" + teamnumberteamspawns.val() + "</td><td>" + x + "</td><td>" + y + "</td><td>+" + z + "</td></tr>" );
+            row = $( "<tr><td>" + teamnumberteamspawns.val() + "</td><td>" + x + "</td><td>" + y + "</td><td>+" + z + "</td><td>" + rot + "</td></tr>" );
+            row.click( clickOnRow );
             mapcreatortables[addpositionfor].append( row );
             break;
         case 1:
             row = $( "<tr><td>" + x + "</td><td>" + y + "</td><td>+" + z + "</td></tr>" );
+            row.click( clickOnRow );
             mapcreatortables[addpositionfor].append( row );
             break;
         case 2:
@@ -79,7 +94,122 @@ function LoadPositionFromClient( x, y, z ) {
             $( "#map_center_z" ).text( "Z: "+z );
             break;
     }
-    
+    disableSendButton();
+}
+
+function loadLanguage( lang ) {
+    langdata = JSON.parse( lang );
+    $( "[data-lang]" ).each( function () {
+        $( this ).html( langdata[$( this ).attr( "data-lang" )] );
+    } );
+}
+
+function sendMap() {
+    let obj = {
+        Name: $( "#map_name_input" ).val(),
+        Type: typeof $( "#normal_radio_switch" ).attr( "checked" ) !== "undefined" ? "normal" : "bomb",
+        Descriptions: {
+            English: $( "#english_description" ).attr( "placeholder" ),
+            German: $( "#german_description" ).attr( "placeholder" )
+        },
+        MinPlayers: parseInt( $( "#min_players" ).val() ),
+        MaxPlayers: parseInt( $( "#max_players" ).val() ),
+        MapSpawns: [],
+        MapLimitPositions: [],
+        MapCenter: {}
+    };
+
+    let english = $( "#english_description" ).val();
+    if ( english !== "" )
+        obj.Descriptions.English = english;
+
+    let german = $( "#german_description" ).val();
+    if ( german !== "" )
+        obj.Descriptions.German = german;
+
+    $( "#team_spawns_table tr:not(:first-child)" ).each(function() {
+        let row = $( this ).find( "td" );
+        let team = parseFloat( row.eq( 0 ).text() );
+        let x = parseFloat( row.eq( 1 ).text() );
+        let y = parseFloat( row.eq( 2 ).text() );
+        let z = parseFloat( row.eq( 3 ).text() );
+        let rot = parseFloat( row.eq( 4 ).text() );
+        obj.MapSpawns.push( { Team: team, X: x, Y: y, Z: z, Rot: rot } );
+    } );
+
+    $( "#map_limit_table tr:not(:first-child)" ).each( function() {
+        let row = $( this ).find( "td" );
+        let x = parseFloat( row.eq( 0 ).text() );
+        let y = parseFloat( row.eq( 1 ).text() );
+        let z = parseFloat( row.eq( 2 ).text() );
+        obj.MapLimitPositions.push( { X: x, Y: y, Z: z } );
+    } );
+
+    obj.MapCenter.X = parseFloat( $( "#map_center_x" ).text().substr( 3 ) );
+    obj.MapCenter.X = isNaN( obj.MapCenter.X ) ? -9999 : obj.MapCenter.X;
+    obj.MapCenter.Y = parseFloat( $( "#map_center_y" ).text().substr( 3 ) );
+    obj.MapCenter.Y = isNaN( obj.MapCenter.Y ) ? -9999 : obj.MapCenter.Y;
+    obj.MapCenter.Z = parseFloat( $( "#map_center_z" ).text().substr( 3 ) );
+    obj.MapCenter.Z = isNaN( obj.MapCenter.Z ) ? -9999 : obj.MapCenter.Z;
+
+    mp.trigger( "sendMapFromCreator", JSON.stringify( obj ) );
+}
+
+function disableSendButton() {
+    if ( !sendbuttondisabled ) {
+        $( "#send_map_button" ).attr( "disabled", "" );
+        sendbuttondisabled = true;
+    }
+
+}
+
+function checkMap() {
+    let name = $( "#map_name_input" ).val();
+    if ( name.length < 6 ) {
+        showDialog( langdata["map_name_atleast"].replace( "{1}", "6" ) );
+        return false;
+    }
+
+    let minplayers = parseInt( $( "#min_players" ).val() );
+    if ( isNaN( minplayers ) || minplayers < 0 || minplayers > 99 ) {
+        showDialog( langdata["map_min_players_min_max"].replace( "{1}", "0" ).replace( "{2}", "99" ) );
+        return false;
+    }
+
+    let maxplayers = parseInt( $( "#max_players" ).val() );
+    if ( isNaN( maxplayers ) || maxplayers < 2 || maxplayers > 999 ) {
+        showDialog( langdata["map_max_players_min_max"].replace( "{1}", "2" ).replace( "{2}", "999" ) );
+        return false;
+    } 
+
+    let mapspawnrows = $( "#team_spawns_table tr" );
+    if ( mapspawnrows.length - 1 < 4 ) {      // -1 because of header
+        showDialog( langdata["map_team_spawns_min_per_team"].replace( "{1}", "4" ) );
+        return false;
+    }
+
+    let maplimitrows = $( "#map_limit_table tr" );
+    if ( maplimitrows.length - 1 !== 0 && ( maplimitrows.length - 1 < 3 || maplimitrows.length - 1 > 20 ) ) {
+        showDialog( langdata["map_limit_min_max"].replace( "{1}", "3" ).replace ( "{2}", 20 ) );
+        return false;
+    }
+
+    sendbuttondisabled = false;
+    $( "#send_map_button" ).removeAttr( "disabled" );
+    return true;
+}
+
+function checkMapName() {
+    mp.trigger( "checkMapName", $("#map_name_input").val() );
+}
+
+function loadResultOfMapNameCheck( alreadyinuse ) {
+    if ( !alreadyinuse )
+        checkMap();
+    else {
+        alert( langdata['map_name_already_used'] );
+        showDialog( langdata["map_name_already_used"] );
+    }
 }
 
 $( document ).keyup( function ( e ) {
