a6519ea2b13b778d1d4d2a7dc6939a9f0c2dc317
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-12 04:57:05 +0100

    Prepare getting new maps list

--- TDS/server/manager/map/MapCreator.cs
+++ TDS/server/manager/map/MapCreator.cs
@@ -1,7 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using GTANetworkAPI;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using TDS.server.extend;
 using TDS.server.instance.map;
 using TDS.server.manager.logs;
 using TDS.server.manager.utility;
@@ -59,6 +62,43 @@ namespace TDS.server.manager.map {
 
         public static bool DoesMapNameExist ( string mapname ) {
             return MapPathByName.ContainsKey ( mapname.ToLower () );
+        }
+
+        private static List<string> GetAllNewMapFileNames ( ) {
+            List<string> files = new List<string> ();
+            string[] subdirectories = Directory.GetDirectories ( newMapsPath );
+            foreach ( string directory in subdirectories ) {
+                string uid = Path.GetDirectoryName ( directory );
+                string[] filesinpath = Directory.GetFiles ( directory );
+                foreach ( string file in filesinpath ) {
+                    string filename = Path.GetFileNameWithoutExtension ( file );
+                    files.Add ( uid + "/" + filename );
+                }
+            }
+            return files;
+        }
+
+        private static List<string> GetOwnNewMapFileNames ( uint uid ) {
+            List<string> files = new List<string> ();
+            if ( Directory.Exists ( newMapsPath + uid + "/" ) ) {
+                string[] filesinpath = Directory.GetFiles ( newMapsPath + uid + "/" );
+                foreach ( string file in filesinpath ) { 
+                    string filename = Path.GetFileNameWithoutExtension ( file );
+                    files.Add ( uid + "/" + filename );
+                }
+            }
+            return files;
+        } 
+
+        public static void RequestNewMapsList ( Client player, bool requestall = false ) {
+            uint uid = player.GetChar ().UID;
+            List<string> filenames; 
+            if ( requestall )
+                filenames = GetAllNewMapFileNames ();
+            else
+                filenames = GetOwnNewMapFileNames ( uid );
+            NAPI.ClientEvent.TriggerClientEvent ( player, "requestNewMapsList", JsonConvert.SerializeObject ( filenames ) );
+
         }
     }
 }
