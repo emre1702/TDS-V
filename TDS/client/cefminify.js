6a7afe615eb820d5acf15b9f1a4f7b6c3a18320b
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-13 14:30:55 +0100

    Minify standalone files and css, too

--- TDS/client/cefminify.js
+++ TDS/client/cefminify.js
@@ -6,20 +6,27 @@ var path = require( "path" );
 var htmlminify = require( "html-minifier" ).minify;
 let toes5 = require( "babel-core" ).transform;
 let uglify = require( "uglify-js" );
+let cleancssrequire = require( "clean-css" );
 
 let files = [
-    { path: "window/choice/", html: "choice.html", js: ["choice.js"], reserved: ["setLobbyChoiceLanguage"] },
-    { path: "window/main/", html: "main.html", js: ["main.js", "mapmanager.js", "chat.js"], 
+    { path: "window/choice/", html: "choice.html", js: ["choice.js"], css: ["window/choice/choice.css"], reserved: ["setLobbyChoiceLanguage"] },
+    { path: "window/main/", html: "main.html", js: ["main.js", "mapmanager.js", "chat.js"], css: ["window/main/chat.css", "window/main/main.css", "window/main/mapmanager.css"],
         reserved: ["setMoney", "playSound", "showBloodscreen", "addKillMessage", "alert", "openMapMenu", "closeMapMenu", "addVoteToMapVoting", "loadMapVotings", "clearMapVotings", "loadFavouriteMaps",
             "toggleCanVoteForMapWithNumpad", "loadOrderNames", "chatAPI", "loadUserName" ]
     },
-    { path: "window/registerlogin/", html: "registerlogin.html", js: ["registerlogin.js"], reserved: ["loadLanguage", "setLoginPanelData"] },
-    {
-        path: "window/mapcreator/", html: "mapcreator.html", js: ["mapcreator.js"],
+    { path: "window/registerlogin/", html: "registerlogin.html", js: ["registerlogin.js"], css: [], reserved: ["loadLanguage", "setLoginPanelData"] },
+    { path: "window/mapcreator/", html: "mapcreator.html", js: ["mapcreator.js"], css: ["window/mapcreator/mapcreator.css"],
         reserved: ["openMapCreatorMenu", "gotoPosition", "addCurrentPosition", "removePosition", "loadPositionFromClient", "loadLanguage", "sendMap", "checkMapName", "loadResultOfMapNameCheck",
         "toggleBombContents" ]
     }
 ];
+let standalonejsfiles = [
+    { path: "window/js/", js: ["copyclipboard.js", "dialog.js", "jqueryui-autocomplete.js", "draggable.js"],
+        reserved: [ ["copyTextToClipboard"], ["showDialog", "closeDialog"], [], ["setElementDraggable"] ]
+    }
+];
+let standalonecssfiles = [{ path: "window/css/", css: ["dialog.css", "scrollbar.css", "style.css", "toggleswitch.css"] }];
+
 let minifysettings = {
     compress: {
         sequences: true,
@@ -66,6 +73,11 @@ let htmlminifysettings = {
     removeScriptTypeAttributes: true,
     removeStyleLinkTypeAttributes: true
 };
+let cssminifysettings = {
+    level: 2
+};
+
+let cleancss = new cleancssrequire( cssminifysettings );
 
 for ( let i = 0; i < files.length; ++i ) {
     let javascript = "";
@@ -76,10 +88,59 @@ for ( let i = 0; i < files.length; ++i ) {
     minifysettings.mangle.reserved = files[i].reserved;
     javascript = uglify.minify( javascript, minifysettings ).code;
     fs.writeFile( path.resolve( files[i].path, "index.js" ), javascript, ( error ) => {
-        let html = fs.readFileSync( path.resolve( files[i].path, files[i].html ), "utf8" );
-        html = htmlminify( html, htmlminifysettings );
-        fs.writeFileSync( path.resolve( files[i].path, "index.html" ), html, ( error ) => {
+        if ( error )
             console.log( error );
-        } );
+        else {
+            let html = fs.readFileSync( path.resolve( files[i].path, files[i].html ), "utf8" );
+            html = htmlminify( html, htmlminifysettings );
+            fs.writeFile( path.resolve( files[i].path, "index.html" ), html, ( error ) => {
+                if ( error )
+                    console.log( error );
+                else {
+                    if ( "css" in files[i] && files[i].css.length > 0 ) {
+                        cleancss.minify( files[i].css, ( error, minified ) => { 
+                            if ( error )
+                                console.log( error );
+                            else 
+                                fs.writeFile( path.resolve( files[i].path, "index.css" ), minified.styles, ( error ) => {
+                                    if ( error )
+                                        console.log( error );
+                                } );
+                        } );
+                    }
+                }
+            } );
+        }
     } );
+}
+
+for ( let i = 0; i < standalonejsfiles.length; ++i ) {
+    for ( let j = 0; j < standalonejsfiles[i].js.length; ++j ) {
+        let javascript = fs.readFileSync( path.resolve( standalonejsfiles[i].path, standalonejsfiles[i].js[j] ) );
+        javascript = toes5( javascript, toes5settings ).code;
+        minifysettings.mangle.reserved = standalonejsfiles[i].reserved[j];
+        javascript = uglify.minify( javascript, minifysettings ).code;
+        let newfilename = standalonejsfiles[i].js[j].split( "." )[0] + ".min.js";
+        fs.writeFile( path.resolve( standalonejsfiles[i].path, newfilename ), javascript, ( error ) => {
+            if ( error )
+                console.log( error );
+        });
+    }
+}
+
+for ( let i = 0; i < standalonecssfiles.length; ++i ) {
+    for ( let j = 0; j < standalonecssfiles[i].css.length; ++j ) {
+        let css = cleancss.minify( [standalonecssfiles[i].path + standalonecssfiles[i].css[j]], ( error, minified ) => {
+            if ( error )
+                console.log( error );
+            else {
+                let newfilename = standalonecssfiles[i].css[j].split( "." )[0] + ".min.css";
+                fs.writeFile( path.resolve( standalonecssfiles[i].path, newfilename ), minified.styles, ( error ) => {
+                    if ( error )
+                        console.log( error );
+                });
+            }
+        } );
+        
+    }
 }
