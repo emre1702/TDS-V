85e35786cec9ef68369b7ebe6e9f153a239ed176
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-13 19:41:10 +0100

    Add GangLobby (will work at it later)

---
+++ TDS/server/instance/lobby/ganglobby/GangLobby.cs
@@ -0,0 +1,10 @@
+﻿namespace TDS.server.instance.lobby {
+
+    public partial class GangLobby : FightLobby {
+
+        public GangLobby ( string name, int id = -1 ) : base ( name, id ) {
+
+        }
+
+    }
+}
---
+++ TDS/server/instance/lobby/ganglobby/Player.cs
@@ -0,0 +1,12 @@
+﻿using GTANetworkAPI;
+
+namespace TDS.server.instance.lobby {
+    partial class GangLobby {
+
+        public override void AddPlayer ( Client player, bool spectator = false ) {
+            base.AddPlayer ( player, spectator );
+
+            player.Freeze ( false );
+        }
+    }
+}
