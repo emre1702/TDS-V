da9f792490b5d8d942722a1773a67e9611084f6f
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-02-23 14:21:05 +0100

    No need for public

--- TDS/server/instance/lobby/ganglobby/GangLobby.cs
+++ TDS/server/instance/lobby/ganglobby/GangLobby.cs
@@ -1,6 +1,6 @@
 ﻿namespace TDS.server.instance.lobby {
 
-    public partial class GangLobby : FightLobby {
+    partial class GangLobby : FightLobby {
 
         public GangLobby ( string name, int id = -1 ) : base ( name, id ) {
 
