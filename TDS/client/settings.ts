bbfb719776fdea15e5c69eb0e8907683e91f1eab
Emre Kara <emre1702@mail.uni-paderborn.de>
2018-03-01 16:29:58 +0100

    Add settings, load languages, add hitsound setting

--- TDS/client/settings.ts
+++ TDS/client/settings.ts
@@ -18,3 +18,41 @@ function changeSetting ( index, value ) {
 	API.setSetting( "TDS_settings", JSON.stringify( settings ) );
 }
 */
+
+var settingsdata = {
+    language: "ENGLISH",
+    hitsound: true
+}
+
+mp.events.add( "onPlayerSettingChange", ( setting, value ) => {
+    switch ( setting as PlayerSetting ) {
+        case PlayerSetting.LANGUAGE:
+            settingsdata.language = value;
+            mp.storage.data.language = value;
+            callRemoteCooldown( "onPlayerLanguageChange", value );
+            loadOrderNamesInBrowser( JSON.stringify( getLang( "orders" ) ) );
+            break;
+
+        case PlayerSetting.HITSOUND:
+            mp.storage.data.hitsound = value;
+            break;
+    } 
+} );
+
+
+function loadSettings() {
+    let savedlang = mp.storage.data.language;
+    let savedhitsound = mp.storage.data.hitsound;
+
+    if ( typeof savedlang !== "undefined" )
+        settingsdata.language = savedlang;
+    else {
+        let langnumber = mp.game.invoke( "E7A981467BC975BA", 0 );
+        if ( langnumber == 2 )
+            settingsdata.language = "" + Language.GERMAN;
+    }
+
+    if ( typeof savedhitsound !== "undefined" )
+        settingsdata.hitsound = savedhitsound;
+
+}
