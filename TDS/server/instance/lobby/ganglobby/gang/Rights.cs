ce4090b4d5c83669363c4ecdac1729c3a411378d
Emre Kara <emre1702@live.de>
2018-03-07 03:00:57 +0100

    Add rights + IsAllowedTo + IsOwner + IsAtleastRank

--- TDS/server/instance/lobby/ganglobby/gang/Rights.cs
+++ TDS/server/instance/lobby/ganglobby/gang/Rights.cs
@@ -1,12 +1,41 @@
-﻿using TDS.server.enums;
+﻿using System.Collections.Generic;
+using TDS.server.enums;
 using TDS.server.instance.player;
+using TDS.server.manager.logs;
 
 namespace TDS.server.instance.lobby.ganglobby {
 
     partial class Gang {
+
+        private Dictionary<GangActivity, uint> allowedAtRank = new Dictionary<GangActivity, uint>
+        {
+            { GangActivity.INVITE, 2 },
+            { GangActivity.UNINVITE, 2 },
+            { GangActivity.RANKUP, 2 },
+            { GangActivity.RANKDOWN, 2 },
+            { GangActivity.CHANGE_RANKNAMES, 2 },
+            { GangActivity.CHANGE_RANKCOLORS, 2 }
+        };
         
         public bool IsAllowedTo ( Character character, GangActivity activity ) {
-            return true;
+            if ( IsOwner ( character ) )
+                return true;
+
+            if ( allowedAtRank.ContainsKey ( activity ) )
+                return IsAtleastRank ( character, allowedAtRank[activity] );
+
+            Log.Error ( $"GangActivity {activity.ToString ()} is not included in allowedAtRank!", "GANG" );
+            return false;
+        }
+
+        public bool IsOwner ( Character character )
+        {
+            return character.UID == owneruid;
+        }
+
+        public bool IsAtleastRank ( Character character, uint rank )
+        {
+            return character.GangRank >= rank;
         }
     }
 }
