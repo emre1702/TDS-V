ac38fccfc531d4ff3f49587339d83b0b5b41a1b9
Emre Kara <emre1702@live.de>
2018-03-08 19:12:38 +0100

    Add Damage to Character

--- TDS/server/instance/player/Character.cs
+++ TDS/server/instance/player/Character.cs
@@ -20,31 +20,31 @@ namespace TDS.server.instance.player {
 
     public class Character {
         public Client Player;
-		public uint UID;
-		public uint AdminLvl = 0;
-		public uint DonatorLvl = 0;
-		public uint Playtime = 0;
-		public uint Money = 0;
+        public uint UID;
+        public uint AdminLvl = 0;
+        public uint DonatorLvl = 0;
+        public uint Playtime = 0;
+        public uint Money = 0;
         public LobbyDeathmatchStats ArenaStats;
         public LobbyDeathmatchStats TempStats;
         public LobbyDeathmatchStats CurrentStats;
         public int Team = 0;
         public Gang Gang;
         public uint GangRank = 0;
-		public uint Lifes = 0;
-		public Language Language = Language.ENGLISH;
+        public uint Lifes = 0;
+        public Language Language = Language.ENGLISH;
         public Lobby Lobby = manager.lobby.MainMenu.TheLobby;
-		public Character Spectating;
-		public bool LoggedIn;
-		public bool IsLobbyOwner = false;
-		public bool IsVIP = false;
+        public Character Spectating;
+        public bool LoggedIn;
+        public bool IsLobbyOwner = false;
+        public bool IsVIP = false;
 
-		public bool HitsoundOn = true;
+        public bool HitsoundOn = true;
 
-		public Character ( Client player, bool loggedin = true ) {
+        public Character ( Client player, bool loggedin = true ) {
             Player = player;
-			LoggedIn = loggedin;
-		}
+            LoggedIn = loggedin;
+        }
 
         public void GiveMoney ( short money ) {
             if ( money > 0 || Money > money * -1 ) {
@@ -52,10 +52,10 @@ namespace TDS.server.instance.player {
                 NAPI.ClientEvent.TriggerClientEvent ( Player, "onClientMoneyChange", Money );
             } else
                 Log.Error ( $"Player {Player.Name} should have went to minus money! Current: {Money} | Substracted money: {money}" );
-                
+
         }
 
-        public void GiveKill () {
+        public void GiveKill ( ) {
             ++CurrentStats.Kills;
             ++CurrentStats.TotalKills;
         }
@@ -75,7 +75,24 @@ namespace TDS.server.instance.player {
             CurrentStats.TotalDeaths += thedmg;
         }
 
-        public void SaveData () {
+        public void Damage ( ref int damage ) {
+            if ( Player.Armor + Player.Health < damage )
+				damage = Player.Armor + Player.Health;
+            int leftdamage = damage;
+            if ( Player.Armor > 0 ) {
+                if ( Player.Armor >= leftdamage ) {
+                    Player.Armor -= leftdamage;
+                    leftdamage = 0;
+                } else {
+                    leftdamage -= Player.Armor;
+                    Player.Armor = 0;
+                }
+            }
+            if ( leftdamage > 0 )
+                Player.Health -= leftdamage;
+        }
+
+        public void SaveData ( ) {
             if ( LoggedIn ) {
                 Database.Exec ( $"UPDATE player SET playtime = {Playtime}, money = {Money} WHERE uid = {UID}" );
                 Database.Exec ( $"UPDATE playerarenastats SET arenakills = {ArenaStats.Kills}, arenaassists = {ArenaStats.Assists}, arenadeaths = {ArenaStats.Deaths}" +
