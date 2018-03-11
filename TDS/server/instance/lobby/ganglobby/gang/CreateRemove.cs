66e4f1ff82994c035810b0016239d5a3c9efa51c
Emre Kara <emre1702@live.de>
2018-03-11 17:44:58 +0100

    Change coding style back

--- TDS/server/instance/lobby/ganglobby/gang/CreateRemove.cs
+++ TDS/server/instance/lobby/ganglobby/gang/CreateRemove.cs
@@ -3,51 +3,45 @@ using System.Linq;
 using TDS.server.instance.player;
 using TDS.server.manager.database;
 
-namespace TDS.server.instance.lobby.ganglobby
-{
+namespace TDS.server.instance.lobby.ganglobby {
 
-    partial class Gang
-    {
-        public static void CreateGang ( Character character, string name, string shortname )
-        {
-            uint ganguid = gangByUID.Keys.Max () + 1;
-            uint owneruid = character.UID;
+	partial class Gang {
+		public static void CreateGang ( Character character, string name, string shortname ) {
+			uint ganguid = gangByUID.Keys.Max () + 1;
+			uint owneruid = character.UID;
 
-            Gang gang = new Gang ( ganguid, name, shortname, owneruid );
+			Gang gang = new Gang ( ganguid, name, shortname, owneruid );
 
-            gangByUID[ganguid] = gang;
+			gangByUID[ganguid] = gang;
 
-            gang.AddMember ( character );
-            gang.membersRank[owneruid] = 2;
+			gang.AddMember ( character );
+			gang.membersRank[owneruid] = 2;
 
-            Database.ExecPrepared ( $"INSERT INTO gang (uid, name, shortname, owneruid) VALUE ({ganguid}, @NAME@, @SHORTNAME@, {owneruid});", new Dictionary<string, string>
-            {
-                { "@NAME@", name },
-                { "@SHORTNAME@", shortname }
-            } );
-        }
+			Database.ExecPrepared ( $"INSERT INTO gang (uid, name, shortname, owneruid) VALUE ({ganguid}, @NAME@, @SHORTNAME@, {owneruid});", new Dictionary<string, string>
+			{
+				{ "@NAME@", name },
+				{ "@SHORTNAME@", shortname }
+			} );
+		}
 
-        public void Remove ( )    
-        {
-            Database.Exec ( $"DELETE FROM gang WHERE uid = {uid}" );
-            Database.Exec ( $"DELETE FROM gangmember WHERE ganguid = {uid}" );
+		public void Remove ( ) {
+			Database.Exec ( $"DELETE FROM gang WHERE uid = {uid}" );
+			Database.Exec ( $"DELETE FROM gangmember WHERE ganguid = {uid}" );
 
-            gangByUID.Remove ( uid );
+			gangByUID.Remove ( uid );
 
-            // Remove online members //
-            for ( int i = membersOnline.Count - 1; i >= 0; --i )
-            {
-                // TODO: Send normal message
-                RemoveOnlineMember ( membersOnline[i], false );
-                playerMemberOfGang.Remove ( membersOnline[i].UID );
-            }
+			// Remove online members //
+			for ( int i = membersOnline.Count - 1; i >= 0; --i ) {
+				// TODO: Send normal message
+				RemoveOnlineMember ( membersOnline[i], false );
+				playerMemberOfGang.Remove ( membersOnline[i].UID );
+			}
 
-            // Remove offline members
-            foreach ( KeyValuePair<uint, Gang> entry in playerMemberOfGang.Where ( entry => entry.Value == this ).ToList() )
-            {
-                // TODO: Send offline message
-                playerMemberOfGang.Remove ( entry.Key );
-            }
-        }
-    }
+			// Remove offline members
+			foreach ( KeyValuePair<uint, Gang> entry in playerMemberOfGang.Where ( entry => entry.Value == this ).ToList () ) {
+				// TODO: Send offline message
+				playerMemberOfGang.Remove ( entry.Key );
+			}
+		}
+	}
 }
