408d8e87ee888ab201874285bdbc6764d824b138
Emre Kara <emre1702@live.de>
2018-03-28 22:54:46 +0200

    add files for userpanel

---
+++ TDS/client/userpanel/reports.ts
@@ -0,0 +1,61 @@
+﻿var reportsdata = {
+    inreportsmenu: false,
+    inreport: false,
+}
+
+// from serverside //
+mp.events.add( "syncReports", ( reports: string ) => {
+    mainbrowserdata.angular.call( "syncReports(`" + reports + "`);" );
+} );
+
+mp.events.add( "syncReportTexts", ( reporttexts: string ) => {
+    // SEND TO ANGULAR
+} );
+
+mp.events.add( "syncReportState", ( reportid: number, state: boolean ) => {
+    mainbrowserdata.angular.call( `syncReportState(${reportid}, ${state});` );
+} );
+
+mp.events.add( "syncReport", ( report: string ) => {
+    // SEND TO ANGULAR
+} );
+
+mp.events.add( "syncReportText", ( reporttext: string ) => {
+    // SEND TO ANGULAR
+} );
+
+
+// from browser //
+
+
+mp.events.add( "addTextToReport", ( reportid: number, text: string ) => {
+    mp.events.callRemote( "onPlayerAddTextToReport", reportid, text );
+} );
+
+mp.events.add( "createReport", ( json: string, text: string ) => {
+    mp.events.callRemote( "onPlayerCreateReport", json, text );
+} );
+
+function openReportsMenu() {
+    reportsdata.inreportsmenu = true;
+    mp.events.callRemote( "onPlayerOpenReportsMenu" );
+}
+
+function closeReportsMenu() {
+    reportsdata.inreport = false;
+    mp.events.callRemote( "onPlayerCloseReportsMenu" );
+}
+
+function openReport( index: number ) {
+    reportsdata.inreport = true;
+    mp.events.callRemote( "onPlayerOpenReport", index );
+}
+
+function closeReport() {
+    reportsdata.inreport = false;
+    mp.events.callRemote( "onPlayerCloseReport" );
+}
+
+function toggleReportState( reportid: number, state: number ) {
+    mp.events.callRemote( "onPlayerChangeReportState", reportid, state == 1 );
+}
---
+++ TDS/client/userpanel/userpanel.ts
@@ -0,0 +1,34 @@
+﻿let userpaneldata = {
+    open: false
+}
+
+function openUserpanel() {
+    mainbrowserdata.angular.call( "openUserpanel();" );
+    toggleCursor( true );
+    userpaneldata.open = true;
+}
+
+function closeUserpanel() {
+    mainbrowserdata.angular.call( "closeUserpanel();" );
+
+    if ( reportsdata.inreport ) {
+        reportsdata.inreport = false;
+        mp.events.callRemote( "onPlayerCloseReport" );
+    }
+    if ( reportsdata.inreportsmenu ) {
+        reportsdata.inreportsmenu = false;
+        mp.events.callRemote( "onPlayerCloseReportsMenu" );
+    }
+
+    toggleCursor( false );
+    userpaneldata.open = false;
+}
+
+mp.keys.bind( Keys.U, false, () => {
+    if ( ischatopen )
+        return; 
+    if ( !userpaneldata.open )
+        openUserpanel();
+    else
+        closeUserpanel();
+} );
