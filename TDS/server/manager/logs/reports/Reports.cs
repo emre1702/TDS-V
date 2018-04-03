3ba11fc3a657c704bed4b25b5f13150491fe729e
Emre Kara <emre1702@live.de>
2018-04-03 13:00:51 +0200

    Log report things

---
+++ TDS/server/manager/logs/reports/Reports.cs
@@ -0,0 +1,17 @@
+﻿namespace TDS.server.manager.logs {
+
+	partial class ReportsLog {
+
+		public static void Creation ( uint reportuid, uint useruid, string title, string text ) {
+			AddReportsLogEntry( reportuid, useruid, title + " | " + text );
+		}
+
+		public static void Answer ( uint reportuid, uint useruid, string text ) {
+			AddReportsLogEntry( reportuid, useruid, text );
+		}
+
+		public static void Remove ( uint reportuid, uint useruid, string text ) {
+			AddReportsLogEntry( reportuid, useruid, text );
+		}
+	}
+}
