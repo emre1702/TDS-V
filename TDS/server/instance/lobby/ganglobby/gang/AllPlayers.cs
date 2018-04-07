091179430ec4b854655c995c2e9ae474286873e3
Emre Kara <emre1702@live.de>
2018-04-07 20:35:48 +0200

    Add methods to easily send message to gang members

---
+++ TDS/server/instance/lobby/ganglobby/gang/AllPlayers.cs
@@ -0,0 +1,43 @@
+﻿using GTANetworkAPI;
+using System.Collections.Generic;
+using System.Text;
+using TDS.server.enums;
+using TDS.server.instance.player;
+using TDS.server.manager.utility;
+
+namespace TDS.server.instance.lobby.ganglobby {
+
+	partial class Gang {
+
+		public void SendAllOnlinePlayerLangMessage ( string type, params string[] args ) {
+			Dictionary<Language, string> lang = ServerLanguage.GetLangDictionary( type, args );
+			foreach ( Character character in membersOnline ) {
+				NAPI.Chat.SendChatMessageToPlayer( character.Player, lang[character.Language] );
+			}		
+		}
+
+		public void SendAllOnlinePlayerLangNotification ( string type, params string[] args ) {
+			Dictionary<Language, string> lang = ServerLanguage.GetLangDictionary( type, args );
+			foreach ( Character character in membersOnline ) {
+				NAPI.Notification.SendNotificationToPlayer( character.Player, lang[character.Language] );
+			}
+		}
+
+		public void SendAllPlayerLangMessage ( string type, params string[] args ) {
+			Dictionary<Language, string> lang = ServerLanguage.GetLangDictionary( type, args );
+			HashSet<uint> membersdone = new HashSet<uint>();
+
+			foreach ( Character character in membersOnline ) {
+				NAPI.Chat.SendChatMessageToPlayer( character.Player, lang[character.Language] );
+				membersdone.Add( character.UID );
+			}
+
+			foreach ( KeyValuePair<uint, uint> entry in membersRank ) {
+				if ( !membersdone.Contains( entry.Key ) ) {
+					OfflineMessages.AddOfflineMessage( entry.Key, lang[Language.ENGLISH], "Gang" );		
+				}
+			}
+		}
+
+	}
+}
