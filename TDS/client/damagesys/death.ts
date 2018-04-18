da54826c923fb63f5ef5d574da79466c5314230b
Emre Kara <emre1702@live.de>
2018-04-18 19:39:11 +0200

    Show wasted message on death

--- TDS/client/damagesys/death.ts
+++ TDS/client/damagesys/death.ts
@@ -1,7 +1,4 @@
-﻿/// <reference path="../types-ragemp/index.d.ts" />
-
-
-mp.events.add( "playerSpawn", ( player: PlayerMp ) => {
+﻿mp.events.add( "playerSpawn", ( player: PlayerMp ) => {
 	if ( player == localPlayer ) {
 		mp.game.cam.doScreenFadeIn( 2000 );
 	}
@@ -11,6 +8,9 @@ mp.events.add( "playerDeath", ( player: PlayerMp, reason, killer ) => {
 	if ( player == localPlayer ) {
 		mp.game.cam.doScreenFadeOut( 2000 );
 		mp.game.gameplay.ignoreNextRestart( true );
-		mp.game.gameplay.setFadeOutAfterDeath( false );
+        mp.game.gameplay.setFadeOutAfterDeath( false );
+        scaleformMessage.showWastedMessage();
+        mp.game.audio.requestScriptAudioBank( "HUD_MINI_GAME_SOUNDSET", true );  //
+        mp.game.audio.playSoundFrontend( -1, "CHECKPOINT_NORMAL", "HUD_MINI_GAME_SOUNDSET", true );  //
 	}
 } );
