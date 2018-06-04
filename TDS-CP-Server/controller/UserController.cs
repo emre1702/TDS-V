4fb77b89dd2dfb2a61779939ae022a5f99eb9e2a
emre1702 <emre1702@live.de>
2018-06-04 04:42:59 +0200

    Add Get for usernames at server

---
+++ TDS-CP-Server/controller/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDSCPServer.controller
+{
+
+    [Authorize]
+    public class UserController : Controller
+    {
+        [HttpGet("/names")]
+        public async Task<IEnumerable<string>> GetUsernames()
+        {
+            DataTable result = await Database.ExecResult("SELECT name FROM playeronline");
+            return result.Rows.OfType<DataRow>().Select(k => k["name"].ToString());
+        }
+    }
+}
