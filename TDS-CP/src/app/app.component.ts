d125de01465ca0cdb016a2a9bada9ade9e288da9
emre1702 <emre1702@live.de>
2018-06-04 04:20:57 +0200

    refresh usernames from server

--- TDS-CP/src/app/app.component.ts
+++ TDS-CP/src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { SettingsService } from "./shared/settings.service";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { GlobalDataService } from "./shared/globaldata.service";
+import { HttpClient } from "@angular/common/http";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
     title = "TDS-CP";
+    private refreshUsernamesTimeout: NodeJS.Timer;
+
+    constructor(private http: HttpClient, private globaldata: GlobalDataService, private auth: AuthService) {}
+
+    private refreshUsernames() {
+        if (this.auth.isAuthenticated()) {
+            this.http.get(this.globaldata.apiUrl + "/User").subscribe((usernames: string[]) => {
+                this.globaldata.userNames = usernames;
+            });
+        }
+    }
+
+    ngOnInit() {
+        this.refreshUsernamesTimeout = setTimeout(this.refreshUsernames, 30 * 1000);
+        this.refreshUsernames();
+    }
+
+    ngOnDestroy() {
+        clearTimeout(this.refreshUsernamesTimeout);
+    }
 }
