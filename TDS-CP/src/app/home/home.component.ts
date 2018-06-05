18fa5e408802a99705853aea9057a4caf1eb30f2
emre1702 <emre1702@live.de>
2018-06-05 06:19:52 +0200

    Refresh usernames once when in home

--- TDS-CP/src/app/home/home.component.ts
+++ TDS-CP/src/app/home/home.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { LoadingService } from "../loading/loading.service";
+import { GlobalDataService } from "../shared/globaldata.service";
+import { UserOnlineService } from "../useronline/useronline.service";
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
 })
+
 export class HomeComponent implements OnInit {
+    constructor(private http: HttpClient, private globaldata: GlobalDataService, private loading: LoadingService, private userOnlineService: UserOnlineService) { }
 
-  constructor(private http: HttpClient, private loading: LoadingService) { }
-
-  ngOnInit() {
-
-  }
-
+    ngOnInit() {
+        this.userOnlineService.refreshUsernames();
+    }
 }
