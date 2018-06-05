511ea31f3838c91dc7d176ab6c6a562e2e4a5506
emre1702 <emre1702@live.de>
2018-06-05 06:20:08 +0200

    Start refreshing usernames when loggedin

--- TDS-CP/src/app/login/login.component.ts
+++ TDS-CP/src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { Router } from "@angular/router";
 import { EventEmitter } from "events";
 import { MatSnackBar } from "@angular/material";
 import { LoadingService } from "../loading/loading.service";
+import { UserOnlineService } from "../useronline/useronline.service";
 
 @Component({
     selector: "app-login",
@@ -19,7 +20,8 @@ export class LoginComponent {
         password: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(100)])
     });
 
-    constructor(private http: HttpClient, private snackBar: MatSnackBar, private settings: GlobalDataService, private loading: LoadingService, private router: Router) { }
+    constructor(private http: HttpClient, private snackBar: MatSnackBar, private settings: GlobalDataService, private loading: LoadingService, private router: Router,
+        private userOnlineService: UserOnlineService ) { }
 
     onSubmit(form: NgForm) {
         this.loading.show();
@@ -28,6 +30,7 @@ export class LoginComponent {
                 localStorage.setItem("UID", data.uid.toString());
                 localStorage.setItem("token", data.token);
                 this.router.navigateByUrl("home");
+                this.userOnlineService.startRefreshingUsernames();
             } else {
                 this.snackBar.open(data.error);
             }
