0e5c9a5eeea3bb0e339f9f429058e8db70dc5224
emre1702 <emre1702@live.de>
2018-06-07 04:36:09 +0200

    Only use 3 adminranks: Projectleader, Administrator, Supporter

--- TDS/server/manager/utility/Admin.cs
+++ TDS/server/manager/utility/Admin.cs
@@ -11,23 +11,20 @@
 			[0] = "User",
 			[1] = "Supporter",
 			[2] = "Administrator",
-			[3] = "Project manager",
-			[4] = "Owner"
+			[3] = "Projectleader",
 		};
 		public static Dictionary<uint, string> LevelFontColor = new Dictionary<uint, string> {
 			[0] = "#s#",
 			[1] = "#g#",
 			[2] = "#o#",
 			[3] = "#r#",
-			[4] = "#dr#"
         };
 		public static Dictionary<uint, List<Character>> AdminsOnline = new Dictionary<uint, List<Character>> {
 			[1] = new List<Character> (),
 			[2] = new List<Character> (),
-			[3] = new List<Character> (),
-			[4] = new List<Character> ()
+			[3] = new List<Character> ()
 		};
-		private const int adminMaxLvl = 4;
+		private const int adminMaxLvl = 3;
 
 		public static void SetOnline ( Character character ) {
 			if ( adminMaxLvl >= character.AdminLvl ) {
