c9850a9db34b12d1e58e5156733777b921e49d6d
emre1702 <emre1702@live.de>
2018-06-09 05:08:08 +0200

    Add more informations to GlobalDataService (specially for logs) + make it singleton

--- TDS-CP/src/app/shared/globaldata.service.ts
+++ TDS-CP/src/app/shared/globaldata.service.ts
@@ -2,8 +2,35 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class GlobalDataService {
+    private static singleton: GlobalDataService;
+
     public readonly apiUrl = "http://194.95.0.48:4201";
     public showingPlayername = false;
-    public adminLvlNames = ["User", "Supporter", "Administrator", "Projectleader"];
-    public adminLvlColors = ["rgb(220,220,220)", "rgb(113,202,113)", "rgb(253,132,85)", "rgb(222,50,50)"];
+    public readonly adminLvlNames = ["User", "Supporter", "Administrator", "Projectleader"];
+    public readonly adminLvlColors = ["rgb(220,220,220)", "rgb(113,202,113)", "rgb(253,132,85)", "rgb(222,50,50)"];
+
+    public readonly logTypes = ["login", "register", "chat", "error", "lobbyowner", "lobbyjoin", "vip", "admin", "report"];
+    public readonly neededAdminLvlForLogTypes = {
+        ["admin"]: 0,
+        ["report"]: 0,
+        ["login"]: 1,
+        ["register"]: 1,
+        ["chat"]: 2,
+        ["error"]: 3,
+        ["lobbyowner"]: 1,
+        ["lobbyjoin"]: 1,
+        ["vip"]: 1
+    };
+    public readonly adminLogTypes = ["permaban", "timeban", "unban", "permamute", "timemute", "unmute", "next", "kick", "lobbykick", "permabanlobby", "timebanlobby", "unbanlobby", "all"];
+
+    get adminLvl() {
+        return localStorage.getItem("adminlvl") || 0;
+    }
+
+    constructor() {
+        if (!GlobalDataService.singleton) {
+            GlobalDataService.singleton = this;
+        }
+        return GlobalDataService.singleton;
+    }
 }
