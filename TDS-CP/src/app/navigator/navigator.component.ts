42068633639baa6867e1c477090989f114ebf33e
emre1702 <emre1702@live.de>
2018-06-13 01:12:12 +0200

    Add logout button

--- TDS-CP/src/app/navigator/navigator.component.ts
+++ TDS-CP/src/app/navigator/navigator.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { GlobalDataService } from "../shared/globaldata.service";
+import { PlayerOnlineService } from "../playeronline/playeronline.service";
 
 @Component({
     selector: "app-navigator",
     templateUrl: "./navigator.component.html",
     styleUrls: ["./navigator.component.css"]
 })
-export class NavigatorComponent implements OnInit {
+export class NavigatorComponent {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private playeronline: PlayerOnlineService, private globaldata: GlobalDataService) { }
 
-    ngOnInit() {
+    logout() {
+        localStorage.removeItem("token");
+        this.playeronline.ngOnDestroy();
+        this.router.navigateByUrl("login");
     }
 
 }
