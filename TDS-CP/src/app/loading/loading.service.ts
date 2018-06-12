8cc2fc3f940d2f7a79b62362ae07a83920a540fc
emre1702 <emre1702@live.de>
2018-06-13 01:11:20 +0200

    Add showing variable to LoadingServer

--- TDS-CP/src/app/loading/loading.service.ts
+++ TDS-CP/src/app/loading/loading.service.ts
@@ -4,12 +4,15 @@ import { EventEmitter } from "events";
 @Injectable()
 export class LoadingService {
     @Output() change: EventEmitter = new EventEmitter();
+    showing = false;
 
     show() {
         this.change.emit("toggle", true);
+        this.showing = true;
     }
 
     hide() {
         this.change.emit("toggle", false);
+        this.showing = false;
     }
 }
