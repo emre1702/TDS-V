284dbab1a52abee8032539e374e297bd7f8785be
emre1702 <emre1702@live.de>
2018-08-04 01:15:28 +0200

    Add Logout controller to be used later

---
+++ TDS-CP-Server/controller/LogoutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDSCPServer.controller
+{
+
+    [Authorize]
+    [Route("[controller]")]
+    public class LogoutController : Controller
+    {
+
+        [HttpPut]
+        public IActionResult Logout()
+        {
+            return Ok();
+        }
+    }
+}
