32c746aa361b4fd7e29c1faf89806dcaae3ed05a
emre1702 <emre1702@live.de>
2018-08-05 22:35:16 +0200

    Add username to chatMessage model

--- TDS-CP/src/app/models/chatMessage.model.ts
+++ TDS-CP/src/app/models/chatMessage.model.ts
@@ -1,7 +1,7 @@
 export class ChatMessage {
     public sent: Date;
 
-    constructor(public message: string = "", date: string = "") {
+    constructor(public username: string = "", public message: string = "", date: string = "") {
         this.sent = new Date(date);
     }
 }
