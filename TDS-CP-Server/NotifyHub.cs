32101fe8279ea9bbce036b4ce50d67c21db3ecb4
emre1702 <emre1702@live.de>
2018-08-06 01:32:51 +0200

    Add methods to add to and remove from group

--- TDS-CP-Server/NotifyHub.cs
+++ TDS-CP-Server/NotifyHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,6 +22,16 @@ namespace TDSCPServer
         public Task SendLastChatMessages()
         {
             return Clients.Caller.SendAsync("SendLastChatMessages", lastChatMessages.Take(25));
+        }
+
+        public Task AddToGroup(EGroupID id)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, id.ToString());
+        }
+
+        public Task RemoveFromGroup(EGroupID id)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, id.ToString());
         }
     }
 }
