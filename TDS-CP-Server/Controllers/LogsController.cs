09625a7c25bd9ddb8e169b590d71a62e1d0d60bf
emre1702 <emre1702@live.de>
2018-08-09 13:40:37 +0200

    [CP] LogEntry class to Models

--- TDS-CP-Server/Controllers/LogsController.cs
+++ TDS-CP-Server/Controllers/LogsController.cs
@@ -6,39 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TDSCPServer.Enums;
+using TDSCPServer.Models;
 using TDSCPServer.Utils;
 
 namespace TDSCPServer.Controllers
 {
-
-    public class LogEntry
-    {
-        public int id;
-        public string name;
-        public string target;
-        public int type;
-        public string info;
-        public string lobby;
-        public string date;
-
-        
-        private static T GetValue<T>(DataRow row, string column)
-        {
-            return row.Table.Columns.Contains(column) ? (T) row[column] : default(T);
-        }
-
-        public LogEntry(DataRow row, int type)
-        {
-            id = Convert.ToInt32(row["id"]);
-            name = Convert.ToString(row["name"]);
-            target = Convert.ToString(row["target"]);
-            this.type = type;
-            info = Convert.ToString(row["info"]);
-            lobby = GetValue<string>(row, "lobby");
-            date = Convert.ToString(row["date"]);
-        }
-    }
-
     [Authorize]
     [Route("[controller]")]
     public class LogsController : Controller
