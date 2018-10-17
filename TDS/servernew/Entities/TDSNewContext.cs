acfc23743c79cde298755d63fa2562bac8fc5052
emre1702 <emre1702@live.de>
2018-10-17 19:43:24 +0200

    Use Entity Framework

---
+++ TDS/servernew/Entities/TDSNewContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace TDS.servernew.Entities
+{
+    public partial class TDSNewContext : DbContext
+    {
+        public TDSNewContext()
+        {
+        }
+
+        public TDSNewContext(DbContextOptions<TDSNewContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(ConfigurationManager.ConnectionStrings["MainDB"].ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
