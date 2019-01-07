4f308f00013bc49b1db8d4cd01734f395d69fb8b
emre1702 <emre1702@live.de>
2019-01-07 19:42:36 +0100

    Fix binding (multiple triggers instead of once)

--- TDS_Client/Manager/Utility/BindManager.cs
+++ TDS_Client/Manager/Utility/BindManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TDS_Client.Dto;
 using TDS_Client.Enum;
+using TDS_Client.Manager.Lobby;
 using Events = RAGE.Events;
 
 namespace TDS_Client.Manager.Utility
@@ -13,6 +14,8 @@ namespace TDS_Client.Manager.Utility
     {
         private static readonly Dictionary<ConsoleKey, List<KeyBindDto>> bindedKeys = new Dictionary<ConsoleKey, List<KeyBindDto>>();
         private static readonly Dictionary<Control, List<ControlBindDto>> bindedControls = new Dictionary<Control, List<ControlBindDto>>();
+        private static readonly Dictionary<ConsoleKey, bool> lastKeyDownState = new Dictionary<ConsoleKey, bool>();
+        private static readonly Dictionary<Control, bool> lastControlPressedState = new Dictionary<Control, bool>();
 
         public BindManager()
         {
@@ -63,25 +66,31 @@ namespace TDS_Client.Manager.Utility
         {
             foreach (var entry in bindedKeys)
             {
+
                 bool isDown = Input.IsDown((int)entry.Key);
-                bool isUp = Input.IsUp((int)entry.Key);
+                if (lastKeyDownState.ContainsKey(entry.Key))
+                    if (lastKeyDownState[entry.Key] == isDown)
+                        continue;
+                lastKeyDownState[entry.Key] = isDown;
+
                 foreach (var bind in entry.Value)
-                {
-                    if (isDown && bind.OnDown || isUp && bind.OnUp)
+                    if (isDown && bind.OnDown || !isDown && bind.OnUp)
                         bind.Method(entry.Key);
-                }
             }
 
             foreach (var entry in bindedControls)
             {
                 bool isDownEnabled = Pad.IsControlPressed(0, (int)entry.Key);
                 bool isDownDisabled = Pad.IsDisabledControlPressed(0, (int)entry.Key);
+                if (lastControlPressedState.ContainsKey(entry.Key))
+                    if (lastControlPressedState[entry.Key] == (isDownEnabled || isDownDisabled))
+                        continue;
+                lastControlPressedState[entry.Key] = (isDownEnabled || isDownDisabled);
+
                 foreach (var bind in entry.Value)
-                {
                     if (bind.OnDown && (isDownEnabled && bind.OnEnabled || isDownDisabled && bind.OnDisabled)
                         || bind.OnUp && (!isDownEnabled && bind.OnEnabled || !isDownDisabled && bind.OnDisabled))
                         bind.Method(entry.Key);
-                }
             }
         }
 
