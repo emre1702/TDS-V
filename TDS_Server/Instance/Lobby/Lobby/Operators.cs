5d893d232019f15904ab36c75bbd026eef0207a0
emre1702 <emre1702@live.de>
2019-02-28 19:45:42 +0100

    Fix operators for lobby

--- TDS_Server/Instance/Lobby/Lobby/Operators.cs
+++ TDS_Server/Instance/Lobby/Lobby/Operators.cs
@@ -9,7 +9,7 @@ namespace TDS_Server.Instance.Lobby
         public static bool operator ==(Lobby lobby1, Lobby lobby2)
         {
             if (lobby1 is null)
-                return false;
+                return lobby2 is null;
             if (lobby2 is null)
                 return false;
             return lobby1.Id == lobby2.Id;
@@ -18,7 +18,7 @@ namespace TDS_Server.Instance.Lobby
         public static bool operator !=(Lobby lobby1, Lobby lobby2)
         {
             if (lobby1 is null)
-                return true;
+                return !(lobby2 is null);
             if (lobby2 is null)
                 return true;
             return lobby1.Id != lobby2.Id;
