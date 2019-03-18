d161b60763057c82fc7337780b8081094e2aef20
emre1702 <emre1702@live.de>
2019-03-18 21:29:40 +0100

    Fix DxLine

--- TDS_Client/Instance/Draw/Dx/DxLine.cs
+++ TDS_Client/Instance/Draw/Dx/DxLine.cs
@@ -1,9 +1,11 @@
-﻿using RAGE.Game;
+﻿using RAGE;
+using RAGE.Game;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
 using TDS_Client.Enum;
+using TDS_Client.Manager.Utility;
 
 namespace TDS_Client.Instance.Draw.Dx
 {
@@ -17,16 +19,33 @@ namespace TDS_Client.Instance.Draw.Dx
             endZ;
         private Color color;
         private bool relative;
+        private bool is3D;
 
 
         public DxLine(float startX, float startY, float? startZ, float endX, float endY, float? endZ, Color color, bool relative = true) : base()
         {
-            this.startX = startX;
-            this.startY = startY;
-            this.startZ = startZ.HasValue ? startZ.Value : 0;
-            this.endX = endX;
-            this.endY = endY;
-            this.endZ = endZ.HasValue ? endZ.Value : 0;
+            if (is3D)
+            {
+                this.startX = startX;
+                this.startY = startY;
+                this.startZ = startZ.Value;
+                this.endX = endX;
+                this.endY = endY;
+                this.endZ = endZ.Value;
+            } 
+            else
+            {
+                Vector3 worldStart = ClientUtils.GetWorldCoordFromScreenCoord(GetRelativeX(startX, relative), GetRelativeY(startY, relative));
+                this.startX = worldStart.X;
+                this.startY = worldStart.Y;
+                this.startZ = worldStart.Z;
+
+                Vector3 worldEnd = ClientUtils.GetWorldCoordFromScreenCoord(GetRelativeX(endX, relative), GetRelativeY(endY, relative));
+                this.endX = worldEnd.X;
+                this.endY = worldEnd.Y;
+                this.endZ = worldEnd.Z;
+            }
+            
             this.color = color;
             this.relative = relative;
         }
