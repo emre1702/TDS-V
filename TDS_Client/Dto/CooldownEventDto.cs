9c1c20261d2c8ac637a6952214f25a1499c2d361
emre1702 <emre1702@live.de>
2019-07-27 18:21:00 +0200

    Improve EventsSender cooldown code

--- TDS_Client/Dto/CooldownEventDto.cs
+++ TDS_Client/Dto/CooldownEventDto.cs
@@ -2,13 +2,11 @@
 {
     internal class CooldownEventDto
     {
-        public string EventName;
         public ulong LastExecMs = 0;
         public uint CooldownMs;
 
-        public CooldownEventDto(string eventName, uint cooldownMs)
+        public CooldownEventDto(uint cooldownMs)
         {
-            EventName = eventName;
             CooldownMs = cooldownMs;
         }
     }
