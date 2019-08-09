0966cf7a687b7d6008bebd74a78d6246e0a1ae17
emre1702 <emre1702@live.de>
2019-08-09 23:08:46 +0200

    Add fallback ESC to close chat

--- TDS_Client/Manager/Browser/ChatManager.cs
+++ TDS_Client/Manager/Browser/ChatManager.cs
@@ -32,6 +32,7 @@ namespace TDS_Client.Manager.Browser
             Chat.Show(false);
             BindManager.Add(Control.MpTextChatAll, OpenLobbyChatInput);
             BindManager.Add(Control.MpTextChatTeam, OpenTeamChatInput);
+            BindManager.Add(System.ConsoleKey.Escape, (_) => CloseChatInput());
         }
 
         public static void OnUpdate()
