49e2e43738f7cc3fa5c54acda5177d6d191a647f
emre1702 <emre1702@live.de>
2019-08-25 18:10:46 +0200

    Improve scaleform handling

--- TDS_Client/Instance/Draw/Scaleform/BasicScaleform.cs
+++ TDS_Client/Instance/Draw/Scaleform/BasicScaleform.cs
@@ -1,16 +1,31 @@
-﻿using RAGE.Game;
+﻿using RAGE;
+using RAGE.Game;
+using System;
+using System.Collections.Generic;
 
 namespace TDS_Client.Instance.Draw.Scaleform
 {
-    internal class BasicScaleform
+    internal class BasicScaleform : IDisposable
     {
-        private int handle;
+        private bool IsLoaded => Graphics.HasScaleformMovieLoaded(_handle);
+        private bool IsValid => _handle != 0;
 
-        public BasicScaleform(string scaleformName) => handle = Graphics.RequestScaleformMovie(scaleformName);
+        private Queue<(string, object[])> _functionQueue = new Queue<(string, object[])>();
+        private int _handle;
+
+        public BasicScaleform(string scaleformName)
+        {
+            _handle = Graphics.RequestScaleformMovie(scaleformName);
+        }
 
         public void Call(string functionName, params object[] args)
         {
-            Graphics.PushScaleformMovieFunction(handle, functionName);
+            if (!IsLoaded || !IsValid)
+            {
+                _functionQueue.Enqueue((functionName, args));
+                return;
+            }
+            Graphics.PushScaleformMovieFunction(_handle, functionName);
             foreach (object arg in args)
             {
                 if (arg is string)
@@ -25,14 +40,67 @@ namespace TDS_Client.Instance.Draw.Scaleform
             Graphics.PopScaleformMovieFunctionVoid();
         }
 
+
         public void RenderFullscreen()
         {
-            Graphics.DrawScaleformMovieFullscreen(handle, 255, 255, 255, 255, 0);
+            OnUpdate();
+            if (IsLoaded && IsValid)
+                Graphics.DrawScaleformMovieFullscreen(_handle, 255, 255, 255, 255, 0);
         }
 
-        public void Destroy()
+        public void Render2D(float x, float y, float width, float height)
         {
-            Graphics.SetScaleformMovieAsNoLongerNeeded(ref handle);
+            OnUpdate();
+            if (IsLoaded && IsValid)
+                Graphics.DrawScaleformMovie(_handle, x, y, width, height, 255, 255, 255, 255, 0);
         }
+
+        public void Render3D(Vector3 position, Vector3 rotation, Vector3 scale)
+        {
+            OnUpdate();
+            if (IsLoaded && IsValid)
+                Graphics.DrawScaleformMovie3dNonAdditive(_handle, position.X, position.Y, position.Z, rotation.X, rotation.Y, rotation.Z, 2, 2, 1, scale.X, scale.Z, scale.Z, 2);
+        }
+
+        public void Render3DAdditive(Vector3 position, Vector3 rotation, Vector3 scale)
+        {
+            OnUpdate();
+            if (IsLoaded && IsValid)
+                Graphics.DrawScaleformMovie3d(_handle, position.X, position.Y, position.Z, rotation.X, rotation.Y, rotation.Z, 2, 2, 1, scale.X, scale.Z, scale.Z, 2);
+        }
+
+        private void OnUpdate()
+        {
+            if (_functionQueue.Count == 0 || !IsLoaded || !IsValid)
+                return;
+            foreach (var entry in _functionQueue)
+            {
+                Call(entry.Item1, entry.Item2);
+            }
+            _functionQueue.Clear();
+        }
+
+        #region IDisposable Support
+        private bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    Graphics.SetScaleformMovieAsNoLongerNeeded(ref _handle);
+                    _functionQueue = null;
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
     }
 }
