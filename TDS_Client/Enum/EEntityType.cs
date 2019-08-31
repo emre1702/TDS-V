e6983e05ecb3f2d07984f45776fd4b05ac9c334f
emre1702 <emre1702@live.de>
2019-08-31 16:20:30 +0200

    Add enum for entity types

---
+++ TDS_Client/Enum/EEntityType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDS_Client.Enum
+{
+    enum EEntityType
+    {
+        None = 0,
+        Ped = 1,
+        Vehicle = 2,
+        Object = 3
+    }
+}
