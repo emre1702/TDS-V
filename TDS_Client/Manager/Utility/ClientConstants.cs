0ed00a522ac2e279b14220eaa032c0353e16af68
emre1702 <emre1702@live.de>
2019-09-01 23:11:16 +0200

    Add client constants data for map creator objects / peds

--- TDS_Client/Manager/Utility/ClientConstants.cs
+++ TDS_Client/Manager/Utility/ClientConstants.cs
@@ -1,4 +1,6 @@
-﻿namespace TDS_Client.Manager.Utility
+﻿using RAGE.Game;
+
+namespace TDS_Client.Manager.Utility
 {
     internal class ClientConstants
     {
@@ -10,6 +12,10 @@
         public static ulong ShowFloatingDamageInfoMs = 1000;
         public static uint MapLimitFasterCheckTimeMs => 100;
         public static int DefaultSpectatePlayerChangeEaseTime => 1500;
-        public static int NametagAlpha = 255;
+        public const int NametagAlpha = 255;
+        public static uint MapCenterHash => Misc.GetHashKey("prop_flagpole_1a");
+        public static uint MapLimitHash => Misc.GetHashKey("prop_flagpole_1a"); 
+        public static uint BombPlantPlaceHash => Misc.GetHashKey("prop_mp_placement_med");
+        public static uint[] TeamSpawnPedHash = new uint[] { 0x7A05FA59, 0x106D9A99, 0x5E3DA4A4, 0xC0F371B7, 0x45348DBB, 0xE83B93B7, 0x5CDEF405 };
     }
 }
