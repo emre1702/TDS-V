8c8b2b9afba497ec97f5e3085b772e5e118fd64f
emre1702 <emre1702@live.de>
2019-09-01 16:37:14 +0200

    Add CursorVisibilityChanged event

--- TDS_Client/Manager/Utility/CursorManager.cs
+++ TDS_Client/Manager/Utility/CursorManager.cs
@@ -7,8 +7,6 @@ namespace TDS_Client.Manager.Utility
 {
     internal class CursorManager
     {
-        private static int _cursorOpenedCounter;
-
         public static bool Visible
         {
             get => Cursor.Visible;
@@ -16,24 +14,32 @@ namespace TDS_Client.Manager.Utility
             {
                 if (value)
                 {
-                    ++_cursorOpenedCounter;
-                    Cursor.Visible = true;
-                    Chat.Activate(false);
+                    if (++_cursorOpenedCounter == 1)
+                    {
+                        Cursor.Visible = true;
+                        Chat.Activate(false);
+                        CursorVisibilityChanged?.Invoke(true);
+                    }
                 }
                 else if (--_cursorOpenedCounter <= 0)
                 {
                     Cursor.Visible = false;
                     Chat.Activate(true);
                     _cursorOpenedCounter = 0;
+                    CursorVisibilityChanged?.Invoke(false);
                 }
             }
         }
+
+        public delegate void CursorVisibilityChangedDelegate(bool visible);
+        public static event CursorVisibilityChangedDelegate CursorVisibilityChanged;
+
+        private static int _cursorOpenedCounter;
 
         public static void ManuallyToggleCursor(EKey _)
         {
             bool isVisible = Cursor.Visible;
             _cursorOpenedCounter = isVisible ? 0 : 1;
-            Chat.Activate(isVisible);
             Cursor.Visible = !isVisible;
         }
     }
