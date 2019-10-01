639136c9cc4be75eda76a3659e0b1dd87e753429
emre1702 <emre1702@live.de>
2019-10-01 22:40:52 +0200

    Add migration script for serial in bans table

---
+++ TDS_Server_DB/Migrations/20191001203855_SerialForBans.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TDS_Server_DB.Migrations
+{
+    public partial class SerialForBans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Serial",
+                table: "player_bans",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Serial",
+                table: "player_bans");
+        }
+    }
+}
