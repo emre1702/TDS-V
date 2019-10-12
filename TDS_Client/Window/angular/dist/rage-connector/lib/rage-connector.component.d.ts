aa616ae4c80492b606009142d9ff8f982cb95997
Emre Kara <emre.kara@wsw.de>
2019-10-12 17:54:22 +0200

    .gitignore Fixed

--- TDS_Client/Window/angular/dist/rage-connector/lib/rage-connector.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class RageConnectorComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- TDS_Client/Window/angular/dist/rage-connector/public-api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/rage-connector.service';
-export * from './lib/rage-connector.component';
-export * from './lib/rage-connector.module';
