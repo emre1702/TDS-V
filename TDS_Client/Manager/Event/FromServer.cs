405399a75aa33cb1892599a0322423c5907176b5
Emre Kara <emre.kara@wsw.de>
2019-10-24 00:15:57 +0200

    Fix small conversion error on round start

--- TDS_Client/Manager/Event/FromServer.cs
+++ TDS_Client/Manager/Event/FromServer.cs
@@ -224,8 +224,8 @@ namespace TDS_Client.Manager.Event
             Round.InFight = !Round.IsSpectator;
             Cam.DoScreenFadeIn(50);
             LobbyCam.StopCountdown();
-            Countdown.End(args.Length < 2 || (ulong)args[1] != 0);
-            RoundInfo.Start(args.Length >= 2 ? (ulong)args[1] : 0);
+            Countdown.End(args.Length < 2 || Convert.ToUInt64(args[1]) != 0);
+            RoundInfo.Start(args.Length >= 2 ? Convert.ToUInt64(args[1]) : 0);
         }
 
         private void OnRoundEndMethod(object[] args)
