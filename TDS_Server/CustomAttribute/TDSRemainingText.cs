4d14a98b87437f202606144eaf8f6f5f46cbebb3
emre1702 <emre1702@live.de>
2019-11-03 18:45:47 +0100

    Improve code

--- TDS_Server/CustomAttribute/TDSRemainingText.cs
+++ TDS_Server/CustomAttribute/TDSRemainingText.cs
@@ -5,12 +5,7 @@ namespace TDS_Server.CustomAttribute
     [AttributeUsage(AttributeTargets.Parameter)]
     internal class TDSRemainingText : Attribute
     {
-        public int MinLength { get; set; }
-        public int MaxLength { get; set; }
-
-        public TDSRemainingText(int minLength = 1, int maxLength = int.MaxValue)
-        {
-
-        }
+        public int MinLength { get; set; } = 1;
+        public int MaxLength { get; set; } = int.MaxValue;
     }
 }
