2abd4ec0baa75a2508049f0b262b804f03512437
emre1702 <emre1702@live.de>
2019-11-06 22:10:19 +0100

    Add EntityWrapperClass for classes wrapping a db entity

---
+++ TDS_Server/Instance/Utility/EntityWrapperClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TDS_Server_DB.Entity;
+
+namespace TDS_Server.Instance.Utility
+{
+    abstract class EntityWrapperClass
+    {
+        protected TDSNewContext DbContext
+        {
+            get
+            {
+                if (_dbContext is null)
+                    _dbContext = new TDSNewContext();
+                return _dbContext;
+            }
+            set => _dbContext = value;
+        }
+
+        private TDSNewContext? _dbContext;
+        private readonly SemaphoreSlim _dbContextSemaphore = new SemaphoreSlim(1);
+        private bool _usingDBContext;
+
+        public void InitDbContext()
+        {
+            _dbContext = new TDSNewContext();
+        }
+
+        public async Task ExecuteForDBAsync(Func<TDSNewContext, Task> action)
+        {
+            bool wasInDBContextBefore = _usingDBContext;
+            if (!wasInDBContextBefore)
+            {
+                await _dbContextSemaphore.WaitAsync();
+                _usingDBContext = true;
+            }
+
+            try
+            {
+                await action(DbContext);
+            }
+            finally
+            {
+                if (!wasInDBContextBefore)
+                {
+                    _dbContextSemaphore.Release();
+                    _usingDBContext = false;
+                }
+            }
+        }
+
+        public async Task<T> ExecuteForDBAsync<T>(Func<TDSNewContext, Task<T>> action)
+        {
+            bool wasInDBContextBefore = _usingDBContext;
+            if (!wasInDBContextBefore)
+            {
+                await _dbContextSemaphore.WaitAsync();
+                _usingDBContext = true;
+            }
+
+            try
+            {
+                return await action(DbContext);
+            }
+            finally
+            {
+                if (!wasInDBContextBefore)
+                {
+                    _dbContextSemaphore.Release();
+                    _usingDBContext = false;
+                }
+            }
+        }
+
+        public async Task ExecuteForDB(Action<TDSNewContext> action)
+        {
+            bool wasInDBContextBefore = _usingDBContext;
+            if (!wasInDBContextBefore)
+            {
+                await _dbContextSemaphore.WaitAsync();
+                _usingDBContext = true;
+            }
+
+            try
+            {
+                action(DbContext);
+            }
+            finally
+            {
+                if (!wasInDBContextBefore)
+                {
+                    _dbContextSemaphore.Release();
+                    _usingDBContext = false;
+                }
+            }
+        }
+
+        public async Task RemoveDBContext()
+        {
+            await ExecuteForDB((dbContext) =>
+            {
+                dbContext.Dispose();
+                _dbContext = null;
+            });
+        }
+    }
+}
