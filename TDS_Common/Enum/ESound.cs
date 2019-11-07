d0d73009e4c10d503b01688bf06748de668db064
emre1702 <emre1702@live.de>
2019-11-07 23:33:42 +0100

    Add enum for sounds

---
+++ TDS_Common/Enum/ESound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDS_Common.Enum
+{
+    public enum ESound
+    {
+        Hitsound,
+        BombTick,
+        Countdown3,
+        Countdown2,
+        Countdown1, 
+        CountdownGo,
+        PrivateMessage,
+        Dominating,
+        DoubleKill,
+        Godlike,
+        HolyShit,
+        Humiliation,
+        KillingSpree,
+        MonsterKill,
+        MultiKill,
+        Rampage,
+        TripleKill,
+        UltraKill,
+        Unstoppable,
+        WickedSick
+    }
+}
