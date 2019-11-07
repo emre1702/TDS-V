2050e55bd473a493efcdacf74e3130593f5eff2b
emre1702 <emre1702@live.de>
2019-11-07 01:55:18 +0100

    Improve gang class

--- TDS_Server/Instance/GangTeam/Gang.cs
+++ TDS_Server/Instance/GangTeam/Gang.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TDS_Server.Instance.Player;
 using TDS_Server.Instance.Utility;
@@ -11,7 +12,11 @@ namespace TDS_Server.Instance.GangTeam
     internal class Gang : EntityWrapperClass
     {
         private static readonly Dictionary<int, Gang> _gangById = new Dictionary<int, Gang>();
-        public static Gang None => _gangById[0];
+        private static readonly Dictionary<int, Gang> _gangByPlayerId = new Dictionary<int, Gang>();
+        private static readonly Dictionary<int, GangMembers> _gangMemberByPlayerId = new Dictionary<int, GangMembers>();
+
+        public static Gang None => _gangById[-1];
+        public static GangRanks NoneRank => None.Entity.Ranks.First();
 
         public Gangs Entity;
         public List<TDSPlayer> PlayersOnline = new List<TDSPlayer>();
@@ -20,6 +25,13 @@ namespace TDS_Server.Instance.GangTeam
         {
             Entity = entity;
             _gangById[entity.Id] = this;
+
+            foreach (var member in entity.Members)
+            {
+                _gangByPlayerId[member.PlayerId] = this;
+                _gangMemberByPlayerId[member.PlayerId] = member;
+            }
+
             DbContext.Attach(entity);
         }
 
@@ -30,14 +42,27 @@ namespace TDS_Server.Instance.GangTeam
 
         public static Gang GetPlayerGang(TDSPlayer player)
         {
-        
+            if (player.Entity != null)
+                if (_gangByPlayerId.ContainsKey(player.Entity.Id))
+                    return _gangByPlayerId[player.Entity.Id];
+            
             return None;
+        }
+
+        public static GangRanks GetPlayerGangRank(TDSPlayer player) 
+        {
+            if (player.Entity != null)
+                if (_gangMemberByPlayerId.ContainsKey(player.Entity.Id))
+                    return _gangMemberByPlayerId[player.Entity.Id].RankNavigation;
+
+            return NoneRank;
         }
 
         public static Task LoadAll(TDSNewContext dbContext)
         {
             return dbContext.Gangs
                 .Include(g => g.Members)
+                .ThenInclude(m => m.RankNavigation)
                 .Include(g => g.RankPermissions)
                 .AsNoTracking()
                 .ForEachAsync(g =>
