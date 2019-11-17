6ef85cae6cdecbba38064dfb8a96831b75d0272f
emre1702 <emre1702@live.de>
2019-11-17 05:55:56 +0100

    Fix small bug in latest commit

--- TDS_Server/Manager/Player/Register.cs
+++ TDS_Server/Manager/Player/Register.cs
@@ -20,7 +20,7 @@ namespace TDS_Server.Manager.Player
             using TDSNewContext dbContext = new TDSNewContext();
             if (string.IsNullOrWhiteSpace(email) || !new EmailAddressAttribute().IsValid(email))
                 email = null;
-            if (!int.TryParse(username, out int result)) 
+            if (int.TryParse(username, out int result)) 
                 return;
 
             Players dbplayer = new Players
