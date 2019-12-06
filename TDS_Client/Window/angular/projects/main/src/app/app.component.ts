5d1970b0a70b3797a9a3d86ec44d1a38a88cdea6
emre1702 <emre1702@live.de>
2019-12-06 20:47:44 +0100

    Improve animations (close/leave) of mapvoting and teamorders

--- TDS_Client/Window/angular/projects/main/src/app/app.component.ts
+++ TDS_Client/Window/angular/projects/main/src/app/app.component.ts
@@ -4,12 +4,34 @@ import { RageConnectorService } from 'rage-connector';
 import { DFromClientEvent } from './enums/dfromclientevent.enum';
 import { MatSnackBar } from '@angular/material';
 import { RoundPlayerRankingStat } from './components/ranking/models/roundPlayerRankingStat';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
+import { TeamOrder } from './components/teamorders/enums/teamorder.enum';
+import { Invitation } from './components/utils/invitation/models/invitation';
 
 @Component({
     selector: 'app-root',
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    animations: [
+
+        trigger('teamOrdersAnimation', [
+            transition('* => *', [
+                query(':enter', [
+                    style({ transform: 'translateX(100%)', opacity: 0 }),
+                    stagger(150, [
+                        animate('500ms', style({ transform: 'translateX(0)', opacity: 1 })),
+                    ])
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0)', opacity: 1 }),
+                    stagger(150, [
+                        animate('500ms', style({ transform: 'translateX(100%)', opacity: 0 }))
+                    ])
+                ], { optional: true })
+            ])
+        ])
+    ],
 })
 export class AppComponent {
     showMapCreator = false;
@@ -25,8 +47,7 @@ export class AppComponent {
         public settings: SettingsService,
         rageConnector: RageConnectorService,
         changeDetector: ChangeDetectorRef,
-        snackBar: MatSnackBar,
-        public vcRef: ViewContainerRef) {
+        snackBar: MatSnackBar) {
 
         rageConnector.listen(DFromClientEvent.InitLoadAngular, (adminLevel: number, remoteId: number) => {
             this.settings.loadAdminLevel(adminLevel);
@@ -77,9 +98,14 @@ export class AppComponent {
         });
 
         rageConnector.listen(DFromClientEvent.ShowCooldown, () => {
-            snackBar.open("Cooldown", undefined, { duration: 2000 });
+            snackBar.open("Cooldown", undefined, { duration: 3000 });
         });
 
         this.settings.InFightLobbyChanged.on(null, () => changeDetector.detectChanges());
+    }
+
+
+    getTeamOrdersLength(): number {
+        return Object.values(TeamOrder).length;
     }
 }
--- TDS_Client/Window/angular/projects/main/src/app/components/teamorders/teamorders.component.ts
+++ TDS_Client/Window/angular/projects/main/src/app/components/teamorders/teamorders.component.ts
@@ -1,28 +1,9 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
 import { TeamOrder } from './enums/teamorder.enum';
 import { SettingsService } from '../../services/settings.service';
-import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-teamorders',
-  animations: [
-    trigger('hideShowAnimation', [
-      transition('* => *', [
-        query(':enter', [
-          style({ transform: 'translateX(100%)', opacity: 0 }),
-          stagger(100, [
-            animate('500ms', style({ transform: 'translateX(0)', opacity: 1 })),
-          ])
-        ], { optional: true }),
-        query(':leave', [
-          style({ transform: 'translateX(0)', opacity: 1 }),
-          stagger(100, [
-            animate('500ms', style({ transform: 'translateX(100%)', opacity: 0 }))
-          ])
-        ], { optional: true })
-      ])
-    ])
-  ],
   templateUrl: './teamorders.component.html',
   styleUrls: ['./teamorders.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
