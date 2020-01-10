e3ba7ef741a39c9049073dfada1d2840a79fc839
emre1702 <emre1702@live.de>
2020-01-10 22:21:26 +0100

    Fix naming rule violation

--- TDS_Client/Manager/Lobby/Countdown.cs
+++ TDS_Client/Manager/Lobby/Countdown.cs
@@ -11,11 +11,11 @@ namespace TDS_Client.Manager.Lobby
 {
     internal static class Countdown
     {
-        private static DxText text;
-        private static TDSTimer countdownTimer;
-        private static int currentCountdownTime;
+        private static DxText _text;
+        private static TDSTimer _countdownTimer;
+        private static int _currentCountdownTime;
 
-        private static string[] countdownSounds = new string[] { "go", "1", "2", "3" };
+        private static string[] _countdownSounds = new string[] { "go", "1", "2", "3" };
 
         public static void Start()
         {
@@ -24,44 +24,44 @@ namespace TDS_Client.Manager.Lobby
                 End();
                 return;
             }
-            countdownTimer?.Kill();
-            currentCountdownTime = Settings.CountdownTime;
-            countdownTimer = new TDSTimer(Refresh, 1000, (uint)currentCountdownTime);
-            text = new DxText(currentCountdownTime.ToString(), 0.5f, 0.2f, 2f, Color.White, alignmentX: UIResText.Alignment.Centered, alignmentY: EAlignmentY.Center);
-            text.BlendScale(6f, 1000);
+            _countdownTimer?.Kill();
+            _currentCountdownTime = Settings.CountdownTime;
+            _countdownTimer = new TDSTimer(Refresh, 1000, (uint)_currentCountdownTime);
+            _text = new DxText(_currentCountdownTime.ToString(), 0.5f, 0.2f, 2f, Color.White, alignmentX: UIResText.Alignment.Centered, alignmentY: EAlignmentY.Center);
+            _text.BlendScale(6f, 1000);
             PlaySound();
         }
 
         public static void StartAfterwards(int timeremainingms)
         {
-            currentCountdownTime = (int)Math.Ceiling((double)timeremainingms / 1000);
-            countdownTimer?.Kill();
-            countdownTimer = new TDSTimer(() =>
+            _currentCountdownTime = (int)Math.Ceiling((double)timeremainingms / 1000);
+            _countdownTimer?.Kill();
+            _countdownTimer = new TDSTimer(() =>
             {
-                if (currentCountdownTime > 1)
-                    countdownTimer = new TDSTimer(Refresh, 1000, (uint)(currentCountdownTime - 1));
+                if (_currentCountdownTime > 1)
+                    _countdownTimer = new TDSTimer(Refresh, 1000, (uint)(_currentCountdownTime - 1));
                 Refresh();
-            }, (uint)(currentCountdownTime - timeremainingms), 1);
-            text = new DxText(currentCountdownTime.ToString(), 0.5f, 0.2f, 2f, Color.White, alignmentX: UIResText.Alignment.Centered, alignmentY: EAlignmentY.Center);
-            text.BlendScale(6f, 1000);
+            }, (uint)(_currentCountdownTime - timeremainingms), 1);
+            _text = new DxText(_currentCountdownTime.ToString(), 0.5f, 0.2f, 2f, Color.White, alignmentX: UIResText.Alignment.Centered, alignmentY: EAlignmentY.Center);
+            _text.BlendScale(6f, 1000);
             PlaySound();
         }
 
         public static void End(bool showGo = true)
         {
-            currentCountdownTime = 0;
+            _currentCountdownTime = 0;
             if (showGo)
             {
-                countdownTimer?.Kill();
-                if (text == null)
-                    text = new DxText("GO", 0.5f, 0.2f, 2f, Color.White, alignmentX: UIResText.Alignment.Centered, alignmentY: EAlignmentY.Center);
+                _countdownTimer?.Kill();
+                if (_text == null)
+                    _text = new DxText("GO", 0.5f, 0.2f, 2f, Color.White, alignmentX: UIResText.Alignment.Centered, alignmentY: EAlignmentY.Center);
                 else
                 {
-                    text.Text = "GO";
-                    text.SetScale(2f);
+                    _text.Text = "GO";
+                    _text.SetScale(2f);
                 }
                 PlaySound();
-                countdownTimer = new TDSTimer(Stop, 2000, 1);
+                _countdownTimer = new TDSTimer(Stop, 2000, 1);
             }       
             else 
                 Stop();
@@ -69,31 +69,31 @@ namespace TDS_Client.Manager.Lobby
 
         public static void Stop()
         {
-            text?.Remove();
-            text = null;
-            countdownTimer?.Kill();
-            countdownTimer = null;
+            _text?.Remove();
+            _text = null;
+            _countdownTimer?.Kill();
+            _countdownTimer = null;
         }
 
         private static void Refresh()
         {
-            if (--currentCountdownTime <= 0)
+            if (--_currentCountdownTime <= 0)
             {
                 End();
                 return;
             }
-            if (text == null)
+            if (_text == null)
                 return;
-            text.Text = currentCountdownTime.ToString();
-            text.SetScale(2f);
-            text.BlendScale(6f, 1000);
+            _text.Text = _currentCountdownTime.ToString();
+            _text.SetScale(2f);
+            _text.BlendScale(6f, 1000);
             PlaySound();
         }
 
         private static void PlaySound()
         {
-            if (countdownSounds.Length > currentCountdownTime)
-                MainBrowser.PlaySound(countdownSounds[currentCountdownTime]);
+            if (_countdownSounds.Length > _currentCountdownTime)
+                MainBrowser.PlaySound(_countdownSounds[_currentCountdownTime]);
         }
     }
 }
--- TDS_Client/Manager/Lobby/MapInfo.cs
+++ TDS_Client/Manager/Lobby/MapInfo.cs
@@ -6,23 +6,23 @@ namespace TDS_Client.Manager.Lobby
 {
     internal static class MapInfo
     {
-        public static string CurrentMap => mapInfo?.Text ?? "-";
+        public static string CurrentMap => _mapInfo?.Text ?? "-";
 
-        private static DxText mapInfo;
+        private static DxText _mapInfo;
 
         public static void SetMapInfo(string mapname)
         {
             //currentMap = mapname;
-            if (mapInfo == null)
-                mapInfo = new DxText(mapname, 0.01f, 0.995f, 0.2f, Color.White, alignmentX: RAGE.NUI.UIResText.Alignment.Left, alignmentY: EAlignmentY.Bottom);
+            if (_mapInfo == null)
+                _mapInfo = new DxText(mapname, 0.01f, 0.995f, 0.2f, Color.White, alignmentX: RAGE.NUI.UIResText.Alignment.Left, alignmentY: EAlignmentY.Bottom);
             else
-                mapInfo.Text = mapname;
+                _mapInfo.Text = mapname;
         }
 
         public static void RemoveMapInfo()
         {
-            mapInfo?.Remove();
-            mapInfo = null;
+            _mapInfo?.Remove();
+            _mapInfo = null;
         }
     }
 }
