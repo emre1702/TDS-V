2a219d5d1cab85df147a2a16d3819f83a3c7a6a7
emre1702 <emre1702@live.de>
2020-01-25 21:48:29 +0100

    Improve LocalServerSync.sh if path is missing for cd

--- LocalServerSync.sh
+++ LocalServerSync.sh
@@ -37,31 +37,24 @@ rsync -hmrtvzP ./TDS_Server.dll ./TDS_Server.pdb /cygdrive/c/RAGEMP/server-files
 
 echo -e "${SEPERATOR}"
 echo -e "Update ${LIGHTBLUE}serverside JS ${NOCOLOR}..."
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Server/JavaScript
-rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/packages/tds
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Server/JavaScript && rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/packages/tds
 
 echo -e "${SEPERATOR}"
 echo -e "Update ${LIGHTBLUE}clientside C# ${NOCOLOR}files ..."
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V
-rsync -hmrtvzP --delete --exclude="bin" --exclude="obj" --exclude="node_modules" --include="*/" --include='*.cs' --exclude='*' TDS_Client/. TDS_Common/. /cygdrive/c/RAGEMP/server-files/client_packages/cs_packages/TDS_Client/
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V && rsync -hmrtvzP --delete --exclude="bin" --exclude="obj" --exclude="node_modules" --include="*/" --include='*.cs' --exclude='*' TDS_Client/. TDS_Common/. /cygdrive/c/RAGEMP/server-files/client_packages/cs_packages/TDS_Client/
 
 echo -e "${SEPERATOR}"
 echo -e "Update ${LIGHTBLUE}clientside JS ${NOCOLOR}..."
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/JavaScript
-rsync -hmrtvzP --delete --include='*.js' --exclude='*' . /cygdrive/c/RAGEMP/server-files/client_packages
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/JavaScript && rsync -hmrtvzP --delete --include='*.js' --exclude='*' . /cygdrive/c/RAGEMP/server-files/client_packages
 
 echo -e "${SEPERATOR}"
 echo -e "Update clientside ${LIGHTBLUE}plain HTML ${NOCOLOR}CEF ..."
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window
-rsync -hmrtvzP --delete --exclude=".vscode" --exclude="angular" --exclude="node_modules" --exclude="*.json" --exclude="*cefminify*" --include="*/" --include='*index.*' --include='*.min.*' --include='*.ttf' --include="*.png" --include='*.jpg' --include='*.mp3' --include='*.wav' --include='*.ogg' --exclude='*' . /cygdrive/c/RAGEMP/server-files/client_packages/Window
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window && rsync -hmrtvzP --delete --exclude=".vscode" --exclude="angular" --exclude="node_modules" --exclude="*.json" --exclude="*cefminify*" --include="*/" --include='*index.*' --include='*.min.*' --include='*.ttf' --include="*.png" --include='*.jpg' --include='*.mp3' --include='*.wav' --include='*.ogg' --exclude='*' . /cygdrive/c/RAGEMP/server-files/client_packages/Window
 
 echo -e "${SEPERATOR}"
 echo -e "Update clientside ${LIGHTBLUE}Angular ${NOCOLOR}CEF ..."
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window/angular/dist/main
-rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/client_packages/Window/angular/main
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window/angular/dist/map-creator-object-choice
-rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/client_packages/Window/angular/map-creator-object-choice
-cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window/angular/dist/map-creator-vehicle-choice
-rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/client_packages/Window/angular/map-creator-vehicle-choice
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window/angular/dist/main && rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/client_packages/Window/angular/main
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window/angular/dist/map-creator-object-choice && rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/client_packages/Window/angular/map-creator-object-choice
+cd B:/Users/EmreKara/Desktop/Tools/GitHub/TDS-V/TDS_Client/Window/angular/dist/map-creator-vehicle-choice && rsync -hmrtvzP --delete . /cygdrive/c/RAGEMP/server-files/client_packages/Window/angular/map-creator-vehicle-choice
 
 cmd /k
