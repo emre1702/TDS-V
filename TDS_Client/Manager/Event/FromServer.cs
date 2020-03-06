a0f51b8209b165e53fd19b15c51837eac849362d
emre1702 <emre1702@live.de>
2020-03-06 19:30:00 +0100

    Lobby join: Fix wrong "GO" if round is already started

--- TDS_Client/Manager/Event/FromServer.cs
+++ TDS_Client/Manager/Event/FromServer.cs
@@ -296,7 +296,7 @@ namespace TDS_Client.Manager.Event
             Round.InFight = !Round.IsSpectator;
             Cam.DoScreenFadeIn(50);
             LobbyCam.StopCountdown();
-            Countdown.End(args.Length < 2 || Convert.ToUInt32(args[1]) != 0);
+            Countdown.End(args.Length < 2);
             RoundInfo.Start(args.Length >= 2 ? Convert.ToUInt32(args[1]) : 0);
             CustomEventManager.SetRoundStart(Round.IsSpectator);
         }
