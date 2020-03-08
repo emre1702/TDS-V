d885662e268b2f0c365b964e678ab640511fcad9
emre1702 <emre1702@live.de>
2020-03-08 11:37:42 +0100

    LobbySettings: Add some todos

--- TDS_Server_DB/Entity/LobbyEntities/Lobbies.cs
+++ TDS_Server_DB/Entity/LobbyEntities/Lobbies.cs
@@ -34,6 +34,23 @@ namespace TDS_Server_DB.Entity.LobbyEntities
         public bool IsOfficial { get; set; }
         public int SpawnAgainAfterDeathMs { get; set; }
         public DateTime CreateTimestamp { get; set; }
+        //Todo: Add SET_RUN_SPRINT_MULTIPLIER_FOR_PLAYER
+        //Todo: Add SET_SWIM_MULTIPLIER_FOR_PLAYER
+        //Todo: Add SET_AIR_DRAG_MULTIPLIER_FOR_PLAYERS_VEHICLE
+        //Todo: Add SET_PED_ACCURACY
+        //Todo: Add SET_PED_MIN_GROUND_TIME_FOR_STUNGUN
+        //Todo: Add SET_PED_SHOOT_RATE
+        //Todo: Add SET_PED_GRAVITY
+        //Todo: Add SET_PED_MAX_HEALTH
+        //Todo: Add SET_PED_CAN_BE_KNOCKED_OFF_VEHICLE
+        //Todo: Add SET_PED_CAN_RAGDOLL
+        //Todo: Add SET_GRAVITY_LEVEL
+        //Todo: Add SET_EXPLOSIVE_AMMO_THIS_FRAME
+        //Todo: Add SET_FIRE_AMMO_THIS_FRAME
+        //Todo: Add SET_EXPLOSIVE_MELEE_THIS_FRAME
+        //Todo: Add SET_SUPER_JUMP_THIS_FRAME
+        //Todo: Add SET_PED_INFINITE_AMMO
+        //Todo: Add SET_PED_INFINITE_AMMO_CLIP
 
         public virtual Players Owner { get; set; }
         public virtual LobbyRewards LobbyRewards { get; set; }
