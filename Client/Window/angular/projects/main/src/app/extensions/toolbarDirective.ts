292292a93a8536833504d366d0b4b35bff1dac3d
emre1702 <emre1702@live.de>
2020-04-23 03:22:17 +0200

    [BUG] Fix toolbar minimize (doubleclick doesn't work for CEF, use workaround)

--- Client/Window/angular/projects/main/src/app/extensions/toolbarDirective.ts
+++ Client/Window/angular/projects/main/src/app/extensions/toolbarDirective.ts
@@ -8,12 +8,30 @@ export class ToolbarDirective {
 
     @Input() closeOnDoubleClick: any;
     private origDisplay: string;
+    private touchtime = 0;
 
     constructor() {}
 
-    @HostListener('dblclick', ['$event'])
+    @HostListener('click', ['$event'])
     onDoubleClick(e: MouseEvent) {
         e.preventDefault();
+        if (this.touchtime == 0) {
+            // set first click
+            this.touchtime = new Date().getTime();
+        } else {
+            // compare first click to this click and see if they occurred within double click threshold
+            if (((new Date().getTime()) - this.touchtime) < 800) {
+                // double click occurred
+                this.doubleClicked();
+                this.touchtime = 0;
+            } else {
+                // not a double click so set as a new first click
+                this.touchtime = new Date().getTime();
+            }
+        }
+    }
+
+    private doubleClicked() {
         if (this.closeOnDoubleClick.style.display !== "none") {
             this.origDisplay = this.closeOnDoubleClick.style.display;
             this.closeOnDoubleClick.style.display = "none";
