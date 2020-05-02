a8d18c042749e538369f832140fb8047007b947c
emre1702 <emre1702@live.de>
2020-05-03 01:29:34 +0200

    [CODE] Add GangLevelsHandler, improve code

---
+++ Server/Handler/GangSystem/GangLevelsHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using TDS_Server.Database.Entity;
+using TDS_Server.Database.Entity.GangEntities;
+
+namespace TDS_Server.Handler.GangSystem
+{
+    public class GangLevelsHandler
+    {
+        public Dictionary<byte, GangLevelSettings> Levels = new Dictionary<byte, GangLevelSettings>();
+
+        public GangLevelsHandler(TDSDbContext dbContext)
+        {
+            LoadGangLevels(dbContext);
+        }
+
+        private void LoadGangLevels(TDSDbContext dbContext)
+        {
+            Levels = dbContext.GangLevelSettings.AsNoTracking().ToDictionary(l => l.Level, l => l);
+        }
+    }
+}
