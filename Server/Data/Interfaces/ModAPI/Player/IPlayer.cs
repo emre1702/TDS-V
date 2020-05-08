62be1e17aac3970af0042a79a3f1febfc1e61fe5
emre1702 <emre1702@live.de>
2020-05-08 23:10:05 +0200

    [CODE] Implement GTANetworkAPI.Player methods to IPlayer

--- Server/Data/Interfaces/ModAPI/Player/IPlayer.cs
+++ Server/Data/Interfaces/ModAPI/Player/IPlayer.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using TDS_Server.Data.Enums;
 using TDS_Server.Data.Interfaces.ModAPI.Ped;
 using TDS_Server.Data.Interfaces.ModAPI.Vehicle;
+using TDS_Server.Data.Models;
 using TDS_Shared.Data.Enums;
 using TDS_Shared.Data.Models.GTA;
 
@@ -36,7 +39,7 @@ namespace TDS_Server.Data.Interfaces.ModAPI.Player
         void SendEvent(string eventName, params object[] args);
         void SetHealth(int health);
         void SetSkin(PedHash pedHash);
-        void SetVoiceTo(ITDSPlayer target, bool on);
+        void SetVoiceTo(ITDSPlayer target, bool toggle);
         void Spawn(Position3D position, float heading = 0);
         void GiveWeapon(WeaponHash hash, int ammo = 0);
         void SetWeaponAmmo(WeaponHash hash, int ammo);
@@ -49,5 +52,60 @@ namespace TDS_Server.Data.Interfaces.ModAPI.Player
         void WarpOutOfVehicle();
         void SetIntoVehicle(ITDSVehicle vehicle, int seat);
         void SetClothes(int slot, int drawable, int texture);
+
+        string Nametag { get; set; }
+        int Ping { get; }
+        bool IsMediaStreamEnabled { get; }
+        float Heading { get; set; }
+        Position3D Velocity { get; set; }
+        bool IsCeFenabled { get; }
+        WeaponHash[] Weapons { get; }
+        string Address { get; }
+        Position3D AimingPoint { get; }
+        bool IsAiming { get; }
+        bool IsShooting { get; }
+        bool IsReloading { get; }
+        bool IsInCover { get; }
+        bool IsOnLadder { get; }
+        HeadBlend HeadBlend { get; set; }
+
+        void Ban(string reason);
+        void Ban();
+        void ClearAccessory(int slot);
+        void ClearDecorations();
+        void Eval(string code);
+        int GetAccessoryDrawable(int slot);
+        int GetAccessoryTexture(int slot);
+        int GetClothesDrawable(int slot);
+        int GetClothesTexture(int slot);
+        float GetFaceFeature(int slot);
+        Color GetHeadBlendPaletteColor(int slot);
+        HeadOverlay GetHeadOverlay(int overlayId);
+        T GetOwnSharedData<T>(string key);
+        int GetWeaponAmmo(WeaponHash weapon);
+        bool HasOwnSharedData(string key);
+        void Kick();
+        void KickSilent(string reason);
+        void KickSilent();
+        void PlayScenario(string scenarioName);
+        void RemoveDecoration(Decoration decoration);
+        void RemoveHeadBlendPaletteColor(int slot);
+        void RemoveWeapon(WeaponHash weapon);
+        void ResetOwnSharedData(string key);
+        void SendPictureNotificationToPlayer(string body, string pic, int flash, int iconType, string sender, string subject);
+        void SetAccessories(int slot, int drawable, int texture);
+        void SetClothes(Dictionary<int, ComponentVariation> clothes);
+        void SetCustomization(bool gender, HeadBlend headBlend, byte eyeColor, byte hairColor, byte highlightColor, float[] faceFeatures, 
+            Dictionary<int, HeadOverlay> headOverlays, Decoration[] decorations);
+        void SetDecoration(Decoration[] decoration);
+        void SetDecoration(Decoration decoration);
+        void SetFaceFeature(int slot, float scale);
+        void SetHeadBlendPaletteColor(int slot, Color color);
+        void SetHeadOverlay(int overlayId, HeadOverlay headOverlay);
+        void SetOwnSharedData(Dictionary<string, object> value);
+        void SetOwnSharedData(string key, object value);
+        void SetSkin(uint newSkin);
+        void TriggerEvent(string eventName, params object[] args);
+        void UpdateHeadBlend(float shapeMix, float skinMix, float thirdMix);
     }
 }
--- Server/RAGEAPI/Player/Player.cs
+++ Server/RAGEAPI/Player/Player.cs
@@ -1,8 +1,11 @@
-﻿using GTANetworkAPI;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using TDS_Server.Data.Interfaces;
 using TDS_Server.Data.Interfaces.ModAPI.Player;
-using TDS_Server.Data.Interfaces.ModAPI.Vehicle;
+using TDS_Server.Data.Models;
 using TDS_Server.RAGEAPI.Extensions;
+using TDS_Shared.Data.Enums;
 using TDS_Shared.Data.Models.GTA;
 
 namespace TDS_Server.RAGEAPI.Player
@@ -24,10 +27,14 @@ namespace TDS_Server.RAGEAPI.Player
             _name = _instance.Name;
             SocialClubId = _instance.SocialClubId;
             SocialClubName = _instance.SocialClubName;
+            IPAddress = _instance.Address;
+            Serial = _instance.Serial;
         }
 
         public ulong SocialClubId { get; }
         public string SocialClubName { get; }
+        public string IPAddress { get; }
+        public string Serial { get; }
 
         public ushort RemoteId => _instance.Handle.Value;
         public int Transparency
@@ -46,16 +53,12 @@ namespace TDS_Server.RAGEAPI.Player
         }
         public int VehicleSeat => _instance.VehicleSeat;
 
-        public string IPAddress => _instance.Address;
-
-        public string Serial => _instance.Serial;
-
         public bool Dead => _instance.Dead;
 
-        public TDS_Shared.Data.Enums.WeaponHash CurrentWeapon
+        public WeaponHash CurrentWeapon
         {
             get => (TDS_Shared.Data.Enums.WeaponHash)_instance.CurrentWeapon;
-            set => NAPI.Player.SetPlayerCurrentWeapon(_instance, (GTANetworkAPI.WeaponHash)value);
+            set => GTANetworkAPI.NAPI.Player.SetPlayerCurrentWeapon(_instance, (GTANetworkAPI.WeaponHash)value);
 
         }
         public int Armor
@@ -75,24 +78,69 @@ namespace TDS_Server.RAGEAPI.Player
 
         public bool IsDead => _instance.Dead;
 
+        public string Nametag 
+        { 
+            get => _instance.Nametag;
+            set => _instance.Nametag = value;
+        }
+
+        public int Ping => _instance.Ping;
+
+        public bool IsMediaStreamEnabled => _instance.IsMediaStreamEnabled;
+
+        public float Heading 
+        { 
+            get => _instance.Heading; 
+            set => _instance.Heading = value; 
+        }
+        public Position3D Velocity 
+        { 
+            get => _instance.Velocity.ToTDS(); 
+            set => _instance.Velocity = value.ToMod(); 
+        }
+
+        public bool IsCeFenabled => _instance.IsCeFenabled;
+
+        public WeaponHash[] Weapons => _instance.Weapons.Select(w => (WeaponHash)w).ToArray();
+
+        public string Address => _instance.Address;
+
+        public Position3D AimingPoint => _instance.AimingPoint.ToTDS();
+
+        public bool IsAiming => _instance.IsAiming;
+
+        public bool IsShooting => _instance.IsShooting;
+
+        public bool IsReloading => _instance.IsReloading;
+
+        public bool IsInCover => _instance.IsInCover;
+
+        public bool IsOnLadder => _instance.IsOnLadder;
+
+        public HeadBlend HeadBlend 
+        { 
+            get => _instance.HeadBlend.ToTDS(); 
+            set => _instance.HeadBlend = value.ToMod(); 
+        }
+
         public void SetHealth(int health)
         {
-            NAPI.Player.SetPlayerHealth(_instance, health);
+            GTANetworkAPI.NAPI.Player.SetPlayerHealth(_instance, health);
         }
 
         public void SendEvent(string eventName, params object[] args)
         {
-            NAPI.ClientEvent.TriggerClientEvent(_instance, eventName, args);
+            GTANetworkAPI.NAPI.ClientEvent.TriggerClientEvent(_instance, eventName, args);
         }
 
         public void Spawn(Position3D position, float heading = 0)
         {
-            NAPI.Player.SpawnPlayer(_instance, position.ToVector3(), heading);
+            GTANetworkAPI.NAPI.Player.SpawnPlayer(_instance, position.ToMod(), heading);
         }
 
-        public void SetSkin(Data.Enums.PedHash pedHash)
+        public void SetSkin(PedHash pedHash)
         {
-            NAPI.Player.SetPlayerSkin(_instance, (PedHash)pedHash);
+            GTANetworkAPI.NAPI.Player.SetPlayerSkin(_instance, (GTANetworkAPI.PedHash)pedHash);
         }
 
         public void SetVoiceTo(ITDSPlayer target, bool on)
@@ -101,14 +149,14 @@ namespace TDS_Server.RAGEAPI.Player
                 return;
 
             if (on)
-                NAPI.Player.EnablePlayerVoiceTo(_instance, targetModPlayer._instance);
+                GTANetworkAPI.NAPI.Player.EnablePlayerVoiceTo(_instance, targetModPlayer._instance);
             else
-                NAPI.Player.DisablePlayerVoiceTo(_instance, targetModPlayer._instance);
+                GTANetworkAPI.NAPI.Player.DisablePlayerVoiceTo(_instance, targetModPlayer._instance);
         }
 
         public void Kick(string reason)
         {
-            NAPI.Player.KickPlayer(_instance, reason);
+            GTANetworkAPI.NAPI.Player.KickPlayer(_instance, reason);
         }
 
         public bool Equals(IPlayer? other)
@@ -141,14 +189,14 @@ namespace TDS_Server.RAGEAPI.Player
             _instance.RemoveAllWeapons();
         }
 
-        public void GiveWeapon(TDS_Shared.Data.Enums.WeaponHash hash, int ammo = 0)
+        public void GiveWeapon(WeaponHash hash, int ammo = 0)
         {
-            _instance.GiveWeapon((WeaponHash)hash, ammo);
+            _instance.GiveWeapon((GTANetworkAPI.WeaponHash)hash, ammo);
         }
 
-        public void SetWeaponAmmo(TDS_Shared.Data.Enums.WeaponHash hash, int ammo)
+        public void SetWeaponAmmo(WeaponHash hash, int ammo)
         {
-            _instance.SetWeaponAmmo((WeaponHash)hash, ammo);
+            _instance.SetWeaponAmmo((GTANetworkAPI.WeaponHash)hash, ammo);
         }
 
         public void PlayAnimation(string animDict, string animName, int flag)
@@ -182,5 +230,118 @@ namespace TDS_Server.RAGEAPI.Player
         {
             _instance.SetClothes(slot, drawable, texture);
         }
+
+        public void Ban(string reason)
+            => _instance.Ban(reason);
+
+        public void Ban()
+            => _instance.Ban();
+
+        public void ClearAccessory(int slot)
+            => _instance.ClearAccessory(slot);
+
+        public void ClearDecorations()
+            => _instance.ClearDecorations();
+
+        public void Eval(string code)
+            => _instance.Eval(code);
+
+        public int GetAccessoryDrawable(int slot)
+            => _instance.GetAccessoryDrawable(slot);
+
+        public int GetAccessoryTexture(int slot)
+            => _instance.GetAccessoryTexture(slot);
+
+        public int GetClothesDrawable(int slot)
+            => _instance.GetClothesDrawable(slot);
+
+        public int GetClothesTexture(int slot)
+            => _instance.GetClothesTexture(slot);
+
+        public float GetFaceFeature(int slot)
+            => _instance.GetFaceFeature(slot);
+
+        public Color GetHeadBlendPaletteColor(int slot)
+            => _instance.GetHeadBlendPaletteColor(slot).ToTDS();
+
+        public HeadOverlay GetHeadOverlay(int overlayId)
+            => _instance.GetHeadOverlay(overlayId).ToTDS();
+
+        public T GetOwnSharedData<T>(string key)
+            => _instance.GetOwnSharedData<T>(key);
+
+        public int GetWeaponAmmo(WeaponHash weapon)
+            => _instance.GetWeaponAmmo((GTANetworkAPI.WeaponHash)weapon);
+
+        public bool HasOwnSharedData(string key)
+            => _instance.HasOwnSharedData(key);
+
+        public void Kick()
+            => _instance.Kick();
+
+        public void KickSilent(string reason)
+            => _instance.KickSilent(reason);
+
+        public void KickSilent()
+            => _instance.KickSilent();
+
+        public void PlayScenario(string scenarioName)
+            => _instance.PlayScenario(scenarioName);
+
+        public void RemoveDecoration(Decoration decoration)
+            => _instance.RemoveDecoration(decoration.ToMod());
+
+        public void RemoveHeadBlendPaletteColor(int slot)
+            => _instance.RemoveHeadBlendPaletteColor(slot);
+
+        public void RemoveWeapon(WeaponHash weapon)
+            => _instance.RemoveWeapon((GTANetworkAPI.WeaponHash)weapon);
+
+        public void ResetOwnSharedData(string key)
+            => _instance.ResetOwnSharedData(key);
+
+        public void SendPictureNotificationToPlayer(string body, string pic, int flash, int iconType, string sender, string subject)
+            => _instance.SendPictureNotificationToPlayer(body, pic, flash, iconType, sender, subject);
+
+        public void SetAccessories(int slot, int drawable, int texture)
+            => _instance.SetAccessories(slot, drawable, texture);
+
+        public void SetClothes(Dictionary<int, ComponentVariation> clothes)
+            => _instance.SetClothes(clothes.ToDictionary(c => c.Key, c => c.Value.ToMod()));
+
+        public void SetCustomization(bool gender, HeadBlend headBlend, byte eyeColor, byte hairColor, byte highlightColor, float[] faceFeatures, 
+            Dictionary<int, HeadOverlay> headOverlays, Decoration[] decorations)
+            => _instance.SetCustomization(gender, headBlend.ToMod(), eyeColor, hairColor, highlightColor, faceFeatures, 
+                headOverlays.ToDictionary(h => h.Key, h => h.Value.ToMod()), decorations.Select(d => d.ToMod()).ToArray());
+
+        public void SetDecoration(Decoration[] decoration)
+            => _instance.SetDecoration(decoration.Select(d => d.ToMod()).ToArray());
+
+        public void SetDecoration(Decoration decoration)
+            => _instance.SetDecoration(decoration.ToMod());
+
+        public void SetFaceFeature(int slot, float scale)
+            => _instance.SetFaceFeature(slot, scale);
+
+        public void SetHeadBlendPaletteColor(int slot, Color color)
+            => _instance.SetHeadBlendPaletteColor(slot, color.ToMod());
+
+        public void SetHeadOverlay(int overlayId, HeadOverlay headOverlay)
+            => _instance.SetHeadOverlay(overlayId, headOverlay.ToMod());
+
+        public void SetOwnSharedData(Dictionary<string, object> value)
+            => _instance.SetOwnSharedData(value);
+
+        public void SetOwnSharedData(string key, object value)
+            => _instance.SetOwnSharedData(key, value);
+
+        public void SetSkin(uint newSkin)
+            => _instance.SetSkin(newSkin);
+
+        public void TriggerEvent(string eventName, params object[] args)
+            => _instance.TriggerEvent(eventName, args);
+
+        public void UpdateHeadBlend(float shapeMix, float skinMix, float thirdMix)
+            => _instance.UpdateHeadBlend(shapeMix, skinMix, thirdMix);
     }
 }
