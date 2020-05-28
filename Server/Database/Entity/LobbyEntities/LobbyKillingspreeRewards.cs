11387979eb663e1de00eb8734106d0198932992c
emre1702 <emre1702@live.de>
2020-05-28 07:35:45 +0200

    [Change] Remove "OnlyArmor" and "OnlyHealth" from killingspree rewards

--- Server/Database/Entity/LobbyEntities/LobbyKillingspreeRewards.cs
+++ Server/Database/Entity/LobbyEntities/LobbyKillingspreeRewards.cs
@@ -8,8 +8,6 @@
         public short KillsAmount { get; set; }
         public virtual Lobbies Lobby { get; set; }
         public int LobbyId { get; set; }
-        public short? OnlyArmor { get; set; }
-        public short? OnlyHealth { get; set; }
 
         #endregion Public Properties
     }
---
+++ Server/Database/Migrations/20200528053412_LobbyKillingspreeRewards_RemoveOnlyHealthOnlyArmor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TDS_Shared.Data.Enums;
+using TDS_Shared.Data.Enums.Challenge;
+
+namespace TDS_Server.Database.Migrations
+{
+    public partial class LobbyKillingspreeRewards_RemoveOnlyHealthOnlyArmor : Migration
+    {
+        #region Protected Methods
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "OnlyArmor",
+                table: "KillingspreeRewards",
+                type: "smallint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "OnlyHealth",
+                table: "KillingspreeRewards",
+                type: "smallint",
+                nullable: true);
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OnlyArmor",
+                table: "KillingspreeRewards");
+
+            migrationBuilder.DropColumn(
+                name: "OnlyHealth",
+                table: "KillingspreeRewards");
+        }
+
+        #endregion Protected Methods
+    }
+}
