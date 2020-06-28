636bc9cfda04b3485cbbac6045767cc0df871e79
emre1702 <emre1702@live.de>
2020-06-28 07:37:01 +0200

    [BUG] Fix new "not enough teams" validation if teams or maps not loaded yet (they only load if you open the menus)

--- Client/Window/angular/projects/main/src/app/components/lobbychoice/custom-lobby/validators/notEnoughTeamsValidator.ts
+++ Client/Window/angular/projects/main/src/app/components/lobbychoice/custom-lobby/validators/notEnoughTeamsValidator.ts
@@ -2,20 +2,24 @@ import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { MapDataDto } from '../../../mapvoting/models/mapDataDto';
 import { CustomLobbyTeamData } from '../../models/custom-lobby-team-data';
 import { Constants } from 'projects/main/src/app/constants';
+import { MapType } from 'projects/main/src/app/enums/maptype.enum';
 
 export function notEnoughTeamsValidator(selectedMapsGetter: () => MapDataDto[], teamsGetter: () => CustomLobbyTeamData[])
     : ValidatorFn {
     return (control: AbstractControl): ValidationErrors => {
         const teams = teamsGetter();    // teams with spectators
-        if (!teams)
-            return { ["notenoughteams"]: ""};
+        let teamsLength = 0;
+        if (teams)
+            teamsLength = teams.length;
+        else    // teams is null if the window was never opened
+            teamsLength = 3;    // Default
 
-        const selectedMaps = selectedMapsGetter();
+        let selectedMaps = selectedMapsGetter();
         if (!selectedMaps)
-            return { ["notenoughteams"]: "?"};
+            selectedMaps = [{ 0: -1, 2: MapType.Normal, 1: "", 3: {7: "", 9: ""}, 4: "", 5: 0}];
 
         for (const selectedMap of selectedMaps) {
-            if (teams.length - 1 < Constants.MIN_TEAMS_PER_TYPE[selectedMap[2]]) {
+            if (teamsLength - 1 < Constants.MIN_TEAMS_PER_TYPE[selectedMap[2]]) {
                 return { ["notenoughteams"]: selectedMap };
             }
         }
