203e9d59224c3fd110040c4844c3dfbe413fbe44
emre1702 <emre1702@live.de>
2020-06-30 23:04:54 +0200

    [CODE] Add migration for info settings

---
+++ Server/Database/Migrations/20200630210357_PlayerSettings_Infos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TDS_Shared.Data.Enums;
+using TDS_Shared.Data.Enums.Challenge;
+
+namespace TDS_Server.Database.Migrations
+{
+    public partial class PlayerSettings_Infos : Migration
+    {
+        #region Protected Methods
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowCursorInfo",
+                table: "PlayerSettings");
+
+            migrationBuilder.DropColumn(
+                name: "ShowLobbyLeaveInfo",
+                table: "PlayerSettings");
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowCursorInfo",
+                table: "PlayerSettings",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowLobbyLeaveInfo",
+                table: "PlayerSettings",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        #endregion Protected Methods
+    }
+}
