c3e36b7e8ad077d5f1b600a2a34e8cbfd8fcf587
emre1702 <emre1702@live.de>
2020-08-29 12:54:37 +0200

    [CODE] Add floating damage info

---
+++ Client/TypeScript/src/entities/draw/floating-damage-info.entity.ts
@@ -0,0 +1,50 @@
+﻿import SettingsService from "../../services/settings/settings.service";
+import game from "natives";
+import alt from "alt-client";
+import DxService from "../../services/draw/dx.service";
+
+export class FloatingDamageInfo {
+    get removeAtHandler(): boolean {
+        return this._removeAtHandler;
+    }
+
+    private text: DxText;
+    private targetPos: alt.Vector3;
+
+    private _removeAtHandler: boolean;
+    private startMs: number;
+
+    constructor(target: alt.Player, private damage: number, private settingsService: SettingsService, private dxService: DxService) {
+        this.startMs = Date.now();
+        this.targetPos = target.pos;
+    }
+
+    updatePosition(currentMs: number) {
+        const elapsedMs = currentMs - this.startMs; 
+        if (elapsedMs > this.settingsService.playerSettings.ShowFloatingDamageInfoDurationMs) {
+            this._removeAtHandler = true;
+            this.remove();
+            return;
+        }
+
+        let [, screenX, screenY] = game.getScreenCoordFromWorldCoord(this.targetPos.x, this.targetPos.y, this.targetPos.z, 0, 0);
+        const percentage = elapsedMs / this.settingsService.playerSettings.ShowFloatingDamageInfoDurationMs;
+        screenY -= 0.2 * percentage;
+
+        const scale = 0.4 - (0.3 * percentage);
+        const color = new alt.RGBA(220, 220, 200, 255 - (255 * percentage));
+
+        if (!this.text) {
+            this.text = new DxText(this.dxService, this.damage, screenX, screenY, scale, color, alignmentX: AlignmentX.Center, alignmentY: AlignmentY.Bottom, dropShadow: false, outline: true);
+        } else {
+            this.text.setRelativeY(screenY);
+        }
+    }
+
+    remove() {
+        if (this.text) {
+            this.text.remove();
+            this.text = undefined;
+        }
+    }
+}
---
+++ Client/TypeScript/src/services/draw/floating-damage-info.service.ts
@@ -0,0 +1,63 @@
+﻿import { injectable, inject } from "inversify";
+import { Player } from "alt-client";
+import { FloatingDamageInfo } from "../../entities/draw/floating-damage-info.entity";
+import DIIdentifier from "../../datas/enums/dependency-injection/di-identifier.enum";
+import SettingsService from "../settings/settings.service";
+import DxService from "./dx.service";
+import EventsService from "../events/events.service";
+import { clearEveryTick } from "alt-client";
+import { everyTick } from "alt-client";
+
+@injectable()
+export class FloatingDamageInfoService {
+
+    private damageInfos: FloatingDamageInfo[] = [];
+
+    private updateAllPositionTickId: number;
+
+    constructor(
+        @inject(DIIdentifier.EventsService) eventsService: EventsService,
+        @inject(DIIdentifier.SettingsService) private settingsService: SettingsService,
+        @inject(DIIdentifier.DxService) private dxService: DxService
+    ) {
+        eventsService.onInFightStatusChanged.on(this.onInFightStatusChanged.bind(this));
+    }
+
+    add(target: Player, damage: number) {
+        const info = new FloatingDamageInfo(target, damage, this.settingsService, this.dxService);
+        this.damageInfos.push(info);
+    }
+
+    private clear() {
+        for (const info of this.damageInfos) {
+            info.remove();
+        }
+        this.damageInfos.length = 0;
+    }
+
+    private updateAllPosition() {
+        const currentMs = Date.now();
+
+        for (let i = this.damageInfos.length - 1; i >= 0; --i) {
+            const damageInfo = this.damageInfos[i];
+            if (damageInfo.removeAtHandler)
+                this.damageInfos.splice(i, 1);
+            else
+                damageInfo.updatePosition(currentMs);
+        }
+    }
+
+    private onInFightStatusChanged(inFight: boolean) {
+        if (this.updateAllPositionTickId !== undefined) {
+            clearEveryTick(this.updateAllPositionTickId);
+            this.updateAllPositionTickId = undefined;
+        }
+
+        if (inFight) {
+            this.updateAllPositionTickId = everyTick(this.updateAllPosition);
+        } else {
+            this.clear();
+        }
+    }
+
+}
