58daa92368f1bc9217e94015c4142f4771636bbb
emre1702 <emre1702@live.de>
2020-08-31 01:49:08 +0200

    [CODE] Add entity extensions

---
+++ Client/TypeScript/src/entities/extensions/entity.extension.ts
@@ -0,0 +1,16 @@
+﻿import { Entity } from "alt-client"
+
+declare module "alt-client" {
+    interface Entity {
+        exists(): boolean;
+        isStreamed(): boolean;
+    }
+}
+
+Entity.prototype.exists = function () {
+    return (this as Entity).valid;
+}
+
+Entity.prototype.isStreamed = function () {
+    return !!(this as Entity).scriptID;
+}
