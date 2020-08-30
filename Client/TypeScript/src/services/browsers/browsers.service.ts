53f97782abfd54b6174d631aac6b3fab90284781
emre1702 <emre1702@live.de>
2020-08-31 01:46:00 +0200

    [CODE] Add browsers service

--- Client/TypeScript/src/services/browsers/browsers.service.ts
+++ Client/TypeScript/src/services/browsers/browsers.service.ts
@@ -3,6 +3,10 @@ import { inject, injectable } from "inversify";
 import RemoteEventsSender from "../events/remote-events-sender.service";
 import AngularBrowserService from "./angular-browser.service";
 import DIIdentifier from "../../datas/enums/dependency-injection/di-identifier.enum";
+import PlainMainBrowserService from "./plain-main-browser.service";
+import { on } from "alt-client";
+import FromBrowserEvent from "../../datas/enums/events/from-browser-event.enum";
+import RegisterLoginBrowserService from "./register-login-browser.service";
 
 @injectable()
 export default class BrowsersService {
@@ -11,6 +15,8 @@ export default class BrowsersService {
     mapCreatorObjectChoice: MapCreatorObjectChoiceBrowserService;
     mapCreatorVehicleChoice: MapCreatorVehicleChoiceBrowserService;
     plainMain: PlainMainBrowserService;
+    registerLogin: RegisterLoginBrowserService;
+
     inInput: boolean;
 
     constructor(
@@ -19,5 +25,12 @@ export default class BrowsersService {
     ) {
         this.angular = new AngularBrowserService(eventsService);
 
+
+        this.plainMain = new PlainMainBrowserService(eventsService);
+        this.registerLogin = new RegisterLoginBrowserService(eventsService);
+
+        //Todo: Does alt.on even work for browser? Or do I need to use browser.on?
+        on(FromBrowserEvent.InputStarted, () => this.inInput = true);
+        on(FromBrowserEvent.InputStarted, () => this.inInput = false);
     }
 }
