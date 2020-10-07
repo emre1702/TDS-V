d6317230365cd4b9fd4e0682baadc1c112c37e45
emre1702 <emre1702@live.de>
2020-10-08 00:39:00 +0200

    [CHANGE] Fix & improve nametag

---
+++ Server/Database/Migrations/20201007223714_Nametag_Improvements.cs
@@ -0,0 +1,30 @@
+﻿using GTANetworkAPI;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TDS_Shared.Data.Enums;
+using TDS_Shared.Data.Enums.Challenge;
+
+namespace TDS_Server.Database.Migrations
+{
+    public partial class Nametag_Improvements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+               table: "ServerSettings",
+               keyColumn: "Id",
+               keyValue: (short)1,
+               columns: new[] { "NametagMaxDistance" },
+               values: new object[] { 1000f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+               table: "ServerSettings",
+               keyColumn: "Id",
+               keyValue: (short)1,
+               columns: new[] { "NametagMaxDistance" },
+               values: new object[] { 80f });
+        }
+    }
+}
