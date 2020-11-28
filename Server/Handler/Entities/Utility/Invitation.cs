4f9badc9c0e3ed205bf9e9cbd36ea6d10467b942
emre1702 <emre1702@live.de>
2020-11-29 00:37:38 +0100

    [BUG] Prevent accepting invitation multiple times (if fast enough)

--- Server/Handler/Entities/Utility/Invitation.cs
+++ Server/Handler/Entities/Utility/Invitation.cs
@@ -3,18 +3,21 @@ using System;
 using TDS.Server.Data.Abstracts.Entities.GTA;
 using TDS.Server.Data.Defaults;
 using TDS.Server.Data.Enums;
+using TDS.Server.Data.Interfaces;
 using TDS.Server.Data.Models;
 using TDS.Server.Handler.Extensions;
 using TDS.Shared.Core;
 
 namespace TDS.Server.Handler.Entities.Utility
 {
-    public class Invitation
+    public class Invitation : IInvitation
     {
         public ITDSPlayer? Sender { get; set; }
         public ITDSPlayer Target { get; set; }
         public InvitationDto Dto { get; set; }
         public InvitationType Type { get; set; }
+        public bool Withdrawn { get; private set; }
+        public bool Reacted { get; private set; }
 
         private static ulong _idCounter = 0;
         private readonly InvitationsHandler _invitationsHandler;
@@ -44,40 +47,48 @@ namespace TDS.Server.Handler.Entities.Utility
 
             invitationsHandler.Add(this);
 
-            var dataJson = Serializer.ToBrowser(Dto);
-            NAPI.Task.RunSafe(() => 
-                target.TriggerBrowserEvent(ToBrowserEvent.AddInvitation, dataJson));
+            Show();
         }
 
         public bool RemoveOnLobbyLeave { get; set; }
 
         public void Accept()
         {
+            if (Reacted)
+                return;
             if (!Target.LoggedIn)
                 return;
+            Reacted = true;
             _onAccept?.Invoke(Target, Sender, this);
         }
 
         public void Reject()
         {
+            if (Reacted)
+                return;
             if (!Target.LoggedIn)
                 return;
+            Reacted = true;
             _onReject?.Invoke(Target, Sender, this);
         }
 
-        public void Resend()
+        public void Show()
         {
+            Reacted = false;
             var dataJson = Serializer.ToBrowser(Dto);
-            NAPI.Task.RunSafe(() => 
+            NAPI.Task.RunSafe(() =>
                 Target.TriggerBrowserEvent(ToBrowserEvent.AddInvitation, dataJson));
         }
 
         public void Withdraw()
         {
+            if (Withdrawn)
+                return;
+            Withdrawn = true;
             _invitationsHandler.Remove(this);
             if (!Target.LoggedIn)
                 return;
-            NAPI.Task.RunSafe(() => 
+            NAPI.Task.RunSafe(() =>
                 Target.TriggerBrowserEvent(ToBrowserEvent.RemoveInvitation, Dto.Id));
         }
     }
