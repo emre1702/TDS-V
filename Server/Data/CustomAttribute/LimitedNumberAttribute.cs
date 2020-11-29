7429e37f51230f02ef1418797f3a23e4a97985e2
emre1702 <emre1702@live.de>
2020-11-30 00:56:26 +0100

    [CODE] Add LimitedNumber attribute for min and max validation in commands

---
+++ Server/Data/CustomAttribute/LimitedNumberAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TDS.Server.Data.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class LimitedNumberAttribute : Attribute
+    {
+        public uint MinValue { get; set; } = 0;
+        public uint MaxValue { get; set; } = uint.MaxValue;
+    }
+}
