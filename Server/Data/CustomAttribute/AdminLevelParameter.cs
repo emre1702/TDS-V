f24955dcc3c75a53fb70683cbb5cfdbb7fcfc7c4
emre1702 <emre1702@live.de>
2020-11-30 00:55:39 +0100

    [CODE] Add AdminLevelParameter for command validations

---
+++ Server/Data/CustomAttribute/AdminLevelParameter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TDS.Server.Data.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class AdminLevelParameterAttribute : Attribute
+    {
+    }
+}
