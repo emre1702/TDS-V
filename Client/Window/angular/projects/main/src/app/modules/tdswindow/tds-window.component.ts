54e58f038fea13ddb8d81db9a47dd53a7c42456b
emre1702 <emre1702@live.de>
2020-12-23 19:32:51 +0100

    [CHANGE] Improve window styles

--- Client/Window/angular/projects/main/src/app/modules/tdswindow/tds-window.component.ts
+++ Client/Window/angular/projects/main/src/app/modules/tdswindow/tds-window.component.ts
@@ -1,51 +1,43 @@
 import { Component, OnInit, Input, ChangeDetectorRef, Output, EventEmitter, ViewChild, TemplateRef, OnDestroy } from '@angular/core';
 import { SettingsService } from '../../services/settings.service';
 import { MatMenuPanel } from '@angular/material/menu';
-import { SettingsThemeIndex } from '../userpanel/userpanel-settings-normal/enums/settings-theme-index.enum';
+import { SettingsThemeIndex } from '../../components/userpanel/userpanel-settings-normal/enums/settings-theme-index.enum';
 
 @Component({
     // tslint:disable-next-line: component-selector
-    selector: 'tdswindow',
-    templateUrl: './tdswindow.component.html',
-    styleUrls: [
-        './styles/window/window.design1.scss',
-
-        './styles/toolbar/toolbar.design1.scss',
-        './styles/toolbar/toolbar.design2.scss'
-    ]
+    selector: 'tds-window',
+    templateUrl: './tds-window.component.html',
+    styleUrls: ['./styles/window/window.design1.scss', './styles/toolbar/toolbar.design1.scss', './styles/toolbar/toolbar.design2.scss'],
 })
 export class TDSWindowComponent implements OnInit, OnDestroy {
-
     private _isLoading: boolean;
     get isLoading(): boolean {
         return this._isLoading;
     }
 
-    @Input("isLoading")
+    @Input('isLoading')
     set isLoading(value: boolean) {
         this._isLoading = value;
         this.changeDetector.detectChanges();
     }
-
 
     private _title: string;
     get title(): string {
         return this._title;
     }
 
-    @Input("title")
+    @Input('title')
     set title(value: string) {
         this._title = value;
         this.changeDetector.detectChanges();
     }
-
 
     private _navMenu: MatMenuPanel;
     get navMenu(): MatMenuPanel {
         return this._navMenu;
     }
 
-    @Input("navMenu")
+    @Input('navMenu')
     set navMenu(value: MatMenuPanel) {
         this._navMenu = value;
         this.changeDetector.detectChanges();
@@ -56,7 +48,7 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._navGlow;
     }
 
-    @Input("navGlow")
+    @Input('navGlow')
     set navGlow(value: () => boolean) {
         this._navGlow = value;
         this.changeDetector.detectChanges();
@@ -67,7 +59,7 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._currentNav;
     }
 
-    @Input("currentNav")
+    @Input('currentNav')
     set currentNav(value: string) {
         this._currentNav = value;
         this.changeDetector.detectChanges();
@@ -78,7 +70,7 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._showBack;
     }
 
-    @Input("showBack")
+    @Input('showBack')
     set showBack(value: boolean) {
         this._showBack = value;
         this.changeDetector.detectChanges();
@@ -89,7 +81,7 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._hideToolbar;
     }
 
-    @Input("hideToolbar")
+    @Input('hideToolbar')
     set hideToolbar(value: boolean) {
         this._hideToolbar = value;
         this.changeDetector.detectChanges();
@@ -100,40 +92,40 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._showSave;
     }
 
-    @Input("showSave")
+    @Input('showSave')
     set showSave(value: boolean) {
         this._showSave = value;
         this.changeDetector.detectChanges();
     }
 
-    private _contentWidth = "auto";
+    private _contentWidth = 'auto';
     get contentWidth(): string {
         return this._contentWidth;
     }
 
-    @Input("contentWidth")
+    @Input('contentWidth')
     set contentWidth(value: string) {
         this._contentWidth = value;
         this.changeDetector.detectChanges();
     }
 
-    private _contentHeight = "auto";
+    private _contentHeight = 'auto';
     get contentHeight(): string {
         return this._contentHeight;
     }
 
-    @Input("contentHeight")
+    @Input('contentHeight')
     set contentHeight(value: string) {
         this._contentHeight = value;
         this.changeDetector.detectChanges();
     }
 
-    private _contentPadding = "10px";
+    private _contentPadding = '10px';
     get contentPadding(): string {
         return this._contentPadding;
     }
 
-    @Input("contentPadding")
+    @Input('contentPadding')
     set contentPadding(value: string) {
         this._contentPadding = value;
         this.changeDetector.detectChanges();
@@ -144,7 +136,7 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._canMinimize;
     }
 
-    @Input("canMinimize")
+    @Input('canMinimize')
     set canMinimize(value: boolean) {
         this._canMinimize = value;
         this.changeDetector.detectChanges();
@@ -155,12 +147,11 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
         return this._isDraggable;
     }
 
-    @Input("isDraggable")
+    @Input('isDraggable')
     set isDraggable(value: boolean) {
         this._isDraggable = value;
         this.changeDetector.detectChanges();
     }
-    
 
     windowDesign = 1;
     toolbarDesign = 1;
@@ -174,7 +165,7 @@ export class TDSWindowComponent implements OnInit, OnDestroy {
     @ViewChild('toolbar1') toolbar1: TemplateRef<any>;
     @ViewChild('toolbar2') toolbar2: TemplateRef<any>;
 
-    constructor(private changeDetector: ChangeDetectorRef, private settings: SettingsService) { }
+    constructor(private changeDetector: ChangeDetectorRef, private settings: SettingsService) {}
 
     ngOnInit(): void {
         this.settings.ThemeSettingChanged.on(null, this.themeChanged.bind(this));
---
+++ Client/Window/angular/projects/main/src/app/modules/tdswindow/tds-window.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TDSWindowComponent } from './tds-window.component';
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    declarations: [TDSWindowComponent],
+    imports: [CommonModule, MaterialModule, SharedModule],
+    exports: [TDSWindowComponent],
+})
+export class TDSWindowModule {}
