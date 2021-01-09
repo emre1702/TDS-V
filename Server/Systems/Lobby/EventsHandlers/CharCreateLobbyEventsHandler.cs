8c9c2e9d54b5008cb08f5ed5f6e0b3cc2c3f8c3f
emre1702 <emre1702@live.de>
2021-01-09 20:34:51 +0100

    [CODE] Fix not being able to leave char creator in another way

--- Server/Systems/Lobby/EventsHandlers/CharCreateLobbyEventsHandler.cs
+++ Server/Systems/Lobby/EventsHandlers/CharCreateLobbyEventsHandler.cs
@@ -15,9 +15,6 @@ namespace TDS.Server.LobbySystem.EventsHandlers
         private readonly LobbiesHandler _lobbiesHandler;
         private readonly RemoteBrowserEventsHandler _remoteBrowserEventsHandler;
 
-        private static int _addedCancelEventCount = 0;
-        private static object _addedCancelEventCountLock = new object();
-
         public CharCreateLobbyEventsHandler(ICharCreateLobby lobby, EventsHandler eventsHandler, ILoggingHandler loggingHandler, LobbiesHandler lobbiesHandler,
             RemoteBrowserEventsHandler remoteBrowserEventsHandler)
             : base(lobby, eventsHandler, loggingHandler)
@@ -25,27 +22,19 @@ namespace TDS.Server.LobbySystem.EventsHandlers
             _lobbiesHandler = lobbiesHandler;
             _remoteBrowserEventsHandler = remoteBrowserEventsHandler;
 
-            lock (_addedCancelEventCountLock)
-            {
-                if (++_addedCancelEventCount == 1)
-                    _remoteBrowserEventsHandler.AddAsyncEvent(ToServerEvent.CancelCharCreateData, Cancel);
-            }
+            _remoteBrowserEventsHandler.AddMaybeAsyncEvent(ToServerEvent.CancelCharCreateData, Cancel);
         }
 
         protected override void RemoveEvents(IBaseLobby lobby)
         {
             base.RemoveEvents(lobby);
 
-            lock (_addedCancelEventCountLock)
-            {
-                if (--_addedCancelEventCount == 0)
-                    _remoteBrowserEventsHandler.RemoveAsyncEvent(ToServerEvent.CancelCharCreateData);
-            }
+            _remoteBrowserEventsHandler.RemoveMaybeAsyncEvent(ToServerEvent.CancelCharCreateData, Cancel);
         }
 
-        internal async Task<object?> Cancel(ITDSPlayer player, ArraySegment<object> _)
+        internal async ValueTask<object?> Cancel(ITDSPlayer player, ArraySegment<object> _)
         {
-            if (!(player.Lobby is ICharCreateLobby))
+            if (player.Lobby != this)
                 return null;
 
             await _lobbiesHandler.MainMenu.Players.AddPlayer(player, 0).ConfigureAwait(false);
