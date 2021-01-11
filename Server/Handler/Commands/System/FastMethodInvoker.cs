51991f1aff15d47db2f0dfdb7f0db33bb4568035
Jeremy <jenkenkur@gmail.com>
2021-01-11 20:45:03 +0100

    Improve fast invoker.
    - Faster-ish tha before
    - Cleaner code.

--- Server/Handler/Commands/System/FastMethodInvoker.cs
+++ Server/Handler/Commands/System/FastMethodInvoker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using TDS.Server.Data.Models;
@@ -9,98 +10,64 @@ namespace TDS.Server.Handler.Commands.System
     {
         internal FastInvokeHandler GetMethodInvoker(MethodInfo methodInfo)
         {
-            var dynamicMethod = new DynamicMethod(string.Empty, typeof(object), new Type[] { typeof(object),typeof(object[]) },
-                             methodInfo.DeclaringType!.Module);
+            var dynamicMethod = new DynamicMethod(string.Empty, typeof(object), new Type[] { typeof(object), typeof(object[]) }, methodInfo.DeclaringType!.Module);
             var ilGenerator = dynamicMethod.GetILGenerator();
-            var parameters = methodInfo.GetParameters();
-            var paramTypes = new Type[parameters.Length];
-            for (int i = 0; i < paramTypes.Length; i++)
-            {
-                paramTypes[i] = parameters[i].ParameterType;
-            }
-            var locals = new LocalBuilder[paramTypes.Length];
-            for (int i = 0; i < paramTypes.Length; i++)
-            {
-                locals[i] = ilGenerator.DeclareLocal(paramTypes[i]);
-            }
-            for (int i = 0; i < paramTypes.Length; i++)
-            {
-                ilGenerator.Emit(OpCodes.Ldarg_1);
-                EmitFastInt(ilGenerator, i);
-                ilGenerator.Emit(OpCodes.Ldelem_Ref);
-                EmitCastToReference(ilGenerator, paramTypes[i]);
-                ilGenerator.Emit(OpCodes.Stloc, locals[i]);
-            }
+
+            // Get the types of each parameter
+            var paramTypes = methodInfo.GetParameters().Select(p => p.ParameterType).ToArray();
+
+            // Load the object
             ilGenerator.Emit(OpCodes.Ldarg_0);
+
+            // Load all arguments onto the stack.
             for (int i = 0; i < paramTypes.Length; i++)
             {
-                ilGenerator.Emit(OpCodes.Ldloc, locals[i]);
+                // Load the argument from the array. 
+                ilGenerator.Emit(OpCodes.Ldarg_1);
+                EmitInt(i, ilGenerator);
+                ilGenerator.Emit(OpCodes.Ldelem_Ref);
+                EmitCast(paramTypes[i], ilGenerator);
             }
+
+            // Call the method
             ilGenerator.EmitCall(OpCodes.Call, methodInfo, null);
-            if (methodInfo.ReturnType == typeof(void))
-                ilGenerator.Emit(OpCodes.Ldnull);
-            else
-                EmitBoxIfNeeded(ilGenerator, methodInfo.ReturnType);
+
+            // Return the value
+            if (methodInfo.ReturnType == typeof(void)) ilGenerator.Emit(OpCodes.Ldnull);
+            else if (methodInfo.ReturnType.IsValueType) ilGenerator.Emit(OpCodes.Box, methodInfo.ReturnType);
             ilGenerator.Emit(OpCodes.Ret);
+
             return (FastInvokeHandler)dynamicMethod.CreateDelegate(typeof(FastInvokeHandler));
         }
 
-        private void EmitCastToReference(ILGenerator il, Type type)
+        static void EmitCast(Type toType, ILGenerator gen)
         {
-            il.Emit(type.IsValueType ? OpCodes.Unbox_Any : OpCodes.Castclass, type);
+            gen.Emit(toType.IsValueType ? OpCodes.Unbox_Any : OpCodes.Castclass, toType);
         }
 
-        private void EmitBoxIfNeeded(ILGenerator il, Type type)
+        static void EmitInt(int i, ILGenerator gen)
         {
-            if (type.IsValueType)
+            if (i > 8)
             {
-                il.Emit(OpCodes.Box, type);
-            }
-        }
-
-        private void EmitFastInt(ILGenerator il, int value)
-        {
-            switch (value)
-            {
-                case -1:
-                    il.Emit(OpCodes.Ldc_I4_M1);
-                    return;
-                case 0:
-                    il.Emit(OpCodes.Ldc_I4_0);
-                    return;
-                case 1:
-                    il.Emit(OpCodes.Ldc_I4_1);
-                    return;
-                case 2:
-                    il.Emit(OpCodes.Ldc_I4_2);
-                    return;
-                case 3:
-                    il.Emit(OpCodes.Ldc_I4_3);
-                    return;
-                case 4:
-                    il.Emit(OpCodes.Ldc_I4_4);
-                    return;
-                case 5:
-                    il.Emit(OpCodes.Ldc_I4_5);
-                    return;
-                case 6:
-                    il.Emit(OpCodes.Ldc_I4_6);
-                    return;
-                case 7:
-                    il.Emit(OpCodes.Ldc_I4_7);
-                    return;
-                case 8:
-                    il.Emit(OpCodes.Ldc_I4_8);
-                    return;
-            }
-
-            if (value > -129 && value < 128)
-            {
-                il.Emit(OpCodes.Ldc_I4_S, (SByte)value);
+                gen.Emit(OpCodes.Ldc_I4_S, (byte)i); // cba to support above 255
             }
             else
             {
-                il.Emit(OpCodes.Ldc_I4, value);
+                OpCode code = i switch
+                {
+                    0 => OpCodes.Ldc_I4_0,
+                    1 => OpCodes.Ldc_I4_1,
+                    2 => OpCodes.Ldc_I4_2,
+                    3 => OpCodes.Ldc_I4_3,
+                    4 => OpCodes.Ldc_I4_4,
+                    5 => OpCodes.Ldc_I4_5,
+                    6 => OpCodes.Ldc_I4_6,
+                    7 => OpCodes.Ldc_I4_7,
+                    8 => OpCodes.Ldc_I4_8,
+                    _ => throw new Exception()
+                };
+
+                gen.Emit(code);
             }
         }
     }
