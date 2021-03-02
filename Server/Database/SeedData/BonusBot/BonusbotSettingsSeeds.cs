af1956957c52886ae4beb361e424f8ae57396a4e
emre1702 <emre1702@live.de>
2021-03-02 18:42:00 +0100

    [CODE] Remove default bonusbot settings

--- Server/Database/SeedData/BonusBot/BonusbotSettingsSeeds.cs
+++ Server/Database/SeedData/BonusBot/BonusbotSettingsSeeds.cs
@@ -14,14 +14,6 @@ namespace TDS.Server.Database.SeedData.BonusBot
                 new BonusbotSettings
                 {
                     Id = 1,
-                    GuildId = 320309924175282177,
-                    AdminApplicationsChannelId = 659072893526736896,
-                    SupportRequestsChannelId = 659073029896142855,
-                    ServerInfosChannelId = 659073271911809037,
-                    ActionsInfoChannelId = 659088752890871818,
-                    BansInfoChannelId = 659705941771550730,
-
-                    ErrorLogsChannelId = 659073884796092426,
 
                     SendPrivateMessageOnBan = true,
                     SendPrivateMessageOnOfflineMessage = true,
